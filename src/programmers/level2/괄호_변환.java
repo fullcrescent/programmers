91de60253c7b9d6ef32ed9b413202f944a8026e5
user <user@DESKTOP-61GCUTL>
2021-12-18 20:40:44 +0900

    2021-12-18

---
+++ src/programmers/level2/괄호_변환.java
@@ -0,0 +1,59 @@
+package programmers.level2;
+
+public class °ýÈ£_º¯È¯ {
+	public static String solution(String p) {
+	       return p=="" ? "" : stringSeparation1(p);
+	}
+	
+	public static String stringSeparation1(String string) {
+		int standard = 0;
+		int index = 0;
+		String reverse = "";
+		for(int i=0; i<string.length(); i++) {
+			if(string.charAt(i) == '(') {
+				standard++;
+				reverse += ")";
+			}else if(string.charAt(i) == ')') {
+				standard--;
+				reverse += "(";
+			} 
+			
+			if(standard == 0) {
+				index=i;
+				break;
+			}
+		}
+		
+		if(stringVerification1(string.substring(0, index+1))) {
+			return string.substring(0, index+1) + (index+1 == string.length() ? "" : stringSeparation1(string.substring(index+1, string.length())));
+		}else {
+			return  "(" + (index+1 == string.length() ? "" : stringSeparation1(string.substring(index+1, string.length()))) + ")" + reverse.substring(1, reverse.length()-1);
+		}
+	}
+	
+	public static boolean stringVerification1(String string) {
+		boolean verification = true;
+		
+		int standard = 0;
+		for(int i=0; i<string.length(); i++	){
+			if(string.charAt(i) == '(') {
+				standard++;
+			}else if(string.charAt(i) == ')') {
+				standard--;
+			} 
+			
+			if(standard < 0) {
+				verification = false;
+				break;
+			}
+		}
+		
+		return verification;
+	}
+	
+	public static void main(String args[]) {
+		String p = "()))((()";
+		String answer = solution(p);
+		System.out.println(answer);
+	}
+}
