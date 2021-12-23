40e4961250b361f00d47f86b9ac6dddf5af220b9
fullcrescent <fullcrescent@naver.com>
2021-12-23 23:13:24 +0900

    2021-12-23 알고리즘 소스

--- src/programmers/level2/수식_최대화.java
+++ src/programmers/level2/수식_최대화.java
@@ -5,21 +5,21 @@ import java.util.regex.Pattern;
 
 public class 수식_최대화 {
 	public static void main(String args[]) {
-		String expression = "50*6-3*2";
+		String expression = "100-200+100*200-500*300-300";
 		long answer = solution(expression);
 		System.out.println(answer);
 	}
 	
 	public static long solution(String expression) {
-		Pattern patternPrev = Pattern.compile("^[-]?[0-9]*[^*+-]");
-		Pattern patternNext= Pattern.compile("[^*+-][0-9]*$");
+		Pattern patternPrev = Pattern.compile("[^*+-][0-9]*$");
+		Pattern patternNext= Pattern.compile("^[-]?[0-9]*[^*+-]");
 		
 		String operationSelect = "";
-		if(expression.contains("+")) {
-			operationSelect += "+";
-		}
 		if(expression.contains("-")) {
 			operationSelect += "-";
+		}
+		if(expression.contains("+")) {
+			operationSelect += "+";
 		}
 		if(expression.contains("*")) {
 			operationSelect += "*";
@@ -32,12 +32,12 @@ public class 수식_최대화 {
 		return max;
 	}
 	
-	private static int max=0; 
+	private static long max=0; 
 	
 	private static void permutation(String expression, String operationSelect, boolean[] visited, Pattern patternPrev, Pattern patternNext, int count, int index, StringBuffer operations) {
 		if(index == count) {
 			String value = split(expression, operations.toString().trim(), 0, patternPrev, patternNext);
-			max = Math.max(max, Math.abs(Integer.parseInt(value)));
+			max = Math.max(max, Math.abs(Long.parseLong(value)));
 			return;
 		}
 		
@@ -58,9 +58,6 @@ public class 수식_최대화 {
 	
 	private static String split(String expression, String operations, int index, Pattern patternPrev, Pattern patternNext) {
 		if(operations.length() == index) {
-			if(expression.chars().filter(c -> c == '-').count()%2==0) {
-				expression = expression.replaceAll("-", "");
-			}
 			return expression;
 		}
 		
@@ -71,56 +68,36 @@ public class 수식_최대화 {
 		Matcher matcherPrev;
 		Matcher matcherNext;
 		
-		int count =0;
+		long count =0;
 		
 		for(int i=0; i<splitArray.length-1; i++) {
-			matcherNext = patternPrev.matcher(splitArray[i+1]);
+			matcherPrev = patternPrev.matcher(splitArray[i]);
+			matcherPrev.find();
+			splitArray[i] = splitArray[i].substring(0, splitArray[i].length() - matcherPrev.group().length());
+
+			matcherNext = patternNext.matcher(splitArray[i+1]);
 			matcherNext.find();
 			splitArray[i+1] = splitArray[i+1].substring(matcherNext.group().length(), splitArray[i+1].length());
 			
-			if(splitArray[i].equals("")) {
-				count = opertaion(count, Integer.valueOf(matcherNext.group()), operations.charAt(index));
-				if(!splitArray[i+1].equals("")) {
-					expression += count;
-					if(i+2 == splitArray.length) {
-						expression += splitArray[i+1];
-					}
-				}else if(i+2 == splitArray.length) {
-					expression += count;
-				}
+			if(operations.charAt(index) == '-' && splitArray[i].length() >0 && splitArray[i].charAt(splitArray[i].length()-1)=='-') {
+				count = opertaion(Long.valueOf(matcherPrev.group()), Long.valueOf(matcherNext.group()), '+');
+			}else if(operations.indexOf('-') < operations.indexOf('+') && operations.charAt(index) == '+' && splitArray[i].length() >0 && splitArray[i].charAt(splitArray[i].length()-1)=='-') {
+				count = opertaion(Long.valueOf(matcherPrev.group()), Long.valueOf(matcherNext.group()), '-');
 			}else {
-				matcherPrev = patternNext.matcher(splitArray[i]);
-				matcherPrev.find();
-				
-				splitArray[i] = splitArray[i].substring(0, splitArray[i].length() - matcherPrev.group().length());
-				
-				count = opertaion(Integer.valueOf(matcherPrev.group()), Integer.valueOf(matcherNext.group()), operations.charAt(index));
-				
-				if(splitArray[i].equals("")&&splitArray[i+1].equals("") && i+2 != splitArray.length) {
-					continue;
-				}else if(splitArray[i].equals("")){
-					splitArray[i+1] = count + splitArray[i+1]; 
-					if(i+2 == splitArray.length) {
-						expression = splitArray[i+1];
-					}
-					continue;
-				}
-				
-				
-				if(!splitArray[i+1].equals("") || i+2 == splitArray.length) {
-					expression += splitArray[i] + count + splitArray[i+1];
-				}else {
-					expression += splitArray[i];
-				}
-				
+				count = opertaion(Long.valueOf(matcherPrev.group()), Long.valueOf(matcherNext.group()), operations.charAt(index));
 			}
 			
+			splitArray[i+1] = splitArray[i] + count + splitArray[i+1];
 		}
 		
-		return split(expression, operations, index+1, patternPrev, patternNext);
+		if(splitArray[splitArray.length-1].contains("--")) {
+			splitArray[splitArray.length-1] = splitArray[splitArray.length-1].replaceAll("--", "");
+		}
+		
+		return split(splitArray[splitArray.length-1], operations, index+1, patternPrev, patternNext);
 	}
 
-	private static int opertaion(int number1, int number2, char operation) {
+	private static long opertaion(long number1, long number2, char operation) {
 		if(operation == '+') {
 			return number1 + number2;
 		}
