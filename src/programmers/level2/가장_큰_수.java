74b90e4098390634f6806b21a065e2943c7b65de
fullcrescent <fullcrescent@naver.com>
2021-12-27 21:58:31 +0900

    2021-12-27 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/가장_큰_수.java
+++ src/programmers/level2/가장_큰_수.java
@@ -1,6 +1,10 @@
 package programmers.level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class 가장_큰_수 {
 
@@ -8,12 +12,24 @@ public class 가장_큰_수 {
 		int[] numbers = {90,908,89,898,10,101,1,8,9};
 		String answer = solution(numbers);
 		System.out.println(answer);
+		
+		int[] numbers1 = {90,908,89,898,10,101,1,8,9};
+		String answer1 = solution1(numbers1);
+		System.out.println(answer1);
+		
+		int[] numbers2 = {90,908,89,898,10,101,1,8,9};
+		String answer2 = solution1(numbers2);
+		System.out.println(answer2);
+		
+		int[] numbers3 = {90,908,89,898,10,101,1,8,9};
+		String answer3 = solution1(numbers3);
+		System.out.println(answer3);
 	}
 
 	public static String solution(int[] numbers) {
 		String[] numbersToString = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
 		sort(numbersToString);
-		
+
 		String answer = "";
 		
 		for(String string : numbersToString) {
@@ -85,4 +101,41 @@ public class 가장_큰_수 {
         }
     }
 	
+	// 다른 사람의 풀이 참고
+	public static String solution1(int[] numbers) {
+		String answer = Arrays.stream(numbers).mapToObj(String::valueOf).sorted((s1, s2) -> -s1.concat(s2).compareTo(s2.concat(s1))).reduce("", (s1, s2) -> s1.concat(s2));
+		
+		return answer.charAt(0) != '0' ? answer : "0";
+	}
+	
+	public static String solution2(int[] numbers) {
+		String[] numbersToString = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+		
+		Arrays.sort(numbersToString , new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				return -s1.concat(s2).compareTo(s2.concat(s1));
+			}
+		});
+		
+		String answer = Arrays.stream(numbersToString).reduce("", (s1, s2) -> s1.concat(s2));
+		
+		return answer.charAt(0) != '0' ? answer : "0";
+	}
+	
+	public static String solution3(int[] numbers) {
+		List<String> stringList = new ArrayList<>(Arrays.asList(Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new)));
+		
+		Collections.sort(stringList, (s1,s2) ->{
+			return s2.concat(s1).compareTo(s1.concat(s2));
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		for(String s : stringList) {
+			sb.append(s);
+		}
+		
+		String answer = sb.toString();
+		
+		return answer.charAt(0) != '0' ? answer : "0";
+	}
 }
