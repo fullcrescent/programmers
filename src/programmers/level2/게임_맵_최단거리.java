9c06d406d8fef1f820c4ce987bc1ae37e9713ac3
fullcrescent <fullcrescent@naver.com>
2022-01-04 22:03:05 +0900

    2022-01-04 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/게임_맵_최단거리.java
+++ src/programmers/level2/게임_맵_최단거리.java
@@ -1,11 +1,18 @@
 package programmers.level2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class 게임_맵_최단거리 {
 
 	public static void main(String[] args) {
 		int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
 		int answer = solution(maps);
 		System.out.println(answer);
+		
+		int[][] maps1 = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+		int answer1 = solution1(maps1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(int[][] maps) {
@@ -52,6 +59,46 @@ public class 게임_맵_최단거리 {
 				sum[x][y-1] = count;
 				findLoad(maps, x, y-1, count);
 			}
+		}
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int[][] maps) {
+		int[][] visit = new int[maps.length][maps[0].length];
+		visit[0][0]=1;
+		
+		Queue<Point> queue = new LinkedList<Point>();
+		queue.add(new Point(0,0));
+		
+		int[] dx= {0, 0, 1, -1}; 
+		int[] dy= {1, -1, 0, 0};
+		
+		while(!queue.isEmpty()) {
+			Point point = queue.poll();
+			
+			for(int i=0; i<dx.length; i++) {
+				int x=point.x+dx[i];
+				int y=point.y+dy[i];
+				
+				if(x>=0 && x<maps.length && y>=0 && y<maps[0].length) {
+					if(maps[x][y]==1 && visit[x][y]==0) {
+						queue.add(new Point(x,y));
+						visit[x][y]=visit[point.x][point.y]+1;
+					}
+				}
+			}
+		}
+		
+		return visit[maps.length-1][maps[0].length-1]>0 ? visit[maps.length-1][maps[0].length-1] : -1;
+	}
+	
+	static class Point{
+		int x;
+		int y;
+		
+		public Point(int x, int y) {
+			this.x=x;
+			this.y=y;
 		}
 	}
 }
