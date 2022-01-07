dc46b5694e46d9bafd1e72858caae8b032f73a13
fullcrescent <fullcrescent@naver.com>
2022-01-07 22:31:06 +0900

    2022-01-07 알고리즘 소스

--- src/programmers/level2/순위_검색.java
+++ src/programmers/level2/순위_검색.java
@@ -1,8 +1,11 @@
 package programmers.level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class 순위_검색 {
 
@@ -11,7 +14,7 @@ public class 순위_검색 {
 				,"python frontend senior chicken 210"
 				,"python frontend senior chicken 150"
 				,"cpp backend senior pizza 260"
-				,"java backend junior chicken 80"
+				,"java backend junior pizza 80"
 				,"python backend senior chicken 50"};
 		String[] query = {"java and backend and junior and pizza 100"
 				,"python and frontend and senior and chicken 200"
@@ -25,11 +28,73 @@ public class 순위_검색 {
 
 	public static int[] solution(String[] info, String[] query) {
 		int[] answer = new int[query.length];
-		List<String> list = Arrays.asList(info);
+		Map<String, List<Integer>> map = new HashMap<>();
 		
-		list.sort(Comparator.comparingInt((i)->Integer.parseInt(i.replaceAll("[a-z ]", ""))));
+		for(String temp : info) {
+			int index = temp.lastIndexOf(" "); 
+			
+			String key = temp.substring(0, index);
+			int value = Integer.valueOf(temp.substring(index+1, temp.length()));
+			
+			if(map.get(key) == null) {
+				List<Integer> list = new ArrayList<>();
+				list.add(value);
+				map.put(temp.substring(0, index), list);
+			}else {
+				map.get(key).add(value);
+			}
+		}
+		
+		Iterator<String> iter = map.keySet().iterator();
+		
+		while(iter.hasNext()) {
+			map.get(iter.next()).sort(null);
+		}
+		
+		int index=0;
+		
+		for(String temp : query) {
+			String[] tempValue = temp.replaceAll("and ", "").split(" ");
+			answer[index++] = search(map, tempValue[0], tempValue[1], tempValue[2], tempValue[3], Integer.valueOf(tempValue[4]));
+		}
 		
 		return answer;
 	}
-
+	
+	private static int search(Map<String, List<Integer>> map, String language, String jobGroup, String career, String soulFood, int score) {
+		if(language.equals("-")) {
+			return search(map, "java", jobGroup, career, soulFood, score)
+					+ search(map, "cpp", jobGroup, career, soulFood, score)
+					+ search(map, "python", jobGroup, career, soulFood, score);
+		}else if(jobGroup.equals("-")) {
+			return search(map, language, "frontend", career, soulFood, score)
+					+ search(map, language, "backend", career, soulFood, score);
+		}else if(career.equals("-")) {
+			return search(map, language, jobGroup, "junior", soulFood, score)
+					+ search(map, language, jobGroup, "senior", soulFood, score);
+		}else if(soulFood.equals("-")) {
+			return search(map, language, jobGroup, career, "chicken", score)
+					+ search(map, language, jobGroup, career, "pizza", score);
+		}else {
+			List<Integer> list = map.get(language + " " + jobGroup + " " + career + " " + soulFood);
+			if(list == null) {
+				return 0;
+			}
+				
+			int start =0;
+			int end = list.size()-1;
+			
+			while(start<=end) {
+				int mid = (start+end)/2;
+				
+				if(list.get(mid)<score) {
+					start=mid+1;
+				}else {
+					end=mid-1;
+				}
+			}
+			
+			return list.size()-start;
+		}
+	}
 }
