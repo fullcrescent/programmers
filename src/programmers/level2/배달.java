4856cae91b0540512e92c81c9ae3076ad077d763
fullcrescent <fullcrescent@naver.com>
2022-01-09 18:23:32 +0900

    2022-01-09 알고리즘 소스

--- src/programmers/level2/배달.java
+++ src/programmers/level2/배달.java
@@ -1,94 +1,78 @@
 package programmers.level2;
 
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class 배달 {
 
 	public static void main(String[] args) {
-		int N = 5;
-		int[][] road = {{1,2,2},{2,3,3},{5,2,2},{1,4,1},{5,3,1},{5,4,2}};
-		int K = 3;
+		int N = 6;
+		int[][] road = {{1,2,1},{1,3,2},{2,3,2},{3,4,3},{3,5,2},{3,5,3},{5,6,1}};
+		int K = 4;
 		int answer = solution(N, road, K);
 		System.out.println(answer);
 	}
 
 	public static int solution(int N, int[][] road, int K) {
-		// 배달 거리가 가장 가까운 순으로 정렬 -> 크루스칼 알고리즘
-		Queue<int[]> queue = new PriorityQueue<int[]>(N, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				if(o1[2]!=o2[2]) {
-					return Integer.compare(o1[2], o2[2]);
-				}
-				int big1 = o1[0]>o1[1] ? o1[0] : o1[1];
-				int small1 = o1[0]<o1[1] ? o1[0] : o1[1];
-				
-				int big2 = o2[0]>o2[1] ? o2[0] : o2[1];
-				int small2 = o2[0]<o2[1] ? o2[0] : o2[1];
-				
-				if(big1!=big2) {
-					return Integer.compare(big1, big2);
-				}else {
-					return Integer.compare(small1, small2);
-				}
+		// 배달 거리가 가장 가까운 순으로 정렬 -> 다익스트라 알고리즘
+		
+		// 시작점 및 최대값 세팅
+		int start = 1;
+		int max = 500001;
+		int next;
+		
+		// 원본 배열을 저장할 리스트와 최소거리를 정렬해줄 큐 선언 
+		List<int[]> list = new LinkedList<>(Arrays.asList(road));
+		Queue<int[]> queue = new PriorityQueue<>((i1, i2) -> Integer.compare(i1[2], i2[2]));
+		
+		// 정점의 방문 여부를 저장할 배열 및 시작점에서의 거리 배열  
+		boolean[] visit = new boolean[N+1];
+		int[] distance = new int[N+1];
+		
+		Arrays.fill(distance, max);
+		distance[start] = 0;
+		visit[start] = true;
+		
+		// 시작점과 연결된 간선 찾고 삭제 - list의 길이 변경이 일어나므로 Iterator를 통해서 작업 
+		Iterator<int[]> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			int[] temp = iterator.next();
+			if(temp[0]==start || temp[1]==start) {
+				queue.add(temp);
+				iterator.remove();
 			}
-		});
-		
-		for(int[] temp : road) {
-			queue.add(temp);
-		}
-
-		int answer=0;
-		
-		int[] mstValue = new int[N+1];
-		int[] parent = new int[N+1];
-		
-		for(int i=0; i<parent.length; i++) {
-			parent[i]=i;
 		}
 		
 		while(!queue.isEmpty()) {
-			int[] value=queue.poll();
-			System.out.println(Arrays.toString(value));
-			// 사이클 검사
-			if(find(parent, value[0])!=find(parent, value[1])){
-				int big = value[0]>value[1] ? value[0] : value[1];
-				int small = value[0]<value[1] ? value[0] : value[1];
-				
-				parent[big] = small;
-				mstValue[big] = value[2];
+			int[] roadValue = queue.poll();
+			
+			// 큐에서 가져온 값에서 시작점과 끝점을 찾음 
+			start = visit[roadValue[0]] ? roadValue[0] : roadValue[1]; 
+			next = (roadValue[0]!=start) ? roadValue[0] : roadValue[1];
+			
+			// 다음 정점이 방문한 정점일 경우 스킵
+			if(visit[next]) continue;
+			visit[next] = true;
+			
+			distance[next] = roadValue[2];
+			
+			iterator = list.iterator();
+			while(iterator.hasNext()) {
+				int[] temp = iterator.next();
+				if(temp[0]==next || temp[1]==next) {
+					iterator.remove();
+					// 시작점에서의 거리를 더해서 큐에 추가
+					temp[2] += distance[next];
+					queue.add(temp);
+				}
 			}
 		}
 		
-		for(int i=1; i<parent.length; i++) {
-			if(find(parent, i)!=1) {
-				continue;
-			}
-			if(weight(parent, mstValue, i)<=K) {
-				answer++;
-			}
-		}
-		
-		return answer;
-	}
-	
-	private static int find(int[] parent, int index) {
-		if(parent[index]==index) {
-			return index;
-		}else {
-			return find(parent, parent[index]);
-		}
-	}
-	
-	private static int weight(int[] parent, int[] mstValue, int index) {
-		if(parent[index]==index){
-        	return 0;
-        }else {
-			return mstValue[index] + weight(parent, mstValue, parent[index]);
-		}
+		return (int) Arrays.stream(distance).filter(i -> i<=K).count();
 	}
 	
 }
