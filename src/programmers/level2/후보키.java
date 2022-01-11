7e7211a10f13292cc69eb02a24c0f9fe09cfd70b
fullcrescent <fullcrescent@naver.com>
2022-01-11 22:24:18 +0900

    2022-01-11 알고리즘 소스

--- src/programmers/level2/후보키.java
+++ src/programmers/level2/후보키.java
@@ -9,13 +9,18 @@ import java.util.Map;
 public class 후보키 {
 
 	public static void main(String[] args) {
+//		String[][] relation = {
+//				{"100","ryan","music","2", "4"}
+//				,{"200","apeach","math","2", "5"}
+//				,{"300","tube","computer","3", "6"}
+//				,{"400","con","computer","4", "7"}
+//				,{"500","muzi","music","3", "8"}
+//				,{"600","ryan","music","2", "4"}};
+		
+		// 처음 생각한 조건문 if(map.containsKey(key) || map.containsValue(value))이 유효하게 동작하지 않게하는 예시
 		String[][] relation = {
-				{"100","ryan","music","2"}
-				,{"200","apeach","math","2"}
-				,{"300","tube","computer","3"}
-				,{"400","con","computer","4"}
-				,{"500","muzi","music","3"}
-				,{"600","apeach","music","2"}};
+				{"aaa","a2b","c"}
+				,{"bbb","a","b2c"}};
 		int answer = solution(relation);
 		System.out.println(answer);
 	}
@@ -24,16 +29,20 @@ public class 후보키 {
 		boolean visit[] = new boolean[relation[0].length];
 		
 		for(int i=0; i<visit.length; i++) {
+			// visit.length개에서 i+1개를 뽑는 경우의 수 - 조합
 			combination(relation, visit, 0, i+1);
 		}
 
 		return candidateVisit.size();
 	}
 	
+	// 후보키 방문 내역 리스트
 	private static List<boolean[]> candidateVisit = new LinkedList<>();
 	
 	private static void combination(String[][] relation, boolean[] visit, int index, int count) {
+		// 주어진 개수만큼 뽑았을 경우
 		if(count==0) {
+			// 후보키 방문 내역 리스트에서 visit를 비교하여 최소성 검사
 			for(boolean[] tempVisit : candidateVisit) {
 				int tempIndex = 0;
 				
@@ -55,6 +64,7 @@ public class 후보키 {
 				}
 			}
 			
+			// 유일성 검사			
 			Map<String, String> map = new HashMap<>();
 			
 			for(int i=0; i<relation.length; i++) {
@@ -63,12 +73,15 @@ public class 후보키 {
 				
 				for(int j=0; j<relation[i].length; j++) {
 					if(visit[j]) {
-						key += relation[i][j];
+						key += j+relation[i][j];
 					}else {
-						value += relation[i][j];
+						value += j+relation[i][j];
 					}
 				}
-				if(map.containsKey(key) || map.containsValue(value)) {
+				
+				// 처음 생각한 조건문 if(map.containsKey(key) || map.containsValue(value))의 경우 배열을 조작하면 잘못된값 출력 가능
+				// key 중복일 경우 종료
+				if(map.containsKey(key)) {
 					return;
 				}
 				map.put(key, value);
@@ -81,6 +94,7 @@ public class 후보키 {
 			return;
 		}
 		
+		// 조합
 		for(int i=index; i<visit.length; i++) {
 			visit[i] = true;
 			combination(relation, visit, i+1, count-1);
