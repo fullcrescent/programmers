0ee0776740f34772f1676e5d174560ee457c55b3
fullcrescent <fullcrescent@naver.com>
2022-01-15 21:02:29 +0900

    2022-01-15 알고리즘 소스

---
+++ src/programmers/level2/H_Index.java
@@ -0,0 +1,29 @@
+package programmers.level2;
+
+import java.util.Arrays;
+
+public class H_Index {
+
+	public static void main(String[] args) {
+		int[] citations = {3, 0, 6, 1, 5};
+		int answer = solution(citations);
+		System.out.println(answer);
+	}
+
+	public static int solution(int[] citations) {
+		int answer=0;
+		
+		while(true) {
+			int count = answer;
+			if(Arrays.stream(citations).filter(i -> i>=count).count()>=count) {
+				answer++;
+				continue;
+			}
+			
+			break;
+		}
+		
+		return answer-1;
+	}
+
+}
