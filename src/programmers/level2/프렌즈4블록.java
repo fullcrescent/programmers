3db8f799ec4cc18449d73c744ba967efd3f94eb7
fullcrescent <fullcrescent@naver.com>
2022-01-17 11:40:31 +0900

    2022-01-17 알고리즘 소스

--- src/programmers/level2/프렌즈4블록.java
+++ src/programmers/level2/프렌즈4블록.java
@@ -13,28 +13,69 @@ public class 프렌즈4블록 {
 	public static int solution(int m, int n, String[] board) {
         int answer = 0;
         
+        char[][] charBoard = new char[m][n];
         boolean[][] delete = new boolean[m][n]; 
         
-        for(int i=0; i<m; i++) {
-        	for(int j=0; j<n; j++) {
-        		verification(board, i, j, delete);
+        int x = 0;
+        for(String temp : board) {
+       		charBoard[x++] = temp.toCharArray();
+        }
+        while(true) {
+        	for(int i=0; i<m; i++) {
+        		for(int j=0; j<n; j++) {
+        			verification(charBoard, i, j, delete);
+        		}
+        	}
+        	
+        	int count = 0;
+        	for(boolean[] temp1 : delete) {
+        		for(boolean temp2 : temp1) {
+        			if(temp2) count ++; 
+        		}
+        	}
+        	
+        	answer += count;
+        	
+        	if(count==0) {
+        		break;
+        	}
+        	
+        	for(int i=0; i<m; i++) {
+        		for(int j=0; j<n; j++) {
+        			if(i==0) {
+        				delete[i][j] = false;
+        			}else if(delete[i][j]) {
+        				int temp1 = i;
+        				int temp2 = j;
+        				while(temp1>0) {
+        					charBoard[temp1][temp2] = charBoard[--temp1][temp2];
+        				}
+        				charBoard[temp1][temp2] = 0;
+        				delete[i][j] = false;
+        			}
+        		}
         	}
         }
+        
         return answer;
     }
 	
 	static int[] dx = {0, 1, 1};
 	static int[] dy = {1, 0, 1};
 	
-	private static void verification(String[] board, int i, int j, boolean[][] delete) {
-		char tile = board[i].charAt(j);
+	private static void verification(char[][] charBoard, int i, int j, boolean[][] delete) {
+		char tile = charBoard[i][j];
+		
+		if(tile==0) {
+			return;
+		}
 		
 		int count = 1;
 		
 		for(int index=0; index<dx.length; index++) {
 			int x = i+dx[index];
 			int y = j+dy[index];
-			if(x<board.length && y<board[i].length() && tile == board[x].charAt(y)) {
+			if(x<charBoard.length && y<charBoard[i].length && tile == charBoard[x][y]) {
 				count++;
 			}
 		}
