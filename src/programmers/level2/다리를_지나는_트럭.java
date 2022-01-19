5775b72d0a551cf9b185db5c772efb83071e7f79
fullcrescent <fullcrescent@naver.com>
2022-01-19 22:17:32 +0900

    2022-01-19 알고리즘 소스 및 다른 사람의 풀이 작성중

--- src/programmers/level2/다리를_지나는_트럭.java
+++ src/programmers/level2/다리를_지나는_트럭.java
@@ -11,6 +11,12 @@ public class 다리를_지나는_트럭 {
 		int[] truck_weights = {10,10,10,10,10,10,10,10,10,10};
 		int answer = solution(bridge_length, weight, truck_weights);
 		System.out.println(answer);
+		
+		int bridge_length1 = 100;
+		int weight1 = 100;
+		int[] truck_weights1 = {10,10,10,10,10,10,10,10,10,10};
+		int answer1 = solution1(bridge_length1, weight1, truck_weights1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(int bridge_length, int weight, int[] truck_weights) {
@@ -42,4 +48,18 @@ public class 다리를_지나는_트럭 {
 		
 		return hour+(site.length-1);
 	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int bridge_length, int weight, int[] truck_weights) {
+		
+		
+		
+		
+		
+		return 0;
+	}
+	
+	
+	
+	
 }
