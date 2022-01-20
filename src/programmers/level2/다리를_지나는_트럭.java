30b42788775dce4bc87549ac06e3ce10ab8a9af7
fullcrescent <fullcrescent@naver.com>
2022-01-20 23:26:21 +0900

    2022-01-20 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/다리를_지나는_트럭.java
+++ src/programmers/level2/다리를_지나는_트럭.java
@@ -1,7 +1,9 @@
 package programmers.level2;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 public class 다리를_지나는_트럭 {
@@ -51,15 +53,34 @@ public class 다리를_지나는_트럭 {
 	
 	// 다른 사람의 풀이 참고
 	public static int solution1(int bridge_length, int weight, int[] truck_weights) {
+		Queue<Integer> queue = new LinkedList<>();
+		// 현재 지나가고 있는 트럭을 나타냄
+		Map<Integer, Integer> map = new HashMap<>();
 		
+		for(int temp : truck_weights) {
+			queue.add(temp);
+		}
 		
+		int answer = 0;
+		int max = 0;
 		
+		while(!queue.isEmpty()) {
+			answer++;
+			
+			if(map.containsKey(answer)) {
+				map.remove(answer);
+			}
+			
+			if(!queue.isEmpty()) {
+				if(map.values().stream().mapToInt(Number::intValue).sum() + queue.peek() <= weight) {
+					// 마지막으로 map에 추가 되는 값이 최대값
+					max = answer+bridge_length;
+					map.put(max, queue.poll());
+				}
+			}
+		}
 		
-		
-		return 0;
+		return max;
 	}
-	
-	
-	
 	
 }
