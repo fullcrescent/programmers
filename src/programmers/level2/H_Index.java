7d22440ee5ba39f73ec0e91f233f4768c75ab7b9
fullcrescent <fullcrescent@naver.com>
2022-01-20 22:28:37 +0900

    2022-01-20 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/H_Index.java
+++ src/programmers/level2/H_Index.java
@@ -8,6 +8,10 @@ public class H_Index {
 		int[] citations = {3, 0, 6, 1, 5};
 		int answer = solution(citations);
 		System.out.println(answer);
+		
+		int[] citations1 = {3, 0, 6, 1, 5};
+		int answer1 = solution1(citations1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(int[] citations) {
@@ -26,5 +30,19 @@ public class H_Index {
 		return answer-1;
 	}
 	
-	// 다른 사람의 풀이 참고 작성중
+	// 다른 사람의 풀이 참고 
+	public static int solution1(int[] citations) {
+		Arrays.sort(citations);
+		
+		int max = 0;
+		
+		for(int i=0; i<citations.length; i++) {
+			// 배열 값은 최소 인용 횟수, 길이값은 최대 논문수 서로 줄여가며 값 탐색
+			int min = Math.min(citations[i], citations.length - i);
+			if(max<min) max = min;
+		}
+		
+		return max;
+	}
+	
 }
