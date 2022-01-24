d13af97cbca4e9d3bc076c4b69a90c6c2d7dfc59
fullcrescent <fullcrescent@naver.com>
2022-01-24 23:29:40 +0900

    2022-01-24 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/주식가격.java
+++ src/programmers/level2/주식가격.java
@@ -1,6 +1,7 @@
 package programmers.level2;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class 주식가격 {
 
@@ -8,6 +9,14 @@ public class 주식가격 {
 		int[] prices = {1, 2, 3, 2, 3};
 		int[] answer = solution(prices);
 		System.out.println(Arrays.toString(answer));
+		
+		int[] prices1 = {1, 2, 3, 2, 3};
+		int[] answer1 = solution1(prices1);
+		System.out.println(Arrays.toString(answer1));
+		
+		int[] prices2 = {1, 2, 3, 2, 3};
+		int[] answer2 = solution2(prices2);
+		System.out.println(Arrays.toString(answer2));
 	}
 	
 	public static int[] solution(int[] prices) {
@@ -23,6 +32,45 @@ public class 주식가격 {
 				count++;
 			}
 			answer[i] = count;
+		}
+		
+		return answer;
+    }
+	
+	// 다른 사람의 풀이 참고 
+	public static int[] solution1(int[] prices) {
+		int[] answer = new int[prices.length];
+		
+		for(int i=0; i<prices.length-1; i++) {
+			for(int j=i+1; j<prices.length; j++) {
+				answer[i]++;
+				if(prices[i]>prices[j]) {
+					break;
+				}
+			}
+		}
+		
+		return answer;
+    }
+	
+	public static int[] solution2(int[] prices) {
+		int[] answer = new int[prices.length];
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		stack.push(0);
+		
+		for(int i=1; i<prices.length; i++) {
+			while(!stack.isEmpty()&&prices[i]<prices[stack.peek()]) {
+				int temp = stack.pop();
+				answer[temp] = i - temp;
+			}
+			stack.push(i);
+		}
+		
+		while(!stack.isEmpty()) {
+			int temp = stack.pop();
+			answer[temp] = prices.length-temp-1;
 		}
 		
 		return answer;
