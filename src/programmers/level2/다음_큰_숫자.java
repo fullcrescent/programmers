a12c7e4902d701ce5d13685ebc0596a57e8d772a
fullcrescent <fullcrescent@naver.com>
2022-02-06 22:36:58 +0900

    2022-02-06 알고리즘 소스

--- src/programmers/level2/다음_큰_숫자.java
+++ src/programmers/level2/다음_큰_숫자.java
@@ -6,6 +6,14 @@ public class 다음_큰_숫자 {
 		int n = 78;
 		int answer = solution(n);
 		System.out.println(answer);
+		
+		int n1 = 78;
+		int answer1 = solution1(n1);
+		System.out.println(answer1);
+		
+		int n2 = 78;
+		int answer2 = solution2(n2);
+		System.out.println(answer2);
 	}
 	
 	public static int solution(int n) {
@@ -38,4 +46,24 @@ public class 다음_큰_숫자 {
 		return answer;
 	}
 	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int n) {
+		// Math.pow(2, zeroCount)에 해당
+		int patternPrev = n&-n;
+		
+		// n^(n+patternPrev) 새로나온 1 + 그전까지의 1 + 0들 -> /patternPrev 0제거 -> >> 2 새로 나온 1과 전에 있던 1 두개 제거
+		int patternPost = (n^(n+patternPrev))/patternPrev >> 2;
+		
+		return n + patternPrev + patternPost;
+	}
+	
+	public static int solution2(int n) {
+		int count = Integer.bitCount(n++);
+		
+		while(count!=Integer.bitCount(n)) {
+			n++;
+		}
+		
+		return n;
+	}
 }
