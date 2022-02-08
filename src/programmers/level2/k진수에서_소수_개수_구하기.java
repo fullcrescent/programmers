b4c04d7e6d936e5e8970f2c464fa85cae078f915
fullcrescent <fullcrescent@naver.com>
2022-02-08 22:52:55 +0900

    2022-02-08 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/k진수에서_소수_개수_구하기.java
+++ src/programmers/level2/k진수에서_소수_개수_구하기.java
@@ -1,5 +1,8 @@
 package programmers.level2;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 public class k진수에서_소수_개수_구하기 {
 
 	public static void main(String[] args) {
@@ -7,6 +10,11 @@ public class k진수에서_소수_개수_구하기 {
 		int k = 3;
 		int answer = solution(n, k);
 		System.out.println(answer);
+		
+		int n1 = 437670;
+		int k1 = 3;
+		int answer1 = solution1(n1, k1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(int n, int k) {
@@ -46,6 +54,30 @@ public class k진수에서_소수_개수_구하기 {
 		
 		for(int i=2; i<=Math.sqrt(number); i++) {
 			if(number%i==0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	//다른 사람의 풀이 참고
+	public static int solution1(int n, int k) {
+		String s = Integer.toString(n, k);
+		return (int) Arrays.stream(s.split("0")).filter(
+				p-> !p.isEmpty() 
+					&& !p.equals("1") 
+					&& isPrime1(BigInteger.valueOf(Long.parseLong(p)))
+//					&& (s.contains("0${p}0")
+//		            || s.contains("${p}0")
+//		            || s.contains("0${p}")
+//		            || s.contains(p))		
+				).count();
+	}
+
+	private static boolean isPrime1(BigInteger value) {
+		for(long i=2; i<=Math.sqrt(value.longValue()); i++) {
+			if(value.remainder(BigInteger.valueOf(i))==BigInteger.ZERO) {
 				return false;
 			}
 		}
