bb7a5e4bba5558e9a7b2c9345733b2dba17fdcc1
fullcrescent <fullcrescent@naver.com>
2022-02-09 22:05:19 +0900

    2022-02-09 알고리즘 소스 작성중

---
+++ src/programmers/level2/숫자의_표현.java
@@ -0,0 +1,24 @@
+package programmers.level2;
+
+public class 숫자의_표현 {
+
+	public static void main(String[] args) {
+		int n = 15;
+		int answer = solution(n);
+		System.out.println(answer);
+	}
+
+	public static int solution(int n) {
+		int answer = 0;
+		
+		
+		
+		for(int i=0; i<n+2; i++) {
+			
+		}
+		
+		
+		
+		return 0;
+	}
+}
