d5b137c31a055db39ade4e078d25a1c8be211831
fullcrescent <fullcrescent@naver.com>
2022-02-09 23:07:15 +0900

    2022-02-09 알고리즘 소스

--- src/programmers/level2/숫자의_표현.java
+++ src/programmers/level2/숫자의_표현.java
@@ -1,5 +1,8 @@
 package programmers.level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 숫자의_표현 {
 
 	public static void main(String[] args) {
@@ -9,16 +12,38 @@ public class 숫자의_표현 {
 	}
 
 	public static int solution(int n) {
-		int answer = 0;
+		int answer = 1;
 		
+		List<Integer> list = new ArrayList<>();
 		
-		
-		for(int i=0; i<n+2; i++) {
-			
+		for(int i=1; i<n/2+2; i++) {
+			list.add(i);
 		}
 		
+		int temp = n;
+		int startIndex = 0;
+		int endIndex = 1;
 		
+		while(temp>0) {
+			temp-=endIndex++;
+		}
+		endIndex--;
+		while(endIndex!=n/2+2) {
+			int sum = 0;
+			for(int i=startIndex;i<endIndex;i++) {
+				sum+=list.get(i);
+			}
+			
+			if(sum==n) {
+				answer++;
+				endIndex++;
+			}else if(sum>n) {
+				startIndex++;
+			}else {
+				endIndex++;
+			}
+		}
 		
-		return 0;
+		return answer;
 	}
 }
