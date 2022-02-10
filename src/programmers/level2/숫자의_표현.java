64d90b1465f9f28ae6fd690dd3c88bcbe4004f69
fullcrescent <fullcrescent@naver.com>
2022-02-10 23:34:41 +0900

    2022-02-10 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/숫자의_표현.java
+++ src/programmers/level2/숫자의_표현.java
@@ -6,9 +6,19 @@ import java.util.List;
 public class 숫자의_표현 {
 
 	public static void main(String[] args) {
-		int n = 15;
+		for(int i=2; i<1000; i++) {
+			solution(i*2);
+			System.out.println("======"+i);
+		}
+		
+		
+		int n = 52;
 		int answer = solution(n);
 		System.out.println(answer);
+		
+		int n1 = 52;
+		int answer1 = solution1(n1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(int n) {
@@ -36,6 +46,7 @@ public class 숫자의_표현 {
 			
 			if(sum==n) {
 				answer++;
+				System.out.println(startIndex+1 + ":"+ endIndex);
 				endIndex++;
 			}else if(sum>n) {
 				startIndex++;
@@ -46,4 +57,26 @@ public class 숫자의_표현 {
 		
 		return answer;
 	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int n) {
+		int answer = 0;
+		
+		// endIndex까지의 합 endIndex(endIndex+1)/2,  startIndex까지의 합 startIndex(startIndex+1)/2
+		// endIndex(endIndex+1) - startIndex(startIndex+1)  = 2x
+		// (endIndex^2 - startIndex) - (startIndex^2 - endIndex) = 2x
+		// (endIndex - startIndex)*(endIndex + startIndex + 1) = 2x
+		// (endIndex - startIndex), (endIndex + startIndex + 1) 홀수 짝수 값 나눠가짐
+		// 2x = 홀수 * 짝수
+		// 2x = 홀수 * 2 * 짝수/2
+		// x = 홀수 * 짝수/2
+		// x의 홀수 약수의 갯수가지만큼 만들수 있음.
+		for(int i=1; i<=n; i+=2) {
+			if(n%i==0) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+	
 }
