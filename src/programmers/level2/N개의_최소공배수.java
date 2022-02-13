3aca3e4ca2a0e2b6cda9c7c7472c08236d11779d
fullcrescent <fullcrescent@naver.com>
2022-02-13 21:44:19 +0900

    2022-02-13 알고리즘 소스

---
+++ src/programmers/level2/N개의_최소공배수.java
@@ -0,0 +1,48 @@
+package programmers.level2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class N개의_최소공배수 {
+
+	public static void main(String[] args) {
+		int[] arr = {2,6,8,14};
+		int answer = solution(arr);
+		System.out.println(answer);
+	}
+
+	public static int solution(int[] arr) {
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		for(int temp : arr) {
+			Map<Integer, Integer> tempMap = new HashMap<>();
+			int index = 2;
+			while(temp!=1) {
+				if(temp%index==0) {
+					temp /= index;
+					if(tempMap.get(index)==null) {
+						tempMap.put(index, 1);
+					}else {
+						tempMap.put(index, tempMap.get(index)+1);
+					}
+				}else {
+					index++;
+				}
+			}
+			
+			for(int i : tempMap.keySet()) {
+				if(map.get(i)==null||map.get(i)<tempMap.get(i)) {
+					map.put(i, tempMap.get(i));
+				}
+			}
+		}
+		
+		int answer = 1;
+		
+		for(int i : map.keySet()) {
+			answer *= Math.pow(i, map.get(i));
+		}
+		
+		return answer;
+	}
+}
