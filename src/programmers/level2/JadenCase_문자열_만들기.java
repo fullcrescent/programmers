1dae7fa1ee6b3370d89014a8c997396c2e2c9fc0
fullcrescent <fullcrescent@naver.com>
2022-02-14 23:12:27 +0900

    2022-02-14 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/JadenCase_문자열_만들기.java
+++ src/programmers/level2/JadenCase_문자열_만들기.java
@@ -6,6 +6,10 @@ public class JadenCase_문자열_만들기 {
 		String s = "3people  UnFollowed me  ";
 		String answer = solution(s);
 		System.out.println(answer);
+		
+		String s1 = "3people  UnFollowed me  ";
+		String answer1 = solution1(s1);
+		System.out.println(answer1);
 	}
 	
 	public static String solution(String s) {
@@ -38,4 +42,19 @@ public class JadenCase_문자열_만들기 {
 		
 		return sb.toString();
 	}
+	
+	// 다른 사람의 풀이 참고
+	public static String solution1(String s) {
+		StringBuffer sb = new StringBuffer();
+		boolean[] flag = new boolean[1];
+		flag[0] = true;
+		
+		s.toLowerCase().chars().forEach(i -> {
+			sb.append(flag[0] ? (char)Character.toUpperCase(i) : (char)(i));
+			flag[0] = i==' ' ? true : false;
+		});	
+		
+		return sb.toString();
+	}
+	
 }
