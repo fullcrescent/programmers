4da90fa695b89df7d949f92f661c149d384b256b
fullcrescent <fullcrescent@naver.com>
2022-02-15 21:29:39 +0900

    2022-02-15 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/행렬의_곱셈.java
+++ src/programmers/level2/행렬의_곱셈.java
@@ -11,11 +11,17 @@ public class 행렬의_곱셈 {
 		for(int[] temp : answer) {
 			System.out.println(Arrays.toString(temp));
 		}
+		
+		int[][] arr1_1 = {{1,4}, {3,2}, {4,1}};
+		int[][] arr2_1 = {{3, 3}, {3, 3}};
+		int[][] answer1 = solution1(arr1_1, arr2_1);
+		for(int[] temp : answer1) {
+			System.out.println(Arrays.toString(temp));
+		}
 	}
 	
 	public static int[][] solution(int[][] arr1, int[][] arr2) {
 		int[][] answer = new int[arr1.length][arr2[0].length];
-		
 		
 		for(int i=0; i<arr1.length; i++) {
 			for(int j=0; j<arr2[0].length; j++) {
@@ -24,6 +30,21 @@ public class 행렬의_곱셈 {
 					tempSum += arr1[i][k]*arr2[k][j];
 				}
 				answer[i][j] = tempSum;
+			}
+		}
+			
+		return answer;
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int[][] solution1(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+		
+		for(int i=0; i<arr1.length; i++) {
+			for(int j=0; j<arr2[0].length; j++) {
+				for(int k=0; k<arr1[i].length; k++) {
+					answer[i][j] += arr1[i][k]*arr2[k][j];
+				}
 			}
 		}
 			
