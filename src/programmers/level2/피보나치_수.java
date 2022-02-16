e2eb110360119c4d76ec9407303ac3701da89857
fullcrescent <fullcrescent@naver.com>
2022-02-16 23:05:02 +0900

    2022-02-16 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/피보나치_수.java
+++ src/programmers/level2/피보나치_수.java
@@ -6,6 +6,10 @@ public class 피보나치_수 {
 		int n = 3;
 		int answer = solution(n);
 		System.out.println(answer);
+		
+		int n1 = 3;
+		int answer1 = solution1(n1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(int n) {
@@ -23,13 +27,26 @@ public class 피보나치_수 {
 		}
 		
 		if(f[n-1]==0) {
-			f[n-1] = fibonacci(n-1)%1234567;
+			f[n-1] = fibonacci(n-1)%1234567;		// % 필요없음
 		}
 		
 		if(f[n-2]==0) {
-			f[n-2] = fibonacci(n-2)%1234567;
+			f[n-2] = fibonacci(n-2)%1234567;		// % 필요없음
 		}
 		
-		return f[n-1] + f[n-2];
+		return (f[n-1] + f[n-2])%1234567;
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int n) {
+		int[] dp = new int[n+1];
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for(int i=2; i<=n; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%1234567;
+		}
+		
+		return dp[n];
 	}
 }
