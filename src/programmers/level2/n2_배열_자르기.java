10bb3c6316a871f72fe6e93472afcce811ecc514
fullcrescent <fullcrescent@naver.com>
2022-02-18 22:34:19 +0900

    2022-02-18 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/n2_배열_자르기.java
+++ src/programmers/level2/n2_배열_자르기.java
@@ -3,6 +3,7 @@ package programmers.level2;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.LongStream;
 
 public class n2_배열_자르기 {
 
@@ -12,6 +13,12 @@ public class n2_배열_자르기 {
 		long right = 14;
 		int[] answer = solution(n, left, right);
 		System.out.println(Arrays.toString(answer));
+		
+		int n1 = 4;
+		long left1 = 7;
+		long right1 = 14;
+		int[] answer1 = solution1(n1, left1, right1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static int[] solution(int n, long left, long right) {
@@ -29,5 +36,10 @@ public class n2_배열_자르기 {
 		}
 		
 		return list.stream().mapToInt(Integer::intValue).toArray();
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int[] solution1(int n, long left, long right) {
+		return LongStream.rangeClosed(left, right).mapToInt(i -> (int)Math.max(i/n, i%n)+1).toArray();
 	}
 }
