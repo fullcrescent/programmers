689ef5c8bf212d76c3903fba4fe56f12dda783b8
fullcrescent <fullcrescent@naver.com>
2022-02-25 22:33:28 +0900

    2022-02-25 알고리즘 소스

--- src/programmers/level2/가장_큰_정사각형_찾기.java
+++ src/programmers/level2/가장_큰_정사각형_찾기.java
@@ -9,71 +9,36 @@ public class 가장_큰_정사각형_찾기 {
 	}
 
 	public static int solution(int[][] board) {
-		int iLength = board.length;
-		int jLength = board[0].length;
+		int[][] dp = new int[board.length][board[0].length];
 		
-		int count = 0;
-		int temp =1;
+		for(int i=0; i<board[0].length; i++) {
+			dp[0][i] = board[0][i];
+		}
 		
-		for(int i1=0; i1<iLength; i1=i1+temp) {
-			temp=1;
-			Loop1 :
-			for(int j1=0; j1<jLength; j1++) {
-				if(board[i1][j1]==1) {
-					int iIndex = i1+count+1;
-					int jIndex = j1+count+1;
-					
-					for(int i2=i1; i2<iIndex; i2++) {
-						for(int j2=j1; j2<jIndex; j2++) {
-							if(board[i2][j2]==0) {
-								if(iIndex-i2==jIndex-j2) {
-									j1=jIndex+1;
-									temp=count;
-								}else if(iIndex-i2<jIndex-j2){
-									j1=j2;
-									temp=Math.min(temp, i2-i1+1);
-								}else if(iIndex-i2>jIndex-j2){
-                                	i1=i2;
-                                	j1=j2;
-                                }
-								continue Loop1;
-							}
-						}
+		for(int i=1; i<board.length;i++) {
+			for(int j=0; j<board[i].length;j++) {
+				if(board[i][j]==0) {
+					dp[i][j]=0;
+				}else {
+					if(i-1>-1 && j-1>-1) {
+						int min = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+						dp[i][j] = min+1;
+					}else {
+						dp[i][j] = 1;
 					}
-					
-					Loop2 :
-					while(true) {
-						if(board.length==iIndex || board[0].length==jIndex) break;
-						
-						for(int i2=i1; i2<iIndex; i2++) {
-							if(board[i2][jIndex]==0) {
-								break Loop2;
-							}
-						}
-						
-						for(int j2=j1; j2<jIndex; j2++) {
-							if(board[iIndex][j2]==0) {
-								break Loop2;
-							}
-						}
-
-						if(board[iIndex][jIndex]==0) {
-							break Loop2;
-						}
-
-						iIndex++;
-						jIndex++;
-					}
-					count = iIndex-i1;
-					iLength = board.length-count; 
-					jLength = board[0].length-count;
-					j1=jIndex;
 				}
 			}
 		}
 		
-		int answer = (int) Math.pow(count, 2);
+		int answer =0;
 		
-		return answer;
+		for(int i=0; i<dp.length; i++) {
+			for(int j=0; j<dp[i].length; j++) {
+				answer = Math.max(answer, dp[i][j]);
+			}
+		}
+		
+		return (int) Math.pow(answer, 2);
 	}
+	
 }
