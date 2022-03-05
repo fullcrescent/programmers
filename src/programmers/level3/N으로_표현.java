86d8ad393fc3a8416db7dd2789ec8c2cf5e2c8e2
fullcrescent <fullcrescent@naver.com>
2022-03-05 23:24:47 +0900

    2022-03-05 알고리즘 소스 작성중

--- src/programmers/level3/N으로_표현.java
+++ src/programmers/level3/N으로_표현.java
@@ -9,45 +9,54 @@ public class N으로_표현 {
 
 	public static void main(String[] args) {
 		int N = 5;
-		int number = 550;
+		int number = 12;
 		int answer = solution(N, number);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int N, int number) {
-		int answer = 0;
-		
 		Map<Integer, List<Integer>> map = new HashMap<>();
 		
+		List<Integer> tempList = new ArrayList<>();
+		tempList.add(N);
 		
+		map.put(1, tempList);
+		int answer = 1;
 		
+		while(!tempList.contains(number)) {
+			if(answer++>8) {
+				answer = -1;
+				break;
+			}
+			
+			tempList = new ArrayList<>();
+			
+			for(int i=1; i<=answer/2; i++) {
+				dp(tempList, map.get(answer-i), map.get(i), N, answer);
+			}
+			
+			map.put(answer, tempList);
+		}
 		
-//		while(number>0) {
-//			int quotient = number/N;
-//			int count = 0;
-//			
-//			if(quotient>10) {
-//				while(quotient-->10) {
-//					count++;
-//					quotient /= 10;
-//				}
-//				
-//				for(int i=0; i<=count; i++) {
-//					number -= N*Math.pow(10, i);
-//				}
-//			}
-//			
-//			if(quotient>number) {
-//				
-//			}
-//			int remainder = (int) (N%number);
-//		}
+		return answer;
+	}
+
+	private static void dp(List<Integer> tempList, List<Integer> list1, List<Integer> list2, int N, int length) {
+		for(int temp1 : list1) {
+			for(int temp2 : list2) {
+				tempList.add(temp1+temp2);
+				tempList.add(temp1-temp2);
+				tempList.add(temp1*temp2);
+				if(temp2!=0) {
+					tempList.add(temp1/temp2);
+				}
+			}
+		}
 		
-//		N*N
-		
-		
-		
-		
-		return 0;
+		int temp = 0;
+		for(int i=0; i<length; i++) {
+			temp += N*Math.pow(10, i);
+		}
+		tempList.add(temp);
 	}
 }
