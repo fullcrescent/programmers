a609b0fdb268a60489728a7be640e704e8d44fff
fullcrescent <fullcrescent@naver.com>
2022-03-05 23:04:15 +0900

    2022-03-05 알고리즘 소스 작성중

---
+++ src/programmers/level3/N으로_표현.java
@@ -0,0 +1,53 @@
+package programmers.level3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class N으로_표현 {
+
+	public static void main(String[] args) {
+		int N = 5;
+		int number = 550;
+		int answer = solution(N, number);
+		System.out.println(answer);
+	}
+	
+	public static int solution(int N, int number) {
+		int answer = 0;
+		
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		
+		
+		
+		
+//		while(number>0) {
+//			int quotient = number/N;
+//			int count = 0;
+//			
+//			if(quotient>10) {
+//				while(quotient-->10) {
+//					count++;
+//					quotient /= 10;
+//				}
+//				
+//				for(int i=0; i<=count; i++) {
+//					number -= N*Math.pow(10, i);
+//				}
+//			}
+//			
+//			if(quotient>number) {
+//				
+//			}
+//			int remainder = (int) (N%number);
+//		}
+		
+//		N*N
+		
+		
+		
+		
+		return 0;
+	}
+}
