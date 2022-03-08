b1618fbd71207d58127eef404a4ccaeb78dcfafb
fullcrescent <fullcrescent@naver.com>
2022-03-08 23:08:45 +0900

    2022-03-08 알고리즘 소스 작성중

--- src/programmers/level3/입국심사.java
+++ src/programmers/level3/입국심사.java
@@ -1,24 +1,38 @@
 package programmers.level3;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class 입국심사 {
 
 	public static void main(String[] args) {
-		int n = 6;
+		int n = 35;
 		int[] times = {7, 10};
 		long answer = solution(n, times);
 		System.out.println(answer);
 	}
 	
 	public static long solution(int n, int[] times) {
-		long answer=0;
-		while(n>0) {
-			answer++;
-			for(int time : times) {
-				if(answer%time==0) {
-					n--;
-				}
-			}
+		List<Long> list = new LinkedList<>();
+		
+		Arrays.sort(times);
+		
+		for(int time : times) {
+			list.add((long) time);
 		}
-		return answer;
+		
+		int index=0;
+		
+		while(list.size()<n) {
+			if(list.get(index)==1) {
+				
+			}
+			
+		
+		}
+		
+		
+		return 0;
 	}
 }
