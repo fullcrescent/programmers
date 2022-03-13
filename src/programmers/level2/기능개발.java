b4aa7325ab98fc65392088638c785dfd2ce2509d
fullcrescent <fullcrescent@naver.com>
2022-03-13 23:04:50 +0900

    2022-03-13 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/기능개발.java
+++ src/programmers/level2/기능개발.java
@@ -2,7 +2,11 @@ package programmers.level2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
 
 public class 기능개발 {
@@ -12,6 +16,11 @@ public class 기능개발 {
 		int[] speeds = {1, 30, 5};
 		int[] answer = solution(progresses, speeds);
 		System.out.println(Arrays.toString(answer));
+		
+		int[] progresses1 = {93, 30, 55};
+		int[] speeds1 = {1, 30, 5};
+		int[] answer1 = solution1(progresses1, speeds1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static int[] solution(int[] progresses, int[] speeds) {
@@ -48,5 +57,28 @@ public class 기능개발 {
 		
 		return list.stream().mapToInt(i->i).toArray();
 	}
-
+	
+	// 다른 사람의 풀이 참고
+	public static int[] solution1(int[] progresses, int[] speeds) {
+		Map<Integer, Integer> map = new HashMap<>();
+		int day = 0;
+		
+		for(int i=0; i<progresses.length; i++) {
+			while(progresses[i] + (day*speeds[i])<100) {
+				day++;
+			}
+			
+			map.put(day, map.get(day)==null? 1 : map.get(day)+1);
+		}
+		
+		List<Integer> list = new ArrayList<>(map.keySet());
+		Collections.sort(list);
+		
+		List<Integer> answer = new LinkedList<>();
+		for(int key : list) {
+			answer.add(map.get(key));
+		}
+		
+		return answer.stream().mapToInt(i->i).toArray();
+	}
 }
