5047a9156b7ef34677b2b0d49884e436009a9544
fullcrescent <fullcrescent@naver.com>
2022-03-14 22:31:48 +0900

    2022-03-14 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/뉴스_클러스터링.java
+++ src/programmers/level2/뉴스_클러스터링.java
@@ -10,6 +10,11 @@ public class 뉴스_클러스터링 {
 		String str2 = "AAAA12";
 		int answer = solution(str1, str2);
 		System.out.println(answer);
+		
+		String str11 = "aa1+aa2";
+		String str21 = "AAAA12";
+		int answer1 = solution1(str11, str21);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(String str1, String str2) {
@@ -48,5 +53,40 @@ public class 뉴스_클러스터링 {
 		
 		return union==0 ? 65536 : (int) ((double)intersection/union*65536);
 	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(String str1, String str2) {
+		str1 = str1.toLowerCase();
+		str2 = str2.toLowerCase();
+		
+		Queue<String> queue1 = new LinkedList<>();
+		Queue<String> queue2 = new LinkedList<>();
+		
+		splitPush(str1, queue1);
+		splitPush(str2, queue2);
+		
+		int intersection = 0;
+		int union = queue1.size() + queue2.size();
+		
+		while(!queue2.isEmpty()) {
+			String temp = queue2.poll();
+			
+			if(queue1.contains(temp)) {
+				queue1.remove(temp);
+				intersection++;
+				union--;
+			}
+		}
+		
+		return union==0 ? 65536 : (int) ((double)intersection/union*65536);
+	}
 
+	private static void splitPush(String str, Queue<String> queue) {
+		for(int i=0; i<str.length()-1; i++) {
+			String temp = str.substring(i, i+2);
+			if(temp.matches("([a-zA-Z])*")) {
+				queue.add(temp);
+			}
+		}
+	}
 }
