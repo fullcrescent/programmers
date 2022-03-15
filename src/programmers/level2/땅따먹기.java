582dc95d096744aa6e1b54e2e1a9d1483240c1b0
fullcrescent <fullcrescent@naver.com>
2022-03-15 23:15:11 +0900

    2022-03-15 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/땅따먹기.java
+++ src/programmers/level2/땅따먹기.java
@@ -6,6 +6,10 @@ public class 땅따먹기 {
 		int[][] land = {{1,2,3,1003},{5,6,7,1507},{4,3,2,1004}};
 		int answer = solution(land);
 		System.out.println(answer);
+		
+		int[][] land1 = {{1,2,3,1003},{5,6,7,1507},{4,3,2,1004}};
+		int answer1 = solution1(land1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(int[][] land) {
@@ -34,4 +38,15 @@ public class 땅따먹기 {
 		return answer;
 	}
 	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int[][] land) {
+		for(int i=1; i<land.length; i++) {
+			land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));  
+			land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+			land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+			land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+		}
+
+		return Math.max(land[land.length-1][0], Math.max(land[land.length-1][1], Math.max(land[land.length-1][2], land[land.length-1][3])));
+	}
 }
