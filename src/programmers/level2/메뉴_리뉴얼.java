308401f9426d0dd4c89ba3ce67e073fa27bb6674
fullcrescent <fullcrescent@naver.com>
2022-03-16 22:53:33 +0900

    2022-03-16 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/메뉴_리뉴얼.java
+++ src/programmers/level2/메뉴_리뉴얼.java
@@ -7,14 +7,21 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class 메뉴_리뉴얼 {
 
 	public static void main(String[] args) {
-		String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-		int[] course = {2,3,4};
+		String[] orders = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
+		int[] course = {2,3,5};
 		String[] answer = solution(orders, course);
 		System.out.println(Arrays.toString(answer));
+		
+		String[] orders1 = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
+		int[] course1 = {2,3,5};
+		String[] answer1 = solution1(orders1, course1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static String[] solution(String[] orders, int[] course) {
@@ -41,8 +48,8 @@ public class 메뉴_리뉴얼 {
 		return list.toArray(new String[0]);
 	}
 	
-	static Map<Integer, Integer> mapMaxCount = new HashMap<>(); 
-	static Map<String, Integer> mapCount = new HashMap<>(); 
+	static Map<Integer, Integer> mapMaxCount = new HashMap<>(); 	// 각 길이별 최대 등장 횟수
+	static Map<String, Integer> mapCount = new HashMap<>(); 		// 모든 문자 등장 횟수
 			
 	private static void dfs(String order, int course, int index, String temp, int count) {
 		for(int i=index; i<order.length(); i++) {
@@ -69,5 +76,50 @@ public class 메뉴_리뉴얼 {
 		char[] charArray = (s1+s2).toCharArray();
 		Arrays.sort(charArray);
 		return new String(charArray);
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static String[] solution1(String[] orders, int[] course) {
+		Queue<String> queue = new PriorityQueue<>();
+		
+		for(int i=0; i<course.length; i++) {
+			max = 0;
+			map = new HashMap<>();
+			for(int j=0; j<orders.length; j++) {
+				dfs1(orders[j], course[i], "", 0, 0);
+			}
+			for(String key : map.keySet()) {
+				if(map.get(key)==max&&max>1) {
+					queue.add(key);
+				}
+			}
+		}
+		
+//		return Arrays.stream(queue.toArray()).toArray(String[]::new);	우선순위 큐의 경우 poll 할 때 위치를 찾아가서 값을 반환하므로 toArray로 받으면 배열순서 어긋남.  
+		
+		String[] answer = new String[queue.size()];
+		int index = 0;
+		while(!queue.isEmpty()) {
+			answer[index++] = queue.poll();
+		}
+		
+		return answer;
+	}
+	
+	static int max;
+	static Map<String, Integer> map; 
+	
+	private static void dfs1(String order, int course, String temp, int index, int count) {
+		if(course==count) {
+			char[] key = temp.toCharArray();
+			Arrays.sort(key);
+			map.put(new String(key), map.getOrDefault(new String(key), 0)+1);
+			max = Math.max(max, map.get(new String(key)));
+			return;
+		}
+		
+		for(int i=index; i<order.length(); i++) {
+			dfs1(order, course, temp+String.valueOf(order.charAt(i)), i+1, count+1);
+		}
 	}
 }
