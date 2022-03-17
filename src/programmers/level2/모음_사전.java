06ee67207cac616a178197d790e6e3b4ebc2a9b5
fullcrescent <fullcrescent@naver.com>
2022-03-17 22:34:16 +0900

    2022-03-17 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/모음_사전.java
+++ src/programmers/level2/모음_사전.java
@@ -6,6 +6,10 @@ public class 모음_사전 {
 		String word = "AAAAE";
 		int answer = solution(word);
 		System.out.println(answer);
+		
+		String word1 = "AAAAE";
+		int answer1 = solution1(word1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(String word) {
@@ -29,6 +33,18 @@ public class 모음_사전 {
 			answer++;
 		}
         
+        return answer;
+    }
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(String word) {
+		int answer = 0;
+		int count = 3905;
+		
+		for(char temp : word.toCharArray()) {
+			answer += "AEIOU".indexOf(temp)*(count/=5)+1;
+		}
+		
         return answer;
     }
 }
