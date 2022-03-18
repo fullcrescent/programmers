30f636626a6fbaeba6b33f430c748dc8bbefd49c
fullcrescent <fullcrescent@naver.com>
2022-03-18 22:24:22 +0900

    2022-03-18 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/방금그곡.java
+++ src/programmers/level2/방금그곡.java
@@ -7,6 +7,11 @@ public class 방금그곡 {
 		String[] musicinfos = {"12:00,12:03,HELLO,ABC", "13:00,13:05,WORLD,ABC#DEF"};
 		String answer = solution(m, musicinfos);
 		System.out.println(answer);
+		
+		String m1 = "ABC";
+		String[] musicinfos1 = {"12:00,12:03,HELLO,ABC", "13:00,13:05,WORLD,ABC#DEF"};
+		String answer1 = solution1(m1, musicinfos1);
+		System.out.println(answer1);
 	}
 	
     public static String solution(String m, String[] musicinfos) {
@@ -57,4 +62,42 @@ public class 방금그곡 {
     	
     	return answer;
     }
+    
+    // 다른 사람의 풀이 참고
+    public static String solution1(String m, String[] musicinfos) {
+    	String answer = "(None)";
+    	int answerTime = 0;
+    	
+    	m = replace(m);
+    	
+    	StringBuffer sb = new StringBuffer();
+
+    	for(String musicinfo : musicinfos) {
+    		String[] info = musicinfo.split(",");
+    		
+    		int playTime = (Integer.parseInt(info[1].split(":")[0])- Integer.parseInt(info[0].split(":")[0]))*60
+    				+(Integer.parseInt(info[1].split(":")[1])- Integer.parseInt(info[0].split(":")[1]));
+    		
+    		if(playTime>answerTime) {
+    			info[3] = replace(info[3]);
+    			
+    			for(int i=0; i<playTime; i++) {
+    				sb.append(info[3].charAt(i%(info[3].length())));
+    			}
+    			
+    			if(sb.toString().indexOf(m)>=0) {
+    				answer = info[2];
+    				answerTime = playTime;
+    			}
+    			
+    			sb.delete(0, sb.length());
+    		}
+    	}
+    	
+    	return answer;
+    }
+
+	private static String replace(String s) {
+		return s.replace("C#", "V").replace("D#", "W").replace("F#", "X").replace("G#", "Y").replace("A#", "Z");
+	}
 }
