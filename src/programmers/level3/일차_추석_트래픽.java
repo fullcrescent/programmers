ea611ee36c4f2405399e8d6816f96774079ed092
fullcrescent <fullcrescent@naver.com>
2022-03-22 23:00:03 +0900

    2022-03-22 알고리즘 소스

--- src/programmers/level3/일차_추석_트래픽.java
+++ src/programmers/level3/일차_추석_트래픽.java
@@ -1,9 +1,5 @@
 package programmers.level3;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +7,7 @@ public class 일차_추석_트래픽 {
 
 	public static void main(String[] args) {
 		String[] lines = {
-				"2016-09-15 23:59:59.999 0.001s", "2016-09-15 23:59:59.999 0.001s", "2016-09-15 23:59:59.999 0.001s"
+				"2016-09-15 20:59:59.000 0.001s","2016-09-15 20:59:57.421 0.351s", "2016-09-15 20:59:58.233 1.181s", "2016-09-15 20:59:58.299 0.8s", "2016-09-15 20:59:58.688 1.041s", "2016-09-15 20:59:59.591 1.412s", "2016-09-15 21:00:00.464 1.466s", "2016-09-15 21:00:00.741 1.581s", "2016-09-15 21:00:00.748 2.31s", "2016-09-15 21:00:00.966 0.381s", "2016-09-15 21:00:02.066 2.62s"
 				};
 		int answer = solution(lines);
 		System.out.println(answer);
@@ -19,31 +15,30 @@ public class 일차_추석_트래픽 {
 	
 	public static int solution(String[] lines) {
 		int answer = 0;
-		Map<Integer, long[]> map = new HashMap<>();
+		Map<Integer, int[]> map = new HashMap<>();
 		
 		int index=0;
 		for(String line : lines) {
 			String[] temp = line.split(" ");
+			int[] array = new int[2];
 			
-			Calendar cal = Calendar.getInstance();
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-			Date date = null;
-			try {
-				date = sdf.parse(temp[0] + " " + temp[1]);
-			} catch (ParseException e) {
-				e.printStackTrace();
+			array[1] = Integer.parseInt(temp[1].replaceAll("[:.]", ""));
+			
+			int time = (int)(Double.parseDouble(temp[2].replaceAll("[a-z]", ""))*1000);
+			
+			if(Integer.parseInt(temp[1].replaceAll("[:.]", "").substring(2))<time) {
+				array[0] = array[1] - time + 1 - 4040000;
+			}else if(Integer.parseInt(temp[1].replaceAll("[:.]", "").substring(4))<time) {
+				array[0] = array[1] - time + 1 - 40000;
+			}else {
+				array[0] = array[1] - time + 1;
 			}
-			
-			cal.setTime(date);
-			long[] array = new long[2];
-			array[1] = cal.getTimeInMillis();
-			array[0] = cal.getTimeInMillis() - (long)(Double.parseDouble(temp[2].replaceAll("[a-z]", ""))*1000) + 1;
 		
 			map.put(index++, array);
 		}
 		
 		for(int key : map.keySet()) {
-			long[] temp = map.get(key);
+			int[] temp = map.get(key);
 			
 			answer = Math.max(answer, checkTime(map, temp));
 		}
@@ -51,20 +46,22 @@ public class 일차_추석_트래픽 {
 		return answer;
 	}
 
-	private static int checkTime(Map<Integer, long[]> map, long[] value) {
+	private static int checkTime(Map<Integer, int[]> map, int[] value) {
 		int start = 0;
 		int end = 0;
 		
 		for(int key : map.keySet()) {
-			long[] temp = map.get(key);
-			if((temp[0]<=value[0]&&value[0]<=temp[1]) || (temp[0]<=value[0]+999&&value[0]+999<=temp[1])) {
+			int[] temp = map.get(key);
+			if((temp[0]<=value[0]&&value[0]<=temp[1]) || (temp[0]<=value[0]+999&&value[0]+999<=temp[1]) 
+					|| (value[0]<=temp[0]&&value[0]<=temp[1]&&temp[0]<=value[0]+999&&temp[1]<=value[0]+999)) {
 				start++;
 			}
 		}
 		
 		for(int key : map.keySet()) {
-			long[] temp = map.get(key);
-			if((temp[0]<=value[1]&&value[1]<=temp[1]) || (temp[0]<=value[1]+999&&value[1]+999<=temp[1])) {
+			int[] temp = map.get(key);
+			if((temp[0]<=value[1]&&value[1]<=temp[1]) || (temp[0]<=value[1]+999&&value[1]+999<=temp[1])
+					|| (value[1]<=temp[0]&&value[1]<=temp[1]&&temp[0]<=value[1]+999&&temp[1]<=value[1]+999)) {
 				end++;
 			}
 		}
