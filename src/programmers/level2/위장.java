04f147d5bded1145620ff37fbc2be2cca16feff4
fullcrescent <fullcrescent@naver.com>
2022-03-27 22:21:13 +0900

    2022-03-27 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/위장.java
+++ src/programmers/level2/위장.java
@@ -1,10 +1,12 @@
 package programmers.level2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import static java.util.stream.Collectors.*;
 
 public class 위장 {
 
@@ -12,6 +14,10 @@ public class 위장 {
 		String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
 		int answer = solution(clothes);
 		System.out.println(answer);
+		
+		String[][] clothes1 = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+		int answer1 = solution1(clothes1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(String[][] clothes) {
@@ -44,4 +50,12 @@ public class 위장 {
 		return answer;
 	}
 
+	// 다른 사람의 풀이 참고
+	public static int solution1(String[][] clothes) {
+		return Arrays.stream(clothes)
+				.collect(groupingBy(i -> i[1], mapping(i -> i[0], counting())))
+				.values()
+				.stream()
+				.collect(reducing(1L, (x,y) -> x*(y+1))).intValue() -1;
+	}
 }
