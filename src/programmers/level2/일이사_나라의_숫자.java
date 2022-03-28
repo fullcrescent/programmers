fc24f5b4ead8c572bfe73e22959d0d11797c27c2
fullcrescent <fullcrescent@naver.com>
2022-03-28 22:13:21 +0900

    2022-03-28 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/일이사_나라의_숫자.java
+++ src/programmers/level2/일이사_나라의_숫자.java
@@ -3,32 +3,14 @@ package programmers.level2;
 
 public class 일이사_나라의_숫자 {
 	public static void main(String args[]) {
-		int n1 = 1;
-		int n2 = 2;
-		int n3 = 3;
-		int n4 = 4;
-		String answer1 = solution(n1);
-		String answer2 = solution(n2);
-		String answer3 = solution(n3);
-		String answer4 = solution(n4);
-		System.out.println(answer1);
-		System.out.println(answer2);
-		System.out.println(answer3);
-		System.out.println(answer4);
+		int n = 15;
+		String answer = solution(n);
+		System.out.println(answer);
 		
+		int n1 = 15;
+		String answer1 = solution1(n1);
+		System.out.println(answer1);
 	}
-	
-	public static String solution1(int n) {
-		String[] num = {"4", "1", "2"};
-        StringBuilder answer = new StringBuilder();
-
-        while(n>0){
-            answer = answer.insert(0, num[n%3]);
-            n = (n-1)/3;
-        }
-
-        return answer.toString().replace("0","4");
-    }
 	
 	public static String solution(int n) {
         StringBuilder answer = new StringBuilder();
@@ -40,5 +22,18 @@ public class 일이사_나라의_숫자 {
         }
 
         return answer.reverse().toString().replace("0","4");
+    }
+	
+	// 다른 사람의 풀이 참고
+	public static String solution1(int n) {
+		String[] num = {"4", "1", "2"};
+        StringBuilder answer = new StringBuilder();
+
+        while(n>0){
+            answer = answer.insert(0, num[n%3]);
+            n = (n-1)/3;
+        }
+
+        return answer.toString();
     }
 }
