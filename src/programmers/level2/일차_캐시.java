c3ca5cf1025311aa2a6b37623c2221968c02ded2
fullcrescent <fullcrescent@naver.com>
2022-03-29 21:56:53 +0900

    2022-03-29 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/일차_캐시.java
+++ src/programmers/level2/일차_캐시.java
@@ -2,6 +2,7 @@ package programmers.level2;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class 일차_캐시 {
 	public static void main(String[] args) {
@@ -9,6 +10,11 @@ public class 일차_캐시 {
 		String[] cities = {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
 		int answer = solution(cachSize, cities);
 		System.out.println(answer);
+		
+		int cachSize1 = 3;
+		String[] cities1 = {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
+		int answer1 = solution1(cachSize1, cities1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(int cacheSize, String[] cities) {
@@ -36,5 +42,31 @@ public class 일차_캐시 {
 		
         return answer;
     }
-
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int cacheSize, String[] cities) {
+		Stack<String> stack = new Stack<>();
+		
+		int answer = 0 ;
+		int cacheHit = 1;
+		int cacheMiss = 5;
+		
+		for(String temp : cities) {
+			temp = temp.toLowerCase();
+			
+			if(stack.remove(temp)) {
+				answer += cacheHit;
+				stack.add(temp);
+			}else {
+				answer += cacheMiss;
+				stack.add(temp);
+				
+				if(stack.size()>cacheSize) {
+					stack.remove(0);
+				}
+			}
+		}
+		
+        return answer;
+    }
 }
