4c57b02fa6bd85820c11a63e731f235eef0b8b87
fullcrescent <fullcrescent@naver.com>
2022-03-31 23:24:53 +0900

    2022-03-31 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/전화번호_목록.java
+++ src/programmers/level2/전화번호_목록.java
@@ -8,6 +8,10 @@ public class 전화번호_목록 {
 		String[] phone_book = {"119", "97674223", "1195524421"};
 		boolean answer = solution(phone_book);
 		System.out.println(answer);
+		
+		String[] phone_book1 = {"119", "97674223", "1195524421"};
+		boolean answer1 = solution1(phone_book1);
+		System.out.println(answer1);
 	}
 	
 	public static boolean solution(String[] phone_book) {
@@ -16,6 +20,18 @@ public class 전화번호_목록 {
 
 		for(int i=0; i<list.size()-1; i++){
 			if(list.get(i+1).startsWith(list.get(i))) {
+				return false;
+			}
+		}
+
+		return true;
+    }
+	
+	// 다른 사람의 풀이 참고
+	public static boolean solution1(String[] phone_book) {
+		Arrays.sort(phone_book);
+		for(int i=0; i<phone_book.length-1; i++){
+			if(phone_book[i+1].startsWith(phone_book[i])) {
 				return false;
 			}
 		}
