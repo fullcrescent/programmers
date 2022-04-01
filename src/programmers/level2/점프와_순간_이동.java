0892d539cbfdd9785c9d8ae5367839e4bb09bec9
fullcrescent <fullcrescent@naver.com>
2022-04-01 20:49:56 +0900

    2022-04-01 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/점프와_순간_이동.java
+++ src/programmers/level2/점프와_순간_이동.java
@@ -21,4 +21,20 @@ public class 점프와_순간_이동 {
 		
 		return answer;
 	}
+	
+	// 다른 사람의 풀이 참고 - 변경X
+	public static int solution1(int n) {
+		int answer = 1;
+		
+		while(n!=1) {
+			if(n%2==0) {
+				n /= 2;
+			}else {
+				n--;
+				answer++;
+			}
+		}
+		
+		return answer;
+	}
 }
