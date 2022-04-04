82b16f66de3f56a2eed8da38f4cf4362ac6f8be1
fullcrescent <fullcrescent@naver.com>
2022-04-04 21:27:58 +0900

    2022-04-04 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/튜플.java
+++ src/programmers/level2/튜플.java
@@ -2,8 +2,10 @@ package programmers.level2;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class 튜플 {
 
@@ -11,6 +13,10 @@ public class 튜플 {
 		String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
 		int[] answer = solution(s);
 		System.out.println(Arrays.toString(answer));
+		
+		String s1 = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
+		int[] answer1 = solution1(s1);
+		System.out.println(Arrays.toString(answer1));
 	}
 
 	public static int[] solution(String s) {
@@ -38,5 +44,24 @@ public class 튜플 {
 		
 		return answer;
 	}
+	
+	// 다른 사람의 풀이 참고
+	public static int[] solution1(String s) {
+		Map<String, Integer> map = new HashMap<>();
 
+		String[] numbers = s.replace("{", "").replace("}", "").split(",");
+		
+		for(String n : numbers) {
+			map.put(n, map.getOrDefault(n, 0)+1);
+		}
+		
+		int length = map.size();
+		int[] answer = new int[length];
+		
+		for(String key : map.keySet()) {
+			answer[length-map.get(key)] = Integer.parseInt(key);
+		}
+		
+		return answer;
+	}
 }
