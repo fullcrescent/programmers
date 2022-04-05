36595f56e0e80874acbf5e8634274dec773f5613
fullcrescent <fullcrescent@naver.com>
2022-04-05 22:31:05 +0900

    2022-04-05 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/파일명_정렬.java
+++ src/programmers/level2/파일명_정렬.java
@@ -11,6 +11,10 @@ public class 파일명_정렬 {
 		String[] files = {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
 		String[] answer = solution(files);
 		System.out.println(Arrays.toString(answer));
+		
+		String[] files1 = {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
+		String[] answer1 = solution1(files1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static String[] solution(String[] files) {
@@ -43,5 +47,34 @@ public class 파일명_정렬 {
 		
 		return files;
 	}
-
+	
+	// 다른 사람의 풀이 참고
+	public static String[] solution1(String[] files) {
+		Arrays.sort(files, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				s1 = s1.toLowerCase();
+				s2 = s2.toLowerCase();
+				
+				int end1 = 0;
+				int end2 = 0;
+				
+				while(end1<s1.length() && !Character.isDigit(s1.charAt(end1))) end1++;
+				while(end2<s2.length() && !Character.isDigit(s2.charAt(end2))) end2++;
+				
+				int value = s1.substring(0,end1).compareTo(s2.substring(0, end2));
+				
+				if(value!=0) return value;
+				
+				int start1 = end1;
+				int start2 = end2;
+				
+				while(end1<s1.length() && Character.isDigit(s1.charAt(end1))) end1++;
+				while(end2<s2.length() && Character.isDigit(s2.charAt(end2))) end2++;
+				
+				return Integer.parseInt(s1.substring(start1, end1)) - Integer.parseInt(s2.substring(start2, end2));
+			}
+		});
+		
+		return files;
+	}
 }
