2f8b1060646ee683d4fdb5b886497122bb97fee8
fullcrescent <fullcrescent@naver.com>
2022-04-06 20:38:29 +0900

    2022-04-06 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/짝지어_제거하기.java
+++ src/programmers/level2/짝지어_제거하기.java
@@ -8,6 +8,10 @@ public class 짝지어_제거하기 {
 		String s = "baabaa";
 		int answer = solution(s);
 		System.out.println(answer);
+		
+		String s1 = "baabaa";
+		int answer1 = solution1(s1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(String s) {
@@ -22,5 +26,18 @@ public class 짝지어_제거하기 {
 		
 		return stack.isEmpty() ? 1:0;
 	}
-
+	
+	// 다른 사람의 풀이 참고 - 변경X
+	public static int solution1(String s) {
+		Stack<Character> stack = new Stack<>();
+		for(Character data : s.toCharArray()) {
+			if(!stack.isEmpty() && stack.peek().equals(data)) {
+				stack.pop();
+			}else {
+				stack.add(data);
+			}
+		}
+		
+		return stack.isEmpty() ? 1:0;
+	}
 }
