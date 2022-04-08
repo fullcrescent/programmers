f499f2f8a1b2fe0fa533e11353c088b52e6050de
fullcrescent <fullcrescent@naver.com>
2022-04-08 22:20:34 +0900

    2022-04-08 알고리즘 소스

--- src/programmers/level3/단속카메라.java
+++ src/programmers/level3/단속카메라.java
@@ -1,11 +1,7 @@
 package programmers.level3;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class 단속카메라 {
 
@@ -16,27 +12,32 @@ public class 단속카메라 {
 	}
 	
 	public static int solution(int[][] routes) {
-		Map<Integer, Integer> map = new HashMap<>();
-		
-		for(int[] temp : routes) {
-			for(int i=temp[0]; i<=temp[1]; i++) {
-				map.put(i, map.getOrDefault(i, 0)+1);
-			}
-		}
-		
-		List<Integer> list = new ArrayList<>(map.keySet());
-		
-		Collections.sort(list, new Comparator<Integer>(){
-			public int compare(Integer s1, Integer s2) {
-				return -Integer.compare(map.get(s1), map.get(s2));
+		Arrays.sort(routes, new Comparator<int[]>(){
+			public int compare(int[] i1, int[] i2) {
+				return Integer.compare(i1[0], i2[0]);
 			}
 		});
 		
-		int answer = 0;
-		int count = routes.length;
+//		int start = 0;
+		int end = 0;
 		
+		int answer = routes.length;
 		
+		for(int i=0; i<routes.length-1; i++) {
+//			start = routes[i][0];
+			end = routes[i][1];
+			
+			while(++i<routes.length && routes[i][0]<=end) {
+//				start = routes[i][0];
+				if(routes[i][1]<=end) {
+					end = routes[i][1];
+				}
+				answer--;
+			}
+			
+			i--;
+		}
 		
-		return 0;
+		return answer;
 	}
 }
