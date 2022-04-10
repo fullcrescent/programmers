5435283df69bbb3c9e881cf29e468f5ce90e3867
fullcrescent <fullcrescent@naver.com>
2022-04-10 22:59:34 +0900

    2022-04-10 알고리즘 소스

--- src/programmers/level3/등굣길.java
+++ src/programmers/level3/등굣길.java
@@ -3,30 +3,39 @@ package programmers.level3;
 public class 등굣길 {
 
 	public static void main(String[] args) {
-		int m = 4;
-		int n = 3;
+		int m = 99;
+		int n = 99;
 		int[][] puddles = {{2, 2}};
 		int answer = solution(m, n, puddles);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int m, int n, int[][] puddles) {
-		return dfs(0, 0, m-1, n-1, puddles);
-	}
-
-	private static int dfs(int x, int y, int endX, int endY, int[][] puddles) {
-		if(x==endX && y==endY) {
-			return 1;
-		}else if(x>endX || y>endY) {
-			return 0;
-		}
+		long dp[][] = new long[n][m];
 		
 		for(int i=0; i<puddles.length; i++) {
-			if(x==puddles[i][0] && y==puddles[i][1]) {
-				return 0;
+			dp[puddles[i][1]-1][puddles[i][0]-1] = -1;
+		}
+		
+		for(int i=m-1; i>-1; i--) {
+			if(dp[n-1][i]==-1) break;
+			dp[n-1][i] = 1;
+		}
+		
+		for(int i=n-1; i>-1; i--) {
+			if(dp[i][m-1]==-1) break;
+			dp[i][m-1] = 1;
+		}
+		
+		for(int i = n-2; i>-1; i--) {
+			for(int j=m-2; j>-1; j--) {
+				if(dp[i][j]==-1) continue;
+				long bottom = dp[i+1][j]==-1?0:dp[i+1][j];
+				long right = dp[i][j+1]==-1?0:dp[i][j+1];
+				dp[i][j] = (bottom + right)%1000000007;
 			}
 		}
 		
-		return dfs(x+1, y, endX, endY, puddles) + dfs(x, y+1, endX, endY, puddles); 
+		return (int)dp[0][0];
 	}
 }
