50c1bc0ad36bb11e3ab700c713df635c78d2a2b4
fullcrescent <fullcrescent@naver.com>
2022-04-15 22:31:31 +0900

    2022-04-15 알고리즘 소스 작성중

--- src/programmers/level3/디스크_컨트롤러.java
+++ src/programmers/level3/디스크_컨트롤러.java
@@ -9,7 +9,7 @@ import java.util.Queue;
 public class 디스크_컨트롤러 {
 
 	public static void main(String[] args) {
-		int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+		int[][] jobs = {{0, 3}, {10, 20}, {2, 6}};
 		int answer = solution(jobs);
 		System.out.println(answer);
 	}
@@ -28,30 +28,32 @@ public class 디스크_컨트롤러 {
 		
 		Queue<int[]> queue = new PriorityQueue<>((i1, i2) -> Integer.compare(i1[1], i2[1]));
 		
-		list.get(0)[2] = list.get(0)[1]-list.get(0)[0];
-		queue.add(list.get(0));
-		list.remove(0);
 		
-		
-		while(!queue.isEmpty()) {
-			int[] value = queue.poll();
-			answer += value[2]-value[0];
+		while(!list.isEmpty()) {
+			list.get(0)[2] = list.get(0)[1];
+			queue.add(list.get(0));
+			list.remove(0);
 			
-			Iterator<int[]> iter = list.iterator();
-			
-			while(iter.hasNext()){
-				int[] temp = iter.next();
+			while(!queue.isEmpty()) {
+				int[] value = queue.poll();
+				answer += value[2]-value[0];
 				
-				if(temp[0]<=value[1]) {
-					queue.add(temp);
-					iter.remove();
+				Iterator<int[]> iter = list.iterator();
+				
+				while(iter.hasNext()){
+					int[] temp = iter.next();
+					
+					if(temp[0]<=value[1]) {
+						queue.add(temp);
+						iter.remove();
+					}
 				}
-			}
-			
-			iter = queue.iterator();
-			while(iter.hasNext()){
-				int[] temp = iter.next();
-				temp[2] = temp[1]+value[2];
+				
+				iter = queue.iterator();
+				while(iter.hasNext()){
+					int[] temp = iter.next();
+					temp[2] = temp[1]+value[2];
+				}
 			}
 		}
 		
