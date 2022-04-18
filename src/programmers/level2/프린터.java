b232c4e100df20123213a4fd1866b39daabc2485
fullcrescent <fullcrescent@naver.com>
2022-04-18 22:32:26 +0900

    2022-04-18 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/프린터.java
+++ src/programmers/level2/프린터.java
@@ -10,9 +10,47 @@ public class 프린터 {
 		int location = 2;
 		int answer = solution(priorities, location);
 		System.out.println(answer);
+		
+		int[] priorities1 = {2, 1, 3, 2};
+		int location1 = 2;
+		int answer1 = solution1(priorities1, location1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(int[] priorities, int location) {
+		int answer = 0;
+		int work;
+		
+		Queue<Integer> queue = new LinkedList<>();
+		
+		for(int i=0; i<priorities.length; i++) {
+			queue.add(i);
+		}
+		
+		while(!queue.isEmpty()) {
+			work = queue.poll();
+			
+			for(int i=0; i<priorities.length; i++) {
+				if(priorities[work]<priorities[i]) {
+					queue.add(work);
+					break;
+				}
+				
+				if(i==priorities.length-1) {
+					answer++;
+					if(work==location) {
+						return answer;
+					}
+					priorities[work] = 0;
+				}
+			}
+		}
+		
+		return -1;
+	}
+	
+	// 다른 사람의 풀이 참고 - 변경X
+	public static int solution1(int[] priorities, int location) {
 		int answer = 0;
 		int work;
 		
