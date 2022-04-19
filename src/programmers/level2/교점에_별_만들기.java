597a349be49ec2dfba25d956bde65c70cf61c174
fullcrescent <fullcrescent@naver.com>
2022-04-19 22:18:31 +0900

    2022-04-19 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/교점에_별_만들기.java
+++ src/programmers/level2/교점에_별_만들기.java
@@ -10,6 +10,10 @@ public class 교점에_별_만들기 {
 		int[][] line = {{1, -1, 0}, {2, -1, 0}};
 		String[] answer = solution(line);
 		System.out.println(Arrays.toString(answer));
+		
+		int[][] line1 = {{1, -1, 0}, {2, -1, 0}};
+		String[] answer1 = solution1(line1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static String[] solution(int[][] line) {
@@ -92,6 +96,78 @@ public class 교점에_별_만들기 {
 			this.y = y;
 		}
 	}
+	
+	// 다른 사람의 풀이 참고 - 변경X
+	public static String[] solution1(int[][] line) {
+		Queue<Star> queue = new LinkedList<>();
+		
+		long maxX = 0;
+		long minX = 0;
+		
+		long maxY = 0;
+		long minY = 0;  
+		
+		boolean flag = false;
+		
+		for(int i=0; i<line.length-1; i++) {
+			for(int j=i+1; j<line.length; j++) {
+				long A = line[i][0];
+				long B = line[i][1];
+				long C = line[i][2];
+				
+				long D = line[j][0];
+				long E = line[j][1];
+				long F = line[j][2];
+				
+				long numeratorX = B*F-C*E;
+				long numeratorY = C*D-A*F;
+				long denominator = A*E-B*D;
+				
+				if(denominator==0) {
+					continue;
+				}
+				
+				if(numeratorX%denominator==0 && numeratorY%denominator==0) {
+					long X = numeratorX/denominator;
+					long Y = numeratorY/denominator;
+					
+					queue.add(new Star(X, Y));
+					
+					if(flag) {
+						maxX = Math.max(maxX, X);
+						minX = Math.min(minX, X);
+						maxY = Math.max(maxY, Y);
+						minY = Math.min(minY, Y);
+					}else{
+						flag = true;
+						maxX = minX = X;
+						maxY = minY = Y;
+					}
+					
+				}
+			}
+		}
+		
+		String[] answer = new String[(int) (maxY-minY+1)];
+
+		for(int i=0; i<=maxY-minY; i++) {
+			String temp = "";
+			for(int j=0; j<=maxX-minX; j++) {
+				temp += ".";
+			}
+			answer[i] = temp;
+		}
+        
+		while(!queue.isEmpty()) {
+			Star temp = queue.poll();
+			long replaceX = temp.x - minX;
+			long replaceY = maxY - temp.y;
+			
+			answer[(int) replaceY] = answer[(int) replaceY].substring(0, (int) replaceX) + "*" + answer[(int) replaceY].substring((int) (replaceX)+1);
+		}
+		
+        return answer;
+    }
 }
 
 
