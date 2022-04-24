cd937078596aeb077e93e2700a5318fa2051ec79
fullcrescent <fullcrescent@naver.com>
2022-04-24 22:23:20 +0900

    2022-04-24 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/주차_요금_계산.java
+++ src/programmers/level2/주차_요금_계산.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class 주차_요금_계산 {
 
@@ -16,6 +17,13 @@ public class 주차_요금_계산 {
 				"19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
 		int[] answer = solution(fees, records);
 		System.out.println(Arrays.toString(answer));
+		
+		int[] fees1 = {180, 5000, 10, 600};
+		String[] records1 = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", 
+				"07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", 
+				"19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+		int[] answer1 = solution1(fees1, records1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static int[] solution(int[] fees, String[] records) {
@@ -72,5 +80,42 @@ public class 주차_요금_계산 {
 		}
 		
 		return answer.stream().mapToInt(Integer::intValue).toArray();
+	}
+	
+	// 다른 사람의 풀이 참고 
+	public static int[] solution1(int[] fees, String[] records) {
+		Map<String, Integer> map = new TreeMap<>();
+		
+		for(String record : records) {
+			String[] temp = record.split(" ");
+			
+			int value = temp[2].equals("IN") ? -1 : 1;
+			value *= timeToInt(temp[0]);
+			map.put(temp[1], map.getOrDefault(temp[1], 0)+value);
+		}
+		
+		int index = 0;
+		int[] answer = new int[map.size()];
+		
+		for(int time : map.values()) {
+			if(time<1) time += 1439;
+			
+			time -= fees[0];
+			
+			int cost = fees[1];
+			
+			if(time>0) {
+				cost += (time%fees[2]==0 ? time/fees[2] : time/fees[2]+1)*fees[3];
+			}
+			
+			answer[index++] = cost; 
+		}
+		
+		return answer;
+	}
+
+	private static int timeToInt(String s) {
+		String[] temp = s.split(":");
+		return Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);
 	}
 }
