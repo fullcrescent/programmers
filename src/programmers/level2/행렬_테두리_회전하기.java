0652c779e3ced18e4f5fd5d6395068a2f3a435d2
fullcrescent <fullcrescent@naver.com>
2022-04-28 22:54:46 +0900

    2022-04-28 알고리즘 소스 및 다른 사람의 풀이 참고 작성중

--- src/programmers/level2/행렬_테두리_회전하기.java
+++ src/programmers/level2/행렬_테두리_회전하기.java
@@ -10,6 +10,12 @@ public class 행렬_테두리_회전하기 {
 		int[][] queries = {{2,2,5,4},{3,3,6,6},{5,1,6,3}};
 		int[] answer = solution(rows, columns, queries);
 		System.out.println(Arrays.toString(answer));
+		
+		int rows1 = 6;
+		int columns1 = 6;
+		int[][] queries1 = {{2,2,5,4},{3,3,6,6},{5,1,6,3}};
+		int[] answer1 = solution1(rows1, columns1, queries1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static int[] solution(int rows, int columns, int[][] queries) {
@@ -62,5 +68,56 @@ public class 행렬_테두리_회전하기 {
 		
 		return answer;
 	}
+	
+	// 다른 사람의 풀이 참고
+	public static int[] solution1(int rows, int columns, int[][] queries) {
+		int[] answer = new int[queries.length];
+		
+		int[][] array = new int[rows][columns];
+		
+		for(int i=0; i<rows; i++) {
+			for(int j=0; j<columns; j++) {
+				array[i][j] = i*columns + (j+1); 
+			}
+		}
+		
+		for(int i=0; i<queries.length; i++) {
+			answer[i] = rotate(queries[i], array);
+		}
+		
+		return answer;
+	}
 
+	private static int rotate(int[] queries, int[][] array) {
+		int x = queries[0];
+		int y = queries[1];
+		
+		int[] dx = {0, -1, 0, 1};
+		int[] dy = {1, 0, -1, 0};
+		
+		int dir = 3;
+		
+		int temp = array[x][y];
+		int min = temp;
+		
+		while(true) {
+			if(x==queries[2] && y==queries[1]) 	dir = 0;
+			if(x==queries[2] && y==queries[3])	dir = 1;
+			if(x==queries[0] && y==queries[3])	dir = 2;
+			
+			array[x][y] = array[x+dx[dir]][y+dy[dir]];
+			
+			x += dx[dir];
+			y += dy[dir];
+			
+			min = Math.min(array[x][y], min);
+			
+			if(x==queries[0] && y==queries[1]) {
+				array[x][y+1] = temp;
+				break;
+			}
+		}
+		
+		return min;
+	}
 }
