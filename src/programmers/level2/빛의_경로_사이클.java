26bb8f2554abbd589392168fe38b0c54da127935
fullcrescent <fullcrescent@naver.com>
2022-05-01 22:45:01 +0900

    2022-05-01 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/빛의_경로_사이클.java
+++ src/programmers/level2/빛의_경로_사이클.java
@@ -1,5 +1,6 @@
 package programmers.level2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -12,6 +13,10 @@ public class 빛의_경로_사이클 {
 		String[] grid = {"S"};
 		int[] answer = solution(grid);
 		System.out.println(Arrays.toString(answer));
+		
+		String[] grid1 = {"S"};
+		int[] answer1 = solution1(grid1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static int[] solution(String[] grid) {
@@ -91,5 +96,55 @@ public class 빛의_경로_사이클 {
 		}
 		
 		return count;
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int[] solution1(String[] grid) {
+		List<Integer> list = new ArrayList<>();
+		
+		int[] dx = {1, 0, -1, 0};
+		int[] dy = {0, -1, 0, 1};
+		
+		boolean[][][] visit = new boolean[grid.length][grid[0].length()][dx.length];
+		
+		for(int i=0; i<grid.length; i++) {
+			for(int j=0; j<grid[i].length(); j++) {
+				for(int k=0; k<dx.length; k++) {
+					if(!visit[i][j][k]) {
+						int x = i;
+						int y = j;
+						int dir = k;
+						
+						int count = 0;
+						
+						while(!visit[x][y][dir]) {
+							visit[x][y][dir] = true;
+							
+							if(grid[x].charAt(y) == 'R')  dir++;
+							if(grid[x].charAt(y) == 'L')  dir--;
+							
+							if(dir==4) dir=0;
+							if(dir==-1) dir=3;
+							
+							x += dx[dir];
+							y += dy[dir];
+							
+							if(x==-1) x = grid.length-1;
+							if(x==grid.length) x = 0;
+							
+							if(y==-1) y = grid[x].length()-1;
+							if(y==grid[x].length()) y = 0;
+							
+							count++;
+						}
+						list.add(count);
+					}
+				}
+			}
+		}
+		
+		list.sort(null);
+		
+		return list.stream().mapToInt(Integer::intValue).toArray();
 	}
 }
