c21cb89ed03889f08d9acbc90ab98b26b09262f2
fullcrescent <fullcrescent@naver.com>
2022-05-08 17:31:41 +0900

    2022-05-08 알고리즘 소스 작성중

--- src/programmers/level3/브라이언의_고민.java
+++ src/programmers/level3/브라이언의_고민.java
@@ -6,9 +6,26 @@ import java.util.Map;
 public class 브라이언의_고민 {
 
 	public static void main(String[] args) {
-		String sentence = "SpIpGpOpNpGJqOqA";
+		String sentence = "AAAaBaAbBBBBbCcBdBdBdBcCeBfBeGgGGjGjxRvRvRvRvRvRx";
 		String answer = solution(sentence);
 		System.out.println(answer);
+		
+//		System.out.println("테케");
+//		System.out.println(solution("HaEaLaLaObWORLDb"));
+//		System.out.println(solution("SpIpGpOpNpGJqOqA"));
+//		System.out.println(solution("AxAxAxAoBoBoB"));
+//		System.out.println(solution("aIaAM"));
+//		System.out.println(solution("AAAaBaAbBBBBbCcBdBdBdBcCeBfBeGgGGjGjGRvRvRvRvRvR"));
+//		System.out.println(solution("aaA"));
+//		System.out.println(solution("Aaa"));
+//		System.out.println(solution("HaEaLaLaOWaOaRaLaD"));
+//		System.out.println(solution("aHELLOWORLDa"));
+//		System.out.println(solution("HaEaLaLObWORLDb"));
+//		System.out.println(solution("HaEaLaLaObWORLDb"));
+//		System.out.println(solution("aHbEbLbLbOacWdOdRdLdDc"));
+//		System.out.println(solution("abAba"));
+//		System.out.println(solution("xAaAbAaAx"));
+//		System.out.println(solution("AbAaAbAaC"));
 	}
 
 	public static String solution(String sentence) {
@@ -19,20 +36,9 @@ public class 브라이언의_고민 {
 			if(temp==' ') return "invalid";
 		}
 
-		while(!sentence.equals("invalid") && validate(sentence)) {
-			StringBuffer sb = new StringBuffer();
-			
-			sentence = patterRemove(sentence, sb, map);
-		}
+		StringBuffer sb = new StringBuffer();
 		
-		return sentence.replaceAll(" +", " ").trim();
-	}
-
-	private static boolean validate(String sentence) {
-		for(char temp : sentence.toCharArray()) {
-			if(Character.isLowerCase(temp)) return true;
-		}
-		return false;
+		return patterRemove(sentence, sb, map).replaceAll(" +", " ").trim();
 	}
 
 	private static String patterRemove(String sentence, StringBuffer sb, Map<Character, Integer> map) {
@@ -49,32 +55,33 @@ public class 브라이언의_고민 {
 					
 					int count = 1;
 					
-					while(iMove+2<sentence.length() && temp==sentence.charAt(iMove+2)) {
+					iMove += 2;
+					
+					while(iMove<sentence.length() && temp==sentence.charAt(iMove)) {
 						count++;
 						if(Character.isLowerCase(sentence.charAt(++iMove))) return "invalid";
 						iMove++;
 					}
 					
-					if(count!=map.get(temp) || iMove+1>=sentence.length()) return "invalid";
+					if(count!=map.get(temp)) return "invalid";
 					
 					if(flag) sb.insert(sb.length()-1, " ");
-					sb.append(sentence.substring(i, iMove+2).replaceAll(String.valueOf(temp), "") + " ");
+					sb.append(sentence.substring(i, iMove).replaceAll(String.valueOf(temp), "") + " ");
 					
-					i = iMove+1;
+					i = iMove-1;
 				}else if(map.get(temp)==2) {						// 특정 단어 앞뒤
 					while(temp!=sentence.charAt(++iMove)) {}
 					
 					if(Character.isLowerCase(sentence.charAt(i+1))||Character.isLowerCase((sentence.charAt(iMove-1)))) return "invalid";
 					String addString = sentence.substring(i, iMove).replaceAll(String.valueOf(temp), "");
 					
-					if(!validate2(addString, map)) return "invalid";
+					addString = remove(addString, map);
+					if(addString.equals("invalid")) return "invalid";
 					
 					if(flag) sb.append(" ");
-					sb.append(addString + " ");
+					sb.append(addString.replaceAll("[a-z]", "") + " ");
 					
 					 i = iMove;
-				}else {
-					return "invalid";
 				}
 				
 				flag = false;
@@ -87,23 +94,34 @@ public class 브라이언의_고민 {
 		return sb.toString();
 	}
 
-	private static boolean validate2(String addString, Map<Character, Integer> map) {
-		char tempChar=' ';
-		int count = 0;
+	private static String remove(String addString, Map<Character, Integer> map) {
+		char temp =' ';
+		int index = 1;
 		
-		for(char temp : addString.toCharArray()) {
-			if(Character.isLowerCase(temp)) {
-				if(tempChar==' '||tempChar==temp) {
-					tempChar = temp;
-				}else {
-					return false;
-				}
-				
-				count++;
+		Map<Character, Integer> tempMap = new HashMap<>();
+		
+		while(index<addString.length()) {
+			if(temp!=' '&&temp!=addString.charAt(index)) {
+				return "invalid";
 			}
+			
+			temp = addString.charAt(index);
+			
+			if(!Character.isLowerCase(temp)) {
+				if(index==1) {
+					return addString;
+				}else {
+					return "invalid";
+				}
+			}
+			
+			tempMap.put(temp, tempMap.getOrDefault(temp, 0)+1);
+			if(Character.isLowerCase(addString.charAt(++index))) return "invalid";
+			index++;
 		}
 		
-		return tempChar==' ' || map.get(tempChar)==count;
+		
+		return tempMap.get(temp)==map.get(temp) ? addString.replaceAll(String.valueOf(temp), "") : "invalid";
 	}
 
 }
