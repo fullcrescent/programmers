4dd6f3a9c583b750b3aad0ecffde4518a2c4c17f
fullcrescent <fullcrescent@naver.com>
2022-05-12 22:42:07 +0900

    2022-05-12 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/가장_긴_팰린드롬.java
+++ src/programmers/level3/가장_긴_팰린드롬.java
@@ -6,6 +6,10 @@ public class 가장_긴_팰린드롬 {
 		String s = "abaabaaaaaaa";
 		int answer = solution(s);
 		System.out.println(answer);
+		
+		String s1 = "abaabaaaaaaa";
+		int answer1 = solution1(s1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(String s) {
@@ -47,4 +51,41 @@ public class 가장_긴_팰린드롬 {
 		return max;
 	}
 
+	// 다른 사람의 풀이 참고
+	public static int solution1(String s) {
+		int left = 0;
+		int right = 0;
+		
+		int answer = 1;
+		
+		if(s.length()!=1) {
+			for(int center=0; center<=s.length()-2; center++) {
+				left = center;
+				right = center+1;
+				
+				while(left>=0&&right<=s.length()-1) {
+					if(s.charAt(left)!=s.charAt(right)) break;
+					
+					answer = Math.max(right-left+1, answer);
+					left--;
+					right++;
+				}
+			}
+			
+			for(int center=1; center<s.length(); center++) {
+				left = center-1;
+				right = center+1;
+				
+				while(left>=0&&right<=s.length()-1) {
+					if(s.charAt(left)!=s.charAt(right)) break;
+					
+					answer = Math.max(right-left+1, answer);
+					left--;
+					right++;
+				}
+			}
+		}
+		
+		return answer;
+	}
 }
