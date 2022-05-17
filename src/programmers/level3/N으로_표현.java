1b6b4e020277c224998db525e91ccd0c3cd94b99
fullcrescent <fullcrescent@naver.com>
2022-05-17 19:48:03 +0900

    2022-05-17 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/N으로_표현.java
+++ src/programmers/level3/N으로_표현.java
@@ -2,8 +2,10 @@ package programmers.level3;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class N으로_표현 {
 
@@ -12,6 +14,11 @@ public class N으로_표현 {
 		int number = 12;
 		int answer = solution(N, number);
 		System.out.println(answer);
+		
+		int N1 = 5;
+		int number1 = 12;
+		int answer1 = solution1(N1, number1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(int N, int number) {
@@ -62,5 +69,44 @@ public class N으로_표현 {
 			temp += N*Math.pow(10, i);
 		}
 		tempList.add(temp);
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int N, int number) {
+		int answer = -1;
+		int max = 8;
+		
+		List<Set<Integer>> list = new ArrayList<>();
+		
+		int temp = N;
+		
+		list.add(null);
+		for(int i=0; i<max; i++) {
+			Set<Integer> tempSet = new HashSet<>();
+			tempSet.add(temp);
+			list.add(tempSet);
+			temp = temp*10+N;
+		}
+		
+		for(int i=1; i<=max; i++) {
+			Set<Integer> tempSet = list.get(i);
+			for(int j=1; j<i; j++) {
+				for(Integer prev : list.get(j)) {
+					for(Integer post : list.get(i-j)) {
+						tempSet.add(prev+post);
+						tempSet.add(prev-post);
+						tempSet.add(prev*post);
+						if(post!=0)	tempSet.add(prev/post);
+						if(prev!=0)	tempSet.add(post/prev);
+					}
+				}
+			}
+			if(tempSet.contains(number)) {
+				answer = i;
+				break;
+			}
+		}
+		
+		return answer;
 	}
 }
