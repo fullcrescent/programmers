52f8b44d8204d520018bd0426196dc6dd3ffcca6
fullcrescent <fullcrescent@naver.com>
2022-05-18 21:16:33 +0900

    2022-05-18 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/가장_먼_노드.java
+++ src/programmers/level3/가장_먼_노드.java
@@ -1,8 +1,11 @@
 package programmers.level3;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class 가장_먼_노드 {
@@ -12,6 +15,11 @@ public class 가장_먼_노드 {
 		int[][] edge = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
 		int answer = solution(n, edge);
 		System.out.println(answer);
+		
+		int n1 = 6;
+		int[][] edge1 = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+		int answer1 = solution1(n1, edge1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(int n, int[][] edge) {
@@ -57,5 +65,39 @@ public class 가장_먼_노드 {
 		}
 		
 		return answer;
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int n, int[][] edge) {
+		int[] dist = new int[n+1];
+		boolean[][] map = new boolean[n+1][n+1];
+		
+		for(int i=0; i<edge.length; i++) {
+			map[edge[i][0]][edge[i][1]] = map[edge[i][1]][edge[i][0]] = true;
+		}
+		
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(1);
+		
+		while(!queue.isEmpty()) {
+			int temp = queue.poll();
+			
+			for(int i=2; i<n+1; i++) {
+				if(map[temp][i] && dist[i]==0) {
+					queue.add(i);
+					dist[i] = dist[temp]+1;
+				}
+			}
+		}
+		
+		Arrays.sort(dist);
+		
+		int index = dist.length;
+		
+		while(index-->0) {
+			if(dist[index]!=dist[index-1]) break;
+		}
+		
+		return dist.length-index;
 	}
 }
