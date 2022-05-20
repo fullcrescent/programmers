760ae952ef6c79946d92d60313ecc9882d7b5589
fullcrescent <fullcrescent@naver.com>
2022-05-20 21:16:21 +0900

    2022-05-20 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/등굣길.java
+++ src/programmers/level3/등굣길.java
@@ -8,6 +8,12 @@ public class 등굣길 {
 		int[][] puddles = {{2, 2}};
 		int answer = solution(m, n, puddles);
 		System.out.println(answer);
+		
+		int m1 = 99;
+		int n1 = 99;
+		int[][] puddles1 = {{2, 2}};
+		int answer1 = solution1(m1, n1, puddles1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(int m, int n, int[][] puddles) {
@@ -37,5 +43,29 @@ public class 등굣길 {
 		}
 		
 		return (int)dp[0][0];
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int m, int n, int[][] puddles) {
+		int[][] dp = new int[m+1][n+1];
+		
+		for(int i=0; i<puddles.length; i++) {
+			dp[puddles[i][0]][puddles[i][1]] = -1;
+		}
+
+		dp[1][1] = 1;
+		
+		for(int i=1; i<=m; i++) {
+			for(int j=1; j<=n; j++) {
+				if(dp[i][j]==-1){
+					dp[i][j] = 0;
+				}else {
+					if(i!=1) dp[i][j] += dp[i-1][j]%1000000007;
+					if(j!=1) dp[i][j] += dp[i][j-1]%1000000007;
+				}
+			}
+		}
+		
+		return dp[m][n]%1000000007;
 	}
 }
