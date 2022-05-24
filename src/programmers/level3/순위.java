3b80faa9dc15943185e9a3135b441eb65bb427de
fullcrescent <fullcrescent@naver.com>
2022-05-24 22:09:57 +0900

    2022-05-24 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/순위.java
+++ src/programmers/level3/순위.java
@@ -12,6 +12,11 @@ public class 순위 {
 		int[][] results = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
 		int answer = solution(n, results);
 		System.out.println(answer);
+		
+		int n1 = 5;
+		int[][] results1 = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
+		int answer1 = solution1(n1, results1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(int n, int[][] results) {
@@ -71,5 +76,42 @@ public class 순위 {
 				tempList.addAll(map.get(temp));
 			}
 		}
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int n, int[][] results) {
+		int answer = 0;
+		
+		boolean[][] visit = new boolean[n+1][n+1];
+		
+		for(int[] temp : results) {
+			visit[temp[0]][temp[1]] = true;
+		}
+		
+		for(int link=1; link<n+1; link++) {
+			for(int i=1; i<n+1; i++) {
+				for(int j=1; j<n+1; j++) {
+					if(i==j) continue;
+					if(visit[i][link] && visit[link][j]) visit[i][j] =true;
+				}
+			}
+		}
+		
+		for(int i=1; i<n+1; i++) {
+			boolean flag = true;
+			
+			for(int j=1; j<n+1; j++) {
+				if(i==j) continue;
+				
+				if(!(visit[i][j] || visit[j][i])) {
+					flag = false;
+					break;
+				}
+			}
+			
+			if(flag) answer++;
+		}
+		
+		return answer;
 	}
 }
