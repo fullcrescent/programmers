b868a14b43015ead398c0d680c81ec788a95b33d
fullcrescent <fullcrescent@naver.com>
2022-05-28 23:03:59 +0900

    2022-05-28 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/타일링.java
+++ src/programmers/level3/타일링.java
@@ -7,6 +7,10 @@ public class 타일링 {
 		int n = 5;
 		int answer = solution(n);
 		System.out.println(answer);
+		
+		int n1 = 5;
+		int answer1 = solution1(n1);
+		System.out.println(answer1);
 	}
 
 	public static int solution(int n) {
@@ -23,5 +27,18 @@ public class 타일링 {
 		
 		return (int) ((vertical+horizontal)%1000000007);
 	}
-
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int n) {
+		int[] dp = new int[n+1];
+		
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i=3; i<=n; i++) {
+			dp[i] = (dp[i-2] + dp[i-1])%1000000007;
+		}
+		
+		return dp[n];
+	}
 }
