cb30f20889967b7d025c0f38112ca1e3c9a1c5ce
fullcrescent <fullcrescent@naver.com>
2022-06-01 22:48:46 +0900

    2022-06-01 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/단어_변환.java
+++ src/programmers/level3/단어_변환.java
@@ -11,6 +11,12 @@ public class 단어_변환 {
 		String[] words = {"hot", "dot", "dog", "lot", "log"};
 		int answer = solution(begin, target, words);
 		System.out.println(answer);
+		
+		String begin1 = "hit";
+		String target1 = "cog";
+		String[] words1 = {"hot", "dot", "dog", "lot", "log"};
+		int answer1 = solution1(begin1, target1, words1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(String begin, String target, String[] words) {
@@ -55,5 +61,56 @@ public class 단어_변환 {
 			this.s = s;
 			this.count = count;
 		}
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(String begin, String target, String[] words) {
+		int answer = 0;
+		
+		boolean[] visit = new boolean[words.length];
+		
+		Queue<Node1> queue = new LinkedList<>();
+		queue.add(new Node1(begin, 0));
+		
+		while(!queue.isEmpty()) {
+			Node1 node = queue.poll();
+			
+			for(int i=0; i<words.length; i++) {
+				if(visit[i]) continue;
+				
+				if(validate(node.s, words[i])) {
+					if(words[i].equals(target)) {
+						return node.count+1;
+					}
+					
+					queue.add(new Node1(words[i], node.count+1));
+					visit[i] = true;
+				}
+			}
+		}
+		
+		return answer;
+	}
+	
+	static class Node1{
+		String s;
+		int count;
+		
+		public Node1(String s, int count) {
+			this.s = s;
+			this.count = count;
+		}
+	}
+	
+	static boolean validate(String current, String next) {
+		int count = 0;
+		
+		for(int i=0; i<current.length(); i++) {
+			if(current.charAt(i)!=next.charAt(i)) {
+				if(count++>0) return false;
+			}
+		}
+		
+		return true;
 	}
 }
