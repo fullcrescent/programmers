e5d919b9bf2160640fce9b0e1f914d4ed210283c
fullcrescent <fullcrescent@naver.com>
2022-06-04 23:26:51 +0900

    2022-06-04 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/여행경로.java
+++ src/programmers/level3/여행경로.java
@@ -2,9 +2,11 @@ package programmers.level3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 public class 여행경로 {
 
@@ -12,6 +14,10 @@ public class 여행경로 {
 		String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
 		String[] answer = solution(tickets);
 		System.out.println(Arrays.toString(answer));
+		
+		String[][] tickets1 = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
+		String[] answer1 = solution1(tickets1);
+		System.out.println(Arrays.toString(answer1));
 	}
 
 	public static String[] solution(String[][] tickets) {
@@ -88,6 +94,57 @@ public class 여행경로 {
 		}
 		
 		return true;
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static String[] solution1(String[][] tickets) {
+		result = new ArrayList<>();
+		
+		boolean[] visit = new boolean[tickets.length];
+		
+		Stack<String> stack = new Stack<>();
+		stack.push("ICN");
+		
+		dfs(tickets, visit, stack, 0);
+		
+		if(result.size()>0) {
+			Collections.sort(result, (i1, i2) -> {
+				for(int i=0; i<i1.size(); i++) {
+					if(!i1.get(i).equals(i2.get(i))) return i1.get(i).compareTo(i2.get(i));
+				}
+				
+				return 0;
+			});
+		}
+		
+		return result.get(0).toArray(new String[tickets.length]);
+	}
+
+	static List<Stack<String>> result;
+	
+	private static void dfs(String[][] tickets, boolean[] visit, Stack<String> stack, int index) {
+		if(index==tickets.length) {
+			Stack<String> temp = new Stack<>();
+			temp.addAll(stack);
+			result.add(temp);
+			return;
+		}
+		
+		String arrive = stack.peek();
+		
+		for(int i=0; i<tickets.length; i++) {
+			if(visit[i]) continue;
+			
+			if(arrive.equals(tickets[i][0])) {
+				stack.push(tickets[i][1]);
+				visit[i] = true;
+				
+				dfs(tickets, visit, stack, index+1);
+				
+				visit[i] = false;
+				stack.pop();
+			}
+		}
 	}
 }
 
