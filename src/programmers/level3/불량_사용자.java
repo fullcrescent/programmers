42ed5e647b899324e892e81c67be8229b9d6bf85
fullcrescent <fullcrescent@naver.com>
2022-06-07 22:14:07 +0900

    2022-06-07 알고리즘 소스 작성중

---
+++ src/programmers/level3/불량_사용자.java
@@ -0,0 +1,83 @@
+package programmers.level3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class 불량_사용자 {
+
+	public static void main(String[] args) {
+		String[] user_id = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
+		String[] banned_id = {"*rodo", "*rodo", "******"};
+		int answer = solution(user_id, banned_id);
+		System.out.println(answer);
+	}
+	
+	public static int solution(String[] user_id, String[] banned_id) {
+		answer = 0;
+		
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		Map<String, Integer> countMap = new HashMap<>();
+		
+		int index = 0;
+		
+		for(String banned : banned_id) {
+			List<Integer> temp = new ArrayList<>();
+
+			Loop1:
+			for(int i=0; i<user_id.length; i++) {
+				if(banned.length()!=user_id[i].length()) {
+					continue;
+				}
+				
+				for(int j=0; j<banned.length(); j++) {
+					if(banned.charAt(j)!='*'&&banned.charAt(j)!=user_id[i].charAt(j)) {
+						continue Loop1;
+					}
+				}
+				
+				temp.add(i);
+			}
+			
+			map.put(index++, temp);
+			
+			countMap.put(banned, countMap.getOrDefault(banned, 0)+1);
+		}
+		
+		boolean[] visit = new boolean[user_id.length];
+		
+		validate(map, visit, 0, map.size());
+		
+		for(int temp : countMap.values()) {
+			if(temp>1) {
+				int divide = 1;
+				
+				for(int i=2; i<=temp; i++) {
+					divide *= i;
+				}
+				
+				answer = answer/divide;
+			}
+		}
+		
+		return answer;
+	}
+	
+	static int answer;
+
+	private static void validate(Map<Integer, List<Integer>> map, boolean[] visit, int start, int end) {
+		if(start==end) {
+			answer++;
+			return;
+		}
+		
+		for(int temp : map.get(start)) {
+			if(visit[temp]) continue;
+			
+			visit[temp] = true;
+			validate(map, visit, start+1, end);
+			visit[temp] = false;
+		}		
+	}
+}
