c6a9bc3274bc2066f9b52885ee2bf895f83d95f7
fullcrescent <fullcrescent@naver.com>
2022-06-12 21:28:37 +0900

    2022-06-12 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/보석_쇼핑.java
+++ src/programmers/level3/보석_쇼핑.java
@@ -9,9 +9,13 @@ import java.util.Map;
 public class 보석_쇼핑 {
 
 	public static void main(String[] args) {
-		String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA", "SAPPHIRE", "EMERALD", "RUBY"};
+		String[] gems = {"XYZ", "XYZ", "XYZ"};
 		int[] answer = solution(gems);
 		System.out.println(Arrays.toString(answer));
+		
+		String[] gems1 = {"XYZ", "XYZ", "XYZ"};
+		int[] answer1 = solution1(gems1);
+		System.out.println(Arrays.toString(answer1));
 	}
 	
 	public static int[] solution(String[] gems) {
@@ -60,6 +64,55 @@ public class 보석_쇼핑 {
 				flag = false;
 			}else {
 				flag = true;
+			}
+		}
+		
+		return answer;
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int[] solution1(String[] gems) {
+		int[] answer = new int[2];
+		
+		Map<String, List<Integer>> map = new HashMap<>();
+		
+		for(int i=0; i<gems.length; i++) {
+			List<Integer> temp  = map.getOrDefault(gems[i], new ArrayList<>());
+			temp.add(i);
+			
+			map.put(gems[i], temp);
+		}
+		
+		int min = gems.length;
+		int max = 0;
+		
+		List<Integer> temp = map.get(gems[0]);
+		
+		for(int i=0; i<=gems.length-map.size(); i++) {
+			if(temp.get(0)>=max) {
+				for(String key : map.keySet()) {
+					if(map.get(key).size()>0) {
+						max = Math.max(max, map.get(key).get(0));
+					}
+				}
+			}
+			
+			temp = map.get(gems[i]);
+			int tempValue = temp.remove(0);
+			
+			while(temp.size()>0 && i<max && ++tempValue==temp.get(0)) {
+				i++;
+				temp.remove(0);
+			}
+			
+			if(min > max-i) {
+				min = max-i;
+				answer[0] = i+1;
+				answer[1] = max+1;
+			}
+			
+			if(temp.size()==0) {
+				break;
 			}
 		}
 		
