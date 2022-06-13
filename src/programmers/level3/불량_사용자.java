67b07416575ae85075de44a343ba88eb3175cbb5
fullcrescent <fullcrescent@naver.com>
2022-06-13 21:01:29 +0900

    2022-06-13 알고리즘 소스

--- src/programmers/level3/불량_사용자.java
+++ src/programmers/level3/불량_사용자.java
@@ -14,6 +14,11 @@ public class 불량_사용자 {
 		String[] banned_id = {"*rodo", "*rodo", "******"};
 		int answer = solution(user_id, banned_id);
 		System.out.println(answer);
+		
+		String[] user_id1 = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
+		String[] banned_id1 = {"*rodo", "*rodo", "******"};
+		int answer1 = solution1(user_id1, banned_id1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(String[] user_id, String[] banned_id) {
@@ -72,5 +77,31 @@ public class 불량_사용자 {
 			validate(map, visit, start+1, end);
 			visit[temp] = false;
 		}		
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(String[] user_id, String[] banned_id) {
+		set = new HashSet<>();
+		
+		validate1(0, user_id, banned_id, 0);
+		
+		return set.size();
+	}
+	
+	static Set<Integer> set;
+
+	private static void validate1(int index, String[] user_id, String[] banned_id, int bit) {
+		if(index==banned_id.length) {
+			set.add(bit);
+			return;
+		}
+		
+		String regex = banned_id[index].replace("*", "[\\w\\d]");
+		
+		for(int i=0; i<user_id.length; i++) {
+			if((((bit>>i)&1)==1) || !user_id[i].matches(regex)) continue;
+			
+			validate1(index+1, user_id, banned_id, (bit|1<<i));
+		}
 	}
 }
