726903b14c71c9b1730648779ece4a5bfd8504d7
fullcrescent <fullcrescent@naver.com>
2022-06-15 08:21:41 +0900

    2022-06-15 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/자물쇠와_열쇠.java
+++ src/programmers/level3/자물쇠와_열쇠.java
@@ -7,6 +7,11 @@ public class 자물쇠와_열쇠 {
 		int[][] lock = {{0, 1, 1}, {1, 1, 1}, {1, 1, 1}};
 		boolean answer = solution(key, lock);
 		System.out.println(answer);
+		
+		int[][] key1 = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+		int[][] lock1 = {{0, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+		boolean answer1 = solution1(key1, lock1);
+		System.out.println(answer1);
 	}
 	
 	public static boolean solution(int[][] key, int[][] lock) {
@@ -54,4 +59,49 @@ public class 자물쇠와_열쇠 {
 		return false;
 	}
 	
+	
+	// 다른 사람의 풀이 참고
+	public static boolean solution1(int[][] key, int[][] lock) {
+		for(int r=0; r<4; r++) {
+			for(int x=-lock.length; x<lock.length; x++) {
+				for(int y=-lock.length; y<lock.length; y++) {
+					if(move(lock, key, x, y)) {
+						return true;
+					}
+				}
+			}
+			
+			key = rotate(key);
+		}
+		
+		return false;
+	}
+
+	private static boolean move(int[][] tempLock, int[][] key, int x, int y) {
+		for(int i=0; i<tempLock.length; i++) {
+			for(int j=0; j<tempLock.length; j++) {
+				int temp = tempLock[i][j];
+				
+				if(i-x>-1 && i-x<key.length && j-y>-1 && j-y<key.length) {
+					temp += key[i-x][j-y];
+				}
+				
+				if(temp!=1) return false;
+			}
+		}
+		
+		return true;
+	}
+
+	private static int[][] rotate(int[][] key) {
+		int[][] tempKey = new int[key.length][key.length];
+		
+		for(int i=0; i<key.length; i++) {
+			for(int j=0; j<key.length; j++) {
+				tempKey[i][j] = key[key.length-1-j][i];
+			}
+		}
+		
+		return tempKey;
+	}
 }
