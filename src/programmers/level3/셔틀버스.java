311d5457dced83f13da0f50801be4f46b2a30466
fullcrescent <fullcrescent@naver.com>
2022-06-18 20:36:41 +0900

    2022-06-18 알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/셔틀버스.java
+++ src/programmers/level3/셔틀버스.java
@@ -11,6 +11,13 @@ public class 셔틀버스 {
 		String[] timetable = {"00:00", "00:00", "00:00", "00:00","00:00"};
 		String answer = solution(n, t, m, timetable);
 		System.out.println(answer);
+		
+		int n1 = 2;
+		int t1 = 10;
+		int m1 = 5;
+		String[] timetable1 = {"00:00", "00:00", "00:00", "00:00","00:00"};
+		String answer1 = solution1(n1, t1, m1, timetable1);
+		System.out.println(answer1);
 	}
 	
 	public static String solution(int n, int t, int m, String[] timetable) {
@@ -55,5 +62,48 @@ public class 셔틀버스 {
 		int min = value%60;
 		
 		return String.format("%02d", hour) + ":" + String.format("%02d", min);
+	}
+	
+	// 다른 사람의 풀이 참고
+	public static String solution1(int n, int t, int m, String[] timetable) {
+		Integer[] time = new Integer[timetable.length];
+		int tempIndex = 0;
+		
+		for(String tempTime : timetable) {
+			String temp[] = tempTime.split(":");
+			
+			time[tempIndex++] = Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);
+		}
+		
+		Arrays.sort(time);
+		
+		int startTime = 540;
+		int endTime = startTime+(n-1)*t;
+		
+		int index = 0;
+		int endIndex = 0;
+		
+		while(startTime<=endTime) {
+			endIndex = index+m;
+			
+			while(index<time.length && index<endIndex) {
+				if(time[index]<=startTime) {
+					index++;
+				}else {
+					break;
+				}
+			}
+			
+			startTime += t;
+		}
+		
+		return convertString1(index>0 && endIndex==index ? time[index-1]-1 : endTime);
+	}
+	
+	private static String convertString1(int value) {
+		int hour = value/60;
+		int min = value%60;
+		
+		return String.format("%02d:%02d", hour, min);
 	}
 }
