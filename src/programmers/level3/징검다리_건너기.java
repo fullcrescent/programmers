6a5f7cc9759e32186dc9f78741e3473976c6a84e
fullcrescent <fullcrescent@naver.com>
2022-06-21 23:03:28 +0900

    2022-06-21 알고리즘 소스 작성중

--- src/programmers/level3/징검다리_건너기.java
+++ src/programmers/level3/징검다리_건너기.java
@@ -1,44 +1,35 @@
 package programmers.level3;
 
-import java.util.TreeSet;
-import java.util.Iterator;
-import java.util.Set;
-
 public class 징검다리_건너기 {
 
 	public static void main(String[] args) {
-		int[] stones = {2, 4, 5, 3, 2, 2, 4, 2, 5, 2};
+		int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
 		int k = 3;
 		int answer = solution(stones, k);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int[] stones, int k) {
-		Set<Integer> set = new TreeSet<>();
+		int answer = 200000000;
 		
-		for(int i=0; i<stones.length; i++) {
-			set.add(stones[i]);
-		}
-		
-		Iterator<Integer> iter = set.iterator();
-		
-		while(iter.hasNext()) {
-			int min = iter.next();
+		for(int stone : stones) {
+			if(stone>answer) continue;
 			
 			int temp = 0;
 			
-			for(int stone : stones) {
-				if(stone>min) {
-					temp=0;
+			for(int tempStone : stones) {
+				if(tempStone>stone) {
+					temp = 0;
 				}else {
 					temp++;
 					if(temp==k) {
-						return min;
+						answer = stone;
+						break;
 					}
 				}
 			}
 		}
 		
-		return 0;
+		return answer;
 	}
 }
