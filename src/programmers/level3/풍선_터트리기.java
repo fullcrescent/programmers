ee71b97dd81fc5940ef185bdb1e0ddf211a7de2f
fullcrescent <fullcrescent@naver.com>
2022-06-23 22:21:33 +0900

    2022-06-23 알고리즘 소스 작성중

--- src/programmers/level3/풍선_터트리기.java
+++ src/programmers/level3/풍선_터트리기.java
@@ -3,31 +3,40 @@ package programmers.level3;
 public class 풍선_터트리기 {
 
 	public static void main(String[] args) {
-		int[] a = {9,-1,-5};
+		int[] a = {-16,27,65,-2,58,-92,-71,-68,-61,-33};
 		int answer = solution(a);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int[] a) {
-		int answer = 1;
+		int answer = a.length;
 		
-		int left = 1000000000;
+		int left = a[0];
+		int right = 1000000000;
 		
-		for(int i=0; i<a.length-1; i++) {
+		for(int i=0; i<a.length; i++) {
+			right = Math.min(right, a[i]);
+		}
+		
+		for(int i=1; i<a.length-1; i++) {
 			int temp = 0;
 			
-			left = Math.min(left, a[i]);
+			left = Math.min(left, a[i-1]);
 			
-			for(int j=i+1; j<a.length; j++) {
-				if(a[i]>a[j]) {
-					temp++;
-					break;
+			if(left<a[i])	temp++;
+			
+			if(a[i]>right)	temp++;
+			
+			if(right==a[i+1]) {
+				right = 1000000000;
+				for(int j=i+2; j<a.length; j++) {
+					right = Math.min(right, a[j]);
 				}
 			}
 			
-			if(left<a[i+1])	temp++;
-			
-			if(temp<2) answer++;
+			if(temp==2) {
+				answer--;
+			}
 		}
 		
 		return answer;
