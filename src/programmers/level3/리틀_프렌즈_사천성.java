319dbcdddd1b74b64e600c545809549c7aa42d87
fullcrescent <fullcrescent@naver.com>
2022-06-28 03:44:23 +0900

    2022-06-28 알고리즘 소스 작성중

--- src/programmers/level3/리틀_프렌즈_사천성.java
+++ src/programmers/level3/리틀_프렌즈_사천성.java
@@ -3,94 +3,122 @@ package programmers.level3;
 public class 리틀_프렌즈_사천성 {
 
 	public static void main(String[] args) {
-		int m = 2;
+		int m = 3;
 		int n = 4; 
-		String[] board = {"NRYN", "ARYA"};
+		String[] board = {".A*.", "AN..", "*N**"};
 		String answer = solution(m, n, board);
 		System.out.println(answer);
 	}
 	
 	public static String solution(int m, int n, String[] board) {
 		String answer = "";
+		String tempAnswer = null;
 		
-		char temp = 64;
-
-		while(temp++<91) {
-			Point p1 = null;
-			Point p2 = null;
+		while(answer!=tempAnswer) {
+			tempAnswer = answer;
 			
-			for(int i=0; i<m; i++) {
-				for(int j=0; j<n; j++) {
-					if(board[i].charAt(j)==temp) {
-						if(p1==null) {
-							p1 = new Point(i, j);
-						}else {
-							p2 = new Point(i, j);
+			char temp = 64;
+			
+			while(temp++<91) {
+				int startX = -1, startY = -1, endX = -1, endY = -1;
+				
+				Loop :
+				for(int i=0; i<m; i++) {
+					for(int j=0; j<n; j++) {
+						if(board[i].charAt(j)==temp) {
+							if(startX==-1) {
+								startX = i;
+								startY = j;
+							}else if(endX==-1){
+								endX = i;
+								endY = j;
+								break Loop;
+							}
 						}
 					}
 				}
-			}
-			
-			int startX, endX, startY, endY;
-			
-			if(p1==null) {
-				continue;
-			}
-			
-			if(p1.x<p2.x) {
-				startX = p1.x;
-				endX = p2.x;
-			}else {
-				startX = p2.x;
-				endX = p1.x;
-			}
-					 
-			if(p1.y<p2.y) {
-				startY = p1.y;
-				endY = p2.y;
-			}else {
-				startY = p2.y;
-				endY = p1.y;
-			}
-			
-			Loop1 :
-			for(int i=startX+1; i<=endX; i++) {
-				for(int j=startY; j<=endY; j++) {
-					if(board[i].charAt(j)==temp) {
+				
+				if(endX==-1) continue;
+				
+				int i = startX<endX ? 1 : -1;
+				int j = startY<endY ? 1 : -1;
+				
+				if(startX!=endX) {
+					int tempX = startX;
+					int tempY = startY;
+					
+					boolean flag = true;
+					
+					while(tempX!=endX) {
+						tempX += i;
+						
+						if(board[tempX].charAt(startY)!='.') {
+							flag = false;
+							break;
+						}
+					}
+					
+					if(flag) {
+						while(tempY!=endY) {
+							tempY += j;
+							
+							if(board[tempX].charAt(tempY)!='.') {
+								break;
+							}
+						}
+					}
+					
+					if(tempX==endX && tempY==endY) {
 						answer += temp;
-						board[p1.x] = board[p1.x].replace(temp, '*');
-						board[p2.x] = board[p2.x].replace(temp, '*');		
-					}else if(board[i].charAt(j)=='.') {
-						break Loop1;
+						board[startX] = board[startX].replace(temp, '.');
+						board[endX] = board[endX].replace(temp, '.');	
+						continue;
 					}
 				}
-			}
-			
-			Loop2 :
-			for(int i=startY+1; i<=endY; i++) {
-				for(int j=startX; j<=endX; j++) {
-					if(board[i].charAt(j)==temp) {
+				
+				if(startY!=endY) {
+					int tempX = startX;
+					int tempY = startY;
+					
+					boolean flag = true;
+					
+					while(tempY!=endY) {
+						tempY += j;
+						
+						if(board[startX].charAt(tempY)!='.') {
+							flag = false;
+							break;
+						}
+					}
+					
+					if(flag) {
+						while(tempX!=endX) {
+							tempX += i;
+							
+							if(board[tempX].charAt(tempY)!='.') {
+								break;
+							}
+						}
+					}
+					
+					if(tempX==endX && tempY==endY) {
 						answer += temp;
-						board[p1.x] = board[p1.x].replace(temp, '*');
-						board[p2.x] = board[p2.x].replace(temp, '*');		
-					}else if(board[i].charAt(j)=='.') {
-						break Loop2;
+						board[startX] = board[startX].replace(temp, '.');
+						board[endX] = board[endX].replace(temp, '.');
+						continue;
 					}
 				}
 			}
 		}
-
+		
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<n; j++) {
+				if(board[i].charAt(j)>64 && board[i].charAt(j)<91){
+					return "IMPOSSIBLE";
+				}
+			}
+		}
+		
 		return answer;
-	}
-	
-	static class Point{
-		int x;
-		int y;
-
-		public Point(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
 	}
 }
