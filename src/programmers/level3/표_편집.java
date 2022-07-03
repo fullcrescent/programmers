6de58c51c6768394f8216c22a13e2eb05c95aaa4
fullcrescent <fullcrescent@naver.com>
2022-07-04 00:52:46 +0900

    2022-07-04 알고리즘 소스 작성중

--- src/programmers/level3/표_편집.java
+++ src/programmers/level3/표_편집.java
@@ -18,12 +18,6 @@ public class 표_편집 {
 		
 		int start = k;
 		
-		int left = 0;
-		int right = 0;
-		int mid = 0;
-		int count = 0;
-		int index = 0;
-		
 		for(String temp : cmd) {
 			int value = 1;
 			if(temp.length()>1) {
@@ -32,107 +26,20 @@ public class 표_편집 {
 			
 			switch(temp.charAt(0)) {
 				case 'U' :
-					index = start-1;
-					left = 0;
-					right = start-1; 
-					
-					while(left<=right){
-						count = 0;
-						
-						mid = (left+right)/2;
-						
-						for(int i=index; i>=mid; i--) {
-							if(!remove[i]) count++;
-							
-							if(count==value) break;
-						}
-						
-						if(count==value) {
-							start = mid;
-							left = mid+1;
-						}else {
-							right = mid-1;
-						}
-					}
+					start = binarySearch(remove, 0, start-1, value);
 					
 					break;
 				case 'D' : 
-					index = start+1;
-					left = start+1;
-					right = n; 
-					
-					while(left<=right){
-						count = 0;
-						
-						mid = (left+right)/2;
-						
-						for(int i=index; i<=mid; i++) {
-							if(!remove[i]) count++;
-							
-							if(count==value) break;
-						}
-						
-						if(count==value) {
-							start = mid;
-							right = mid-1;
-						}else {
-							left = mid+1;
-						}
-					}
+					start = binarySearch(remove, start+1, n, value);
 					
 					break;
 				case 'C' :
 					stack.add(start);
 					remove[start] = true;
 					
-					index = start+1;
-					left = start+1;
-					right = n; 
+					int search = binarySearch(remove, start+1, n, value);
 					
-					Loop1 :
-					while(left<=right){
-						count = 0;
-						
-						mid = (left+right)/2;
-						
-						for(int i=index; i<=mid; i++) {
-							if(i==remove.length) {
-								start = remove.length;
-								break Loop1;
-							}
-							
-							if(!remove[i]) {
-								start = mid;
-								right = mid-1;
-								continue Loop1;
-							}
-						}
-						
-						left = mid+1;
-					}
-					
-					if(start==remove.length) {
-						index = start-1;
-						left = 0;
-						right = start-1; 
-						
-						Loop2 : 
-						while(left<=right){
-							count = 0;
-							
-							mid = (left+right)/2;
-							
-							for(int i=index; i>=mid; i--) {
-								if(!remove[i]) {
-									start = mid;
-									left = mid+1;
-									continue Loop2;
-								}
-							}
-							
-							right = mid-1;
-						}
-					}
+					start = search;
 					
 					break;
 				case 'Z' : 
@@ -152,48 +59,48 @@ public class 표_편집 {
 		return sb.toString();
 	}
 
-//	private static int binarySearch(boolean[] array, int left, int right, int value) {
-//		int mid = 0, index = 0, weight = 0, answer = 0;
-//		
-//		if(left==0) {
-//			index = right-1;
-//			weight = -1;
-//		}
-//		else{
-//			index = left+1;
-//			weight = 1;
-//		}
-//		
-//		while(left<=right){
-//			int count = 0;
-//			
-//			mid = (left+right)/2;
-//			
-//			for(int i=index; i!=mid+weight; i += weight) {
-//				if(i==array.length) return array.length;
-//				
-//				if(!array[i]) count++;
-//				
-//				if(count==value) break;
-//			}
-//			
-//			if(weight==1) {
-//				if(count==value) {
-//					answer = mid;
-//					right = mid-1;
-//				}else {
-//					left = mid+1;
-//				}
-//			}else {
-//				if(count==value) {
-//					answer = mid;
-//					left = mid+1;
-//				}else {
-//					right = mid-1;
-//				}
-//			}
-//		}
-//		
-//		return answer;
-//	}
+	private static int binarySearch(boolean[] array, int left, int right, int value) {
+		int mid = 0, index = 0, weight = 0, answer = 0;
+		
+		if(left==0) {
+			index = right;
+			weight = -1;
+		}
+		else{
+			index = left;
+			weight = 1;
+		}
+		
+		while(left<=right){
+			int count = 0;
+			
+			mid = (left+right)/2;
+			
+			for(int i=index; i!=mid+weight; i += weight) {
+				if(i==array.length) return binarySearch(array, 0, left-2, value);
+				
+				if(!array[i]) count++;
+				
+				if(count==value) break;
+			}
+			
+			if(weight==1) {
+				if(count==value) {
+					answer = mid;
+					right = mid-1;
+				}else {
+					left = mid+1;
+				}
+			}else {
+				if(count==value) {
+					answer = mid;
+					left = mid+1;
+				}else {
+					right = mid-1;
+				}
+			}
+		}
+		
+		return answer;
+	}
 }
