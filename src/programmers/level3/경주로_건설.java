426b4e40cdfedb4aae45c0a714181c52c88a9a33
fullcrescent <fullcrescent@naver.com>
2022-07-14 18:32:05 +0900

    2022-07-14 알고리즘 소스 작성중

--- src/programmers/level3/경주로_건설.java
+++ src/programmers/level3/경주로_건설.java
@@ -9,49 +9,43 @@ public class 경주로_건설 {
 	}
 	
 	public static int solution(int[][] board) {
-		// 꺽는거 표현 필요
 		sum = new int[board.length][board[0].length];
-		findLoad(board, 0, 0, 0);
+		sum[0][0] = -1;
+		
+		findLoad(board, 0, 0, -500, -1);
+		
 		return min;
 	}
 	
-	static int min = -1;
+	static int min = 1<<30;
 	static int[][] sum;
 	
-	private static void findLoad(int[][] maps, int x, int y, int count) {
-		count++;
-		if(count == min) return ;
+	static int curveCost = 600;
+	static int straightCost = 100;
+	
+	static int[] dx = {0, 0, 1, -1};
+	static int[] dy = {1, -1, 0, 0};
+	static int[] direct = {0, 1, 2, 3};
+	
+	private static void findLoad(int[][] maps, int x, int y, int count, int direction) {
+		if(count >= min) return ;
+		
 		if(x==maps.length-1 && y==maps[0].length-1) {
 			if(min == -1) min = count;
 			min = Math.min(min,count);
 			return;
 		}
 		
-		if(x+1 != maps.length && maps[x+1][y] == 1) {
-			if(sum[x+1][y]==0 || sum[x+1][y] > count) {
-				sum[x+1][y] = count;
-				findLoad(maps, x+1, y, count);
-			}
-		}
-		
-		if(y+1 != maps[0].length && maps[x][y+1] == 1) {
-			if(sum[x][y+1]==0 || sum[x][y+1] > count) {
-				sum[x][y+1] = count;
-				findLoad(maps, x, y+1, count);
-			}
-		}
-		
-		if(x-1 > -1 && maps[x-1][y] == 1) {
-			if(sum[x-1][y]==0 || sum[x-1][y] > count) {
-				sum[x-1][y] = count;
-				findLoad(maps, x-1, y, count);
-			}
-		}
-		
-		if(y-1 > -1 && maps[x][y-1] == 1) {
-			if(sum[x][y-1]==0 || sum[x][y-1] > count) {
-				sum[x][y-1] = count;
-				findLoad(maps, x, y-1, count);
+		for(int i=0; i<4; i++) {
+			int tempX = x+dx[i];
+			int tempY = y+dy[i];
+			
+			if(tempX<=maps.length-1 && tempX>-1 && tempY<=maps[tempX].length-1 && tempY>-1 && maps[tempX][tempY] == 0) {
+				if(sum[tempX][tempY]==0 || sum[tempX][tempY] > count) {
+					int tempCount = count + (direction!=direct[i] ? curveCost : straightCost);
+					sum[tempX][tempY] = tempCount;
+					findLoad(maps, tempX, tempY, tempCount, direct[i]);
+				}
 			}
 		}
 	}
