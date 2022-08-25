bc7450e432c53dcfc0cedf00f740d28e5dbaf36f
fullcrescent <fullcrescent@naver.com>
2022-08-25 23:24:02 +0900

    2022-08-25 알고리즘 작성중

---
+++ src/programmers/level3/등산코스_정하기.java
@@ -0,0 +1,22 @@
+package programmers.level3;
+
+import java.util.Arrays;
+
+public class 등산코스_정하기 {
+
+	public static void main(String[] args) {
+		int n = 7;
+		int[][] paths = {{1, 2, 5}, {1, 4, 1}, {2, 3, 1}, {2, 6, 7}, {4, 5, 1}, {5, 6, 1}, {6, 7, 1}};
+		int[] gates = {3, 7};
+		int[] summits = {1, 5};
+		int[] answer = solution(n, paths, gates, summits);
+		System.out.println(Arrays.toString(answer));
+	}
+	
+	public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+		
+		
+		
+		return null;
+	}
+}
