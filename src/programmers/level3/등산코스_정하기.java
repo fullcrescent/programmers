f6ac1576faa475c870f414cb42fb97614b7e487e
fullcrescent <fullcrescent@naver.com>
2022-08-26 23:06:58 +0900

    2022-08-26 알고리즘 작성중

--- src/programmers/level3/등산코스_정하기.java
+++ src/programmers/level3/등산코스_정하기.java
@@ -1,6 +1,10 @@
 package programmers.level3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class 등산코스_정하기 {
 
@@ -14,9 +18,58 @@ public class 등산코스_정하기 {
 	}
 	
 	public static int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+		Map<Integer, List<int[]>> map = new HashMap<>();
+		boolean[] visit = new boolean[n+1];
+		
+		for(int[] path : paths) {
+			List<int[]> list;
+			
+			list = map.getOrDefault(path[0], new ArrayList<int[]>());
+			list.add(new int[] {path[1], path[2]});
+			map.put(path[0], list);
+			
+			list = map.getOrDefault(path[1], new ArrayList<int[]>());
+			list.add(new int[] {path[0], path[2]});
+			map.put(path[1], list);
+		}
 		
 		
+		for(int gate : gates) {
+			visit[gate] = true;
+		}
 		
-		return null;
+		for(int gate : gates) {
+			function(map, summits, visit, gate, 0);
+		}
+		
+		return new int[] {end, min};
 	}
+
+	static int min = Integer.MAX_VALUE;
+	static int end = Integer.MAX_VALUE;
+	
+	private static void function(Map<Integer, List<int[]>> map, int[] summits, boolean[] visit, int current, int intensity) {
+		if(min<intensity) return;
+		
+		for(int summit : summits) {
+			if(current==summit) {
+				if(min==intensity) {
+					end = Integer.min(end, current);
+				}else {
+					min = intensity;
+					end = current;
+				}
+				return;
+			}
+		}
+	
+		for(int[] temp : map.get(current)) {
+			if(visit[temp[0]]) continue;
+			
+			visit[temp[0]] = true;
+			function(map, summits, visit, temp[0], Integer.max(intensity, temp[1]));
+			visit[temp[0]] = false;
+		}
+	}
+	
 }
