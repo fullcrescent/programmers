21dc64bf688d714e2dea663a0a2159e5bc2608df
fullcrescent <fullcrescent@naver.com>
2022-09-13 22:35:15 +0900

    작성중

---
+++ src/programmers/level3/스티커_모으기.java
@@ -0,0 +1,18 @@
+package programmers.level3;
+
+public class 스티커_모으기 {
+
+	public static void main(String[] args) {
+		int sticker[] = {14, 6, 5, 11, 3, 9, 2, 10};
+		int answer = solution(sticker);
+		System.out.println(answer);
+	}
+	
+	public static int solution(int sticker[]) {
+		
+		
+		
+		
+		return 0;
+	}
+}
