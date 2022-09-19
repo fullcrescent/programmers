39995d37b1450aac8bc62a38be678590b450dba6
fullcrescent <fullcrescent@naver.com>
2022-09-19 21:28:08 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/기지국_설치.java
+++ src/programmers/level3/기지국_설치.java
@@ -11,6 +11,12 @@ public class 기지국_설치 {
 		int w = 2;
 		int answer = solution(n, stations, w);
 		System.out.println(answer);
+		
+		int n1 = 16;
+		int[] stations1 = {9};
+		int w1 = 2;
+		int answer1 = solution1(n1, stations1, w1);
+		System.out.println(answer1);
 	}
 	
 	public static int solution(int n, int[] stations, int w) {
@@ -33,4 +39,30 @@ public class 기지국_설치 {
 
 		return answer;
 	}
+	
+	// 다른 사람의 풀이 참고
+	public static int solution1(int n, int[] stations, int w) {
+		int answer = 0;
+		int left=1, right=1;
+		
+		for(int station : stations) {
+			left = station-w;
+			answer += count(left, right, w);
+			right = station+w+1;
+		}
+		
+		answer += count(n+1, right, w);
+		
+		return answer;
+	}
+
+	private static int count(int left, int right, int w) {
+		if(right<left) {
+			int diff = left - right;
+			return diff/(2*w+1) + (diff%(2*w+1)==0 ? 0 : 1);
+		}
+		
+		return 0;
+	}
+	
 }
