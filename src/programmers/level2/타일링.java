d21a47a04c2459a3260be2bf5f191c586ecdee6b
fullcrescent <fullcrescent@naver.com>
2022-09-26 20:30:16 +0900

    작성중

--- src/programmers/level2/타일링.java
+++ src/programmers/level2/타일링.java
@@ -3,24 +3,30 @@ package programmers.level2;
 public class 타일링 {
 
 	public static void main(String[] args) {
-		int n = 4;
+		int n = 8;
 		int answer = solution(n);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int n) {
-		int answer = 3;
 		int divide = 1000000007;
-		int add = 2;
 		
-		n -= 2;
+		int vertical = 1;
+		int horizontal = 1;
 		
-		while(n>0) {
-			n -= 2;
-			answer = (answer*3 + add)%divide;
-			add *= 2;
+		int index = 2;
+		
+		while(index++<n) {
+			int temp = vertical;
+			vertical = ((vertical + horizontal) % divide);
+			horizontal = temp;
 		}
 		
-		return answer;
+		int n2 = (vertical + horizontal) % divide;
+		
+		int add = (int) (Math.pow(2, n/2) -2);
+		int answer = (n2*2 + add) % divide;
+		
+		return answer-1;
 	}
 }
