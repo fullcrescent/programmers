e6d8eac91e60112685a7d905f223ab534195d9dd
fullcrescent <fullcrescent@naver.com>
2022-10-01 15:29:55 +0900

    작성중

---
+++ src/programmers/level2/멀리_뛰기.java
@@ -0,0 +1,18 @@
+package programmers.level2;
+
+public class 멀리_뛰기 {
+
+	public static void main(String[] args) {
+		int n = 10;
+		long answer = solution(n);
+		System.out.println(answer);
+	}
+	
+	public static long solution(int n) {
+		int divide = 1234567;
+		
+		
+		return 0;
+	}
+
+}
