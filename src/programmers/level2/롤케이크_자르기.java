c13c75524cf548f0fa34fca3151b38f493de79e9
fullcrescent <fullcrescent@naver.com>
2023-02-11 19:43:22 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/롤케이크_자르기.java
+++ src/programmers/level2/롤케이크_자르기.java
@@ -1,34 +1,70 @@
 package programmers.level2;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class 롤케이크_자르기 {
     public static void main(String[] args){
         int[] topping = {1, 2, 1, 3, 1, 4, 1, 2};
         int answer = solution(topping);
         System.out.println(answer);
+
+        int[] topping1 = {1, 2, 1, 3, 1, 4, 1, 2};
+        int answer1 = solution1(topping1);
+        System.out.println(answer1);
     }
 
     public static int solution(int[] topping) {
+        Map<Integer, Integer> map1 = new HashMap<>();
+
+        for(int temp : topping){
+            map1.put(temp, map1.getOrDefault(temp, 0)+1);
+        }
+
+        Map<Integer, Integer> map2 = new HashMap<>();
         int count = 0;
 
-        Loop:
-        for(int i=0; i<topping.length; i++){
-            Set<Integer> set1 = new HashSet<>();
-            Set<Integer> set2 = new HashSet<>();
+        for(int temp : topping){
+            map2.put(temp, map2.getOrDefault(temp, 0)+1);
 
-            for(int j=0; j<i+1; j++){
-                set1.add(topping[j]);
-            }
+            removeMap(map1, temp);
 
-            for(int j=i+1; j<topping.length; j++){
-                if(set1.size()<set2.size()) continue Loop;
+            if(map1.size()==map2.size()) count++;
+        }
 
-                set2.add(topping[j]);
-            }
+        return count;
+    }
 
-            if(set1.size()==set2.size()) count++;
+    private static void removeMap(Map<Integer, Integer> map, int remove) {
+        if(map.get(remove)>1){
+            map.put(remove, map.get(remove)-1);
+        }else{
+            map.remove(remove);
+        }
+    }
+
+    /*다른 사람의 풀이 참고*/
+    public static int solution1(int[] topping) {
+        int count = 0;
+
+        int[] left = new int[10001];
+        int[] right = new int[10001];
+
+        int leftCount = 0;
+        int rightCount = 0;
+
+        for(int temp : topping) right[temp]++;
+
+        for(int temp : right)   rightCount += temp>0 ? 1 : 0;
+
+        for(int temp : topping){
+            right[temp]--;
+            if(right[temp]==0)  rightCount--;
+
+            if(left[temp]==0) leftCount++;
+            left[temp]++;
+
+            if(leftCount==rightCount) count++;
         }
 
         return count;
