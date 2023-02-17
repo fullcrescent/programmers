f5f309880697c004c7eba415a793a9e7a42e5ed8
fullcrescent <fullcrescent@naver.com>
2023-02-17 19:53:15 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/뒤에_있는_큰_수_찾기.java
+++ src/programmers/level2/뒤에_있는_큰_수_찾기.java
@@ -8,6 +8,10 @@ public class 뒤에_있는_큰_수_찾기 {
         int[] numbers = {9, 1, 5, 3, 6, 2};
         int[] answer = solution(numbers);
         System.out.println(Arrays.toString(answer));
+
+        int[] numbers1 = {9, 1, 5, 3, 6, 2};
+        int[] answer1 = solution1(numbers1);
+        System.out.println(Arrays.toString(answer1));
     }
 
     public static int[] solution(int[] numbers) {
@@ -24,6 +28,24 @@ public class 뒤에_있는_큰_수_찾기 {
         }
 
         for(int[] temp : stack) answer[temp[1]] = -1;
+
+        return answer;
+    }
+
+    /*다른 사람의 풀이 참고*/
+    public static int[] solution1(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Arrays.fill(answer, -1);
+        Stack<int[]> stack = new Stack<>();
+
+        for(int i=0; i<numbers.length; i++){
+            while(!stack.isEmpty() && stack.peek()[0]<numbers[i]){
+                int[] temp = stack.pop();
+                answer[temp[1]] = numbers[i];
+            }
+
+            stack.add(new int[] {numbers[i], i});
+        }
 
         return answer;
     }
