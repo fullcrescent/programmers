8bd440d2dd8cc509e0e509dcd4980eaf14bffb7a
fullcrescent <fullcrescent@naver.com>
2023-02-18 22:07:57 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/귤_고르기.java
+++ src/programmers/level2/귤_고르기.java
@@ -1,6 +1,7 @@
 package programmers.level2;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class 귤_고르기 {
     public static void main(String[] args) {
@@ -8,6 +9,11 @@ public class 귤_고르기 {
         int[] tangerine = {1, 3, 2, 5, 4, 5, 2, 3};
         int answer = solution(k, tangerine);
         System.out.println(answer);
+
+        int k1 = 6;
+        int[] tangerine1 = {1, 3, 2, 5, 4, 5, 2, 3};
+        int answer1 = solution1(k1, tangerine1);
+        System.out.println(answer1);
     }
 
     public static int solution(int k, int[] tangerine) {
@@ -31,6 +37,23 @@ public class 귤_고르기 {
             k -= list.get(count);
             count++;
         }
+
+        return count;
+    }
+
+    /*다른 사람의 풀이 참고*/
+    public static int solution1(int k, int[] tangerine) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int temp : tangerine){
+            map.put(temp, map.getOrDefault(temp, 0) + 1);
+        }
+
+        List<Integer> list = map.values().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+        int count = 0;
+
+        while(k>0)  k -= list.get(count++);
 
         return count;
     }
