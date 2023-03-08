ad6d5c4a3dd1816d28ab7378c1141b56df210dee
fullcrescent <fullcrescent@naver.com>
2023-03-08 12:46:45 +0900

    알고리즘 소스

--- src/programmers/level2/유사_칸토어_비트열.java
+++ src/programmers/level2/유사_칸토어_비트열.java
@@ -3,28 +3,37 @@ package programmers.level2;
 public class 유사_칸토어_비트열 {
     public static void main(String[] args){
         int n = 2;
-        long l = 4;
-        long r = 17;
+        long l = 15;
+        long r = 25;
         int answer = solution(n, l, r);
         System.out.println(answer);
     }
 
     public static int solution(int n, long l, long r) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(1);
+        int answer = 0;
+        long all = (long) Math.pow(5, n);
 
-        for(int i=0; i<n; i++){
-            String temp = sb.toString();
-            sb.append(temp);
+        for(var i=l; i<=r; i++){
+            long divide = all;
+            long temp = i;
 
-            for(int j=0; j<Math.pow(5, i); j++){
-                sb.append(0);
+            while(true){
+                divide /= 5;
+
+                if(divide*2<temp && temp<divide*3+1){
+                    i = divide * ((i-1)/divide+1);
+                    break;
+                }
+
+                if(divide==1) {
+                    answer++;
+                    break;
+                }
+
+                temp = temp%divide;
             }
-
-            sb.append(temp);
-            sb.append(temp);
         }
 
-        return (int) sb.substring((int) l-1, (int) r).chars().filter(input -> input=='1').count();
+        return answer;
     }
 }
