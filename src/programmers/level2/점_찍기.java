b6058a49eeb648caa454cc2fa4d9acc0b1a91de8
fullcrescent <fullcrescent@naver.com>
2023-03-19 22:54:42 +0900

    알고리즘 소스

--- src/programmers/level2/점_찍기.java
+++ src/programmers/level2/점_찍기.java
@@ -10,12 +10,9 @@ public class 점_찍기 {
 
     public static long solution(int k, int d) {
         long answer = 0;
+
         for(int i=0; i<=d; i=i+k){
-            for(int j=0; j<=d; j=j+k){
-                if((i*i)+(j*j)<=d*d){
-                    answer++;
-                }
-            }
+            answer += Math.sqrt((long)Math.pow(d, 2) - (long)Math.pow(i, 2))/k +1;
         }
 
         return answer;
