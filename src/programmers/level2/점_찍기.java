740a088f6806ba0133b6ce81e5c7b8fc651ca635
fullcrescent <fullcrescent@naver.com>
2023-03-20 15:51:13 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/점_찍기.java
+++ src/programmers/level2/점_찍기.java
@@ -6,6 +6,11 @@ public class 점_찍기 {
         int d = 5;
         long answer = solution(k, d);
         System.out.println(answer);
+
+        int k1 = 1;
+        int d1 = 5;
+        long answer1 = solution1(k1, d1);
+        System.out.println(answer1);
     }
 
     public static long solution(int k, int d) {
@@ -13,6 +18,17 @@ public class 점_찍기 {
 
         for(int i=0; i<=d; i=i+k){
             answer += (long) (Math.sqrt((long)Math.pow(d, 2) - (long)Math.pow(i, 2))/k +1);
+        }
+
+        return answer;
+    }
+
+    /*다른 사람의 풀이 참고*/
+    public static long solution1(int k, int d) {
+        long answer = 0;
+
+        for(int i=0; i<=d; i=i+k){
+            answer += (int) (Math.sqrt(Math.pow(d, 2) - Math.pow(i, 2))/k +1);
         }
 
         return answer;
