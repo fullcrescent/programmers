6a3a23aca2646fb236a6ce69c9b2f4af1f413c8d
fullcrescent <fullcrescent@naver.com>
2023-03-21 19:27:25 +0900

    알고리즘 소스 작성중

--- src/programmers/level2/리코쳇_로봇.java
+++ src/programmers/level2/리코쳇_로봇.java
@@ -1,5 +1,8 @@
 package programmers.level2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class 리코쳇_로봇 {
     public static void main(String[] args){
         String[] board = {"...D..R", ".D.G...", "....D.D", "D....D.", "..D...."};
@@ -10,16 +13,20 @@ public class 리코쳇_로봇 {
     public static int solution(String[] board) {
         int[][] distance = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         int[][] count = new int[board[0].length()][board.length];
-        int[] start = new int[2];
+        Queue<int[]> queue = new LinkedList<>();
 
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[i].length(); j++){
-                if(board[i].charAt(j)=='R') start = new int[] {i, j};
+                if(board[i].charAt(j)=='R') queue.add(new int[] {i, j});
             }
         }
 
-        for(int[] temp : distance){
+        while(!queue.isEmpty()){
+            int[] temp = queue.poll();
 
+            for(int[] d : distance){
+
+            }
         }
 
         return 0;
