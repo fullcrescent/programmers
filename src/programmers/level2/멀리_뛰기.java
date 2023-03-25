042b669074f7a729f759e531626ca345f83e60df
fullcrescent <fullcrescent@naver.com>
2023-03-25 20:23:00 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/멀리_뛰기.java
+++ src/programmers/level2/멀리_뛰기.java
@@ -8,6 +8,10 @@ public class 멀리_뛰기 {
 		int n = 1999;
 		long answer = solution(n);
 		System.out.println(answer);
+
+		int n1 = 1999;
+		long answer1 = solution1(n1);
+		System.out.println(answer1);
 	}
 	
 	public static long solution(int n) {
@@ -30,4 +34,24 @@ public class 멀리_뛰기 {
 		return answer.mod(BigInteger.valueOf(1234567)).longValue();
 	}
 
+	/*다른 사람의 풀이 참고 - 변경X*/
+	public static long solution1(int n) {
+		BigInteger answer = BigInteger.ZERO;
+
+		BigInteger[] fac = new BigInteger[n+1];
+		fac[0] = BigInteger.ONE;
+		fac[1] = BigInteger.ONE;
+
+		for(int i=2; i<n+1; i++)	fac[i] = fac[i-1].multiply(BigInteger.valueOf(i));
+
+		int count = 0;
+
+		while(n>=count) {
+			answer = answer.add((fac[n].divide(fac[n-count].multiply(fac[count]))));
+			n--;
+			count++;
+		}
+
+		return answer.mod(BigInteger.valueOf(1234567)).longValue();
+	}
 }
