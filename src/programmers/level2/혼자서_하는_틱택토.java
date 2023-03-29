d1ee7da40f52bbf3215d62500fb3a183b0490385
fullcrescent <fullcrescent@naver.com>
2023-03-29 13:46:12 +0900

    알고리즘 소스

--- src/programmers/level2/혼자서_하는_틱택토.java
+++ src/programmers/level2/혼자서_하는_틱택토.java
@@ -16,14 +16,50 @@ public class 혼자서_하는_틱택토 {
             nextCount += temp.chars().filter(i -> i=='X').count();
         }
 
-        if(prevCount<nextCount){
+        if(valid(board, 'O') && !valid(board, 'X')){
+            return prevCount==nextCount+1 ? 1 : 0;
+        }else if(valid(board, 'O') && valid(board, 'X')){
             return 0;
+        }else if(valid(board, 'X')){
+            return prevCount==nextCount ? 1: 0;
+        }else{
+            return prevCount==nextCount || prevCount==nextCount+1 ? 1 : 0;
         }
-
-        return 1;
     }
 
-    private static boolean valid(){
-        return true;
+    private static boolean valid(String[] board, char c){
+        for (String s : board) {
+            int count = 0;
+
+            for (int j = 0; j < board[0].length(); j++) {
+                if (c == s.charAt(j)) count++;
+            }
+
+            if (count == 3) return true;
+        }
+
+        for(int i=0; i<board[0].length(); i++){
+            int count = 0;
+
+            for (String s : board) {
+                if (c == s.charAt(i)) count++;
+            }
+
+            if(count==3) return true;
+        }
+
+        int[][][] cross = {{{0,0}, {1,1}, {2,2}}, {{0,2}, {1,1}, {2,0}}};
+
+        for(int[][] crossArray : cross){
+            int count = 0;
+
+            for(int[] temp : crossArray){
+                if(c==board[temp[0]].charAt(temp[1]))   count++;
+            }
+
+            if(count==3) return true;
+        }
+
+        return false;
     }
 }
