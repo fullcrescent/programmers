76757ae78e61833a78f80f1b03667d6123581979
fullcrescent <fullcrescent@naver.com>
2023-04-09 20:42:11 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/당구_연습.java
+++ src/programmers/level2/당구_연습.java
@@ -11,6 +11,14 @@ public class 당구_연습 {
         int[][] balls = {{4, 7}};
         int[] answer = solution(m, n, startX, startY, balls);
         System.out.println(Arrays.toString(answer));
+
+        int m1 = 10;
+        int n1 = 10;
+        int startX1 = 5;
+        int startY1 = 7;
+        int[][] balls1 = {{4, 7}};
+        int[] answer1 = solution1(m1, n1, startX1, startY1, balls1);
+        System.out.println(Arrays.toString(answer1));
     }
 
     public static int[] solution(int m, int n, int startX, int startY, int[][] balls) {
@@ -37,6 +45,34 @@ public class 당구_연습 {
     }
 
     private static int min(int a, int b, int c, int d){
+        return Math.min(a, Math.min(b, Math.min(c, d)));
+    }
+
+    /*다른 사람의 풀이 참고 - 변경X*/
+    public static int[] solution1(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = new int[balls.length];
+        int index = 0;
+
+        for(int[] ball : balls){
+            answer[index++] = min1(
+                    cal1(startX, startY, -ball[0], ball[1], m, n)
+                    , cal1(startX, startY, ball[0], -ball[1], m, n)
+                    , cal1(startX, startY, ball[0] + 2*(m-ball[0]), ball[1], m, n)
+                    , cal1(startX, startY, ball[0], ball[1] + 2*(n-ball[1]), m, n));
+        }
+
+        return answer;
+    }
+
+    private static int cal1(int startX, int startY, int x, int y, int m, int n) {
+        if((startX==x && (y<0 ? Math.abs(y)<startY : startY<Math.abs(y-2*n))) || (startY==y && (x<0 ? Math.abs(x)<startX : startX<Math.abs(x-2*m)))) {
+            return Integer.MAX_VALUE;
+        }
+
+        return (int) (Math.pow(startX-x, 2) + Math.pow(startY-y, 2));
+    }
+
+    private static int min1(int a, int b, int c, int d){
         return Math.min(a, Math.min(b, Math.min(c, d)));
     }
 }
