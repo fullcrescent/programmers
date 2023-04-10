d7af2d2d389445b237fee5eedc8be39eb37fe0c7
fullcrescent <fullcrescent@naver.com>
2023-04-10 18:46:52 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/합승_택시_요금.java
+++ src/programmers/level3/합승_택시_요금.java
@@ -1,9 +1,6 @@
 package programmers.level3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class 합승_택시_요금 {
 
@@ -18,35 +15,46 @@ public class 합승_택시_요금 {
 	}
 	
 	public static int solution(int n, int s, int a, int b, int[][] fares) {
-		Map<Integer, List<Integer>> map = new HashMap<>();
-		
-		for(int[] fare : fares) {
-			List<Integer> tempList;
-			
-			tempList = map.getOrDefault(fare[0], new ArrayList<>());
-			tempList.add(fare[1]);
-			map.put(fare[0], tempList);
-			
-			tempList = map.getOrDefault(fare[1], new ArrayList<>());
-			tempList.add(fare[0]);
-			map.put(fare[1], tempList);
-		}
-		
-		
+		Dijkstra dijkstra = new Dijkstra(n, fares);
+		int min = dijkstra.algorithm(a, b);
+
 		return 0;
 	}
-	
 }
 
-class Path{
-	Character target;
-	int cost;
-	int nextPoint;
-	
-	public Path(Character target, int cost, int nextPoint) {
-		super();
-		this.target = target;
-		this.cost = cost;
-		this.nextPoint = nextPoint;
+class Dijkstra{
+	int vertexCount;
+	int[][] fares;
+	int[] path;
+
+	public Dijkstra(int vertexCount, int[][] fares) {
+		this.vertexCount = vertexCount;
+		this.fares = fares;
+	}
+
+	public int algorithm(int start, int end){
+		Queue<int[]> queue = new PriorityQueue<>((i1, i2) -> Integer.compare(i1[2], i2[2]));
+		Arrays.stream(fares).filter(i -> i[0]==start || i[1]==start).forEach(i -> queue.add(i));
+
+		boolean[] visit = new boolean[vertexCount+1];
+		visit[start] = true;
+
+		while(!queue.isEmpty()){
+			int[] link = queue.poll();
+			int next = visit[link[0]] ? link[1] : link[0];
+			if(next==end)	return link[2];
+
+			if(visit[next]) continue;
+			visit[next] = true;
+
+			Arrays.stream(fares).filter(i -> i[0]==next || i[1]==next).forEach(i -> {
+				if(!visit[i[0]==next ? i[1] : i[0]]){
+					i[2] = i[2]+link[2];
+					queue.add(i);
+				}
+			});
+		}
+
+		return -1;
 	}
 }
