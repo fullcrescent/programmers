99ce4faabcbb0953a814492fe36a727a06ee21bc
fullcrescent <fullcrescent@naver.com>
2023-04-19 23:33:48 +0900

    알고리즘 소스 작성중

--- src/programmers/level2/마법의_엘리베이터.java
+++ src/programmers/level2/마법의_엘리베이터.java
@@ -1,32 +1,48 @@
 package programmers.level2;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.stream.Stream;
 
 public class 마법의_엘리베이터 {
     public static void main(String[] args){
-        int storey = 545;
+        int storey = 2554;
         int answer = solution(storey);
         System.out.println(answer);
     }
 
     public static int solution(int storey) {
-        int answer = 0;
+        Queue<Test> queue = new LinkedList<>();
+        queue.add(new Test(0, storey, 0));
 
-        int[] array = Stream.of(("0"+storey).split("")).mapToInt(Integer::parseInt).toArray();
-        boolean[] flags = new boolean[array.length];
+        while(!queue.isEmpty()){
+            Test temp = queue.poll();
 
-        for(int i=array.length-1; i>-1; i--){
-            int flag = flags[i] ? 1 : 0;
+            int digit = (int) (temp.value/Math.pow(10, temp.depth)%10);
 
-            if(array[i]<5-flag){
-                answer += array[i]-flag;
+            if(digit!=5){
+                int add = Math.min(digit, digit-10);
+                queue.add(new Test(temp.depth+1, temp.value+add, temp.count+add));
             }else{
-                answer += 10-array[i];
-                array[i-1] += 1;
-                flags[i-1] = true;
+
             }
+
+            System.out.println(digit);
         }
 
-        return answer;
+
+        return 0;
+    }
+
+    static class Test{
+        int depth;
+        int value;
+        int count;
+
+        public Test(int depth, int value, int count) {
+            this.depth = depth;
+            this.value = value;
+            this.count = count;
+        }
     }
 }
