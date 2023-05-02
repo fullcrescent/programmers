1ca2f90ea0ea3452ab818ce093c3bf447b777e02
fullcrescent <fullcrescent@naver.com>
2023-05-02 21:38:14 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/숫자_카드_나누기.java
+++ src/programmers/level2/숫자_카드_나누기.java
@@ -4,10 +4,15 @@ import java.util.Arrays;
 
 public class 숫자_카드_나누기 {
     public static void main(String[] args){
-        int[] arrayA = {10, 20};
-        int[] arrayB = {5, 17};
+        int[] arrayA = {14, 35, 119};
+        int[] arrayB = {18, 30, 102};
         int answer = solution(arrayA, arrayB);
         System.out.println(answer);
+
+        int[] arrayA1 = {14, 35, 119};
+        int[] arrayB1 = {18, 30, 102};
+        int answer1 = solution1(arrayA1, arrayB1);
+        System.out.println(answer1);
     }
 
     public static int solution(int[] arrayA, int[] arrayB) {
@@ -42,7 +47,7 @@ public class 숫자_카드_나누기 {
         return 0;
     }
 
-    public static int gcd(int big, int small) {
+    private static int gcd(int big, int small) {
         if(small==0) return big;
         return gcd(small, big%small);
     }
@@ -54,5 +59,31 @@ public class 숫자_카드_나누기 {
             if(value%start==0)  return start;
             start--;
         }
+    }
+
+    /*다른 사람의 풀이 참고*/
+    public static int solution1(int[] arrayA, int[] arrayB) {
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+
+        int gcdA = gcdArray(arrayA);
+        int gcdB = gcdArray(arrayB);
+
+        boolean a = Arrays.stream(arrayB).filter(i -> i%gcdA==0).findFirst().isEmpty();
+        boolean b = Arrays.stream(arrayA).filter(i -> i%gcdB==0).findFirst().isEmpty();
+
+        return a && b ? Math.max(gcdA, gcdB) : a ? gcdA : b ? gcdB : 0;
+    }
+
+    private static int gcdArray(int[] array){
+        int gcd = array[0];
+        for(int value : array)  gcd = gcd1(gcd, value);
+
+        return gcd;
+    }
+
+    private static int gcd1(int big, int small) {
+        if(small==0) return big;
+        return gcd1(small, big%small);
     }
 }
