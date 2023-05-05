499ea3005ffa280ab9ca3bb785286848545ad8e1
fullcrescent <fullcrescent@naver.com>
2023-05-05 23:19:49 +0900

    알고리즘 소스 작성중

---
+++ src/programmers/level2/이모티콘_할인행사.java
@@ -0,0 +1,18 @@
+package programmers.level2;
+
+import java.util.Arrays;
+
+public class 이모티콘_할인행사 {
+    public static void main(String[] args) {
+        int[][] users = {{40, 10000}, {25, 10000}};
+        int[] emoticons = {7000, 9000};
+        int[] answer = solution(users, emoticons);
+        System.out.println(Arrays.toString(answer));
+    }
+
+    public static int[] solution(int[][] users, int[] emoticons) {
+
+
+        return null;
+    }
+}
