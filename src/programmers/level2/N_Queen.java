a2220da42aa7eb3f764682bb0cc940f245d03484
fullcrescent <fullcrescent@naver.com>
2023-05-19 21:54:20 +0900

    알고리즘 소스 작성중

---
+++ src/programmers/level2/N_Queen.java
@@ -0,0 +1,14 @@
+package programmers.level2;
+
+public class N_Queen {
+    public static void main(String[] args) {
+        int n = 4;
+        int answer = solution(n);
+        System.out.println(answer);
+    }
+
+    public static int solution(int n) {
+
+        return 0;
+    }
+}
