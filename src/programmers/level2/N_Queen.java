aa718a3319fdde6217e416dbd03b8f251126acb1
fullcrescent <fullcrescent@naver.com>
2023-05-20 22:35:05 +0900

    알고리즘 소스 작성중

--- src/programmers/level2/N_Queen.java
+++ src/programmers/level2/N_Queen.java
@@ -1,5 +1,8 @@
 package programmers.level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class N_Queen {
     public static void main(String[] args) {
         int n = 4;
@@ -8,6 +11,23 @@ public class N_Queen {
     }
 
     public static int solution(int n) {
+        for(int i=0; i<n; i++){
+            boolean[] visit = new boolean[n];
+            visit[i] = true;
+
+            List<Integer> list = new ArrayList<>();
+            list.add(i);
+
+            for(int j=1; j<n; j++){
+                for(int k=0; k<n; k++){
+                    if(visit[k]) continue;
+
+
+                }
+            }
+
+        }
+
 
         return 0;
     }
