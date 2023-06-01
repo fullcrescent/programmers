f676a10c77f0988a7e2e1d5b0eb989fc5b1bc745
fullcrescent <fullcrescent@naver.com>
2023-06-02 00:13:14 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/아방가르드_타일링.java
+++ src/programmers/level3/아방가르드_타일링.java
@@ -2,20 +2,37 @@ package programmers.level3;
 
 public class 아방가르드_타일링 {
     public static void main(String[] args){
-        int n = 3;
+        int n = 9;
         int answer = solution(n);
         System.out.println(answer);
     }
 
     public static int solution(int n) {
-        // 1 > 1 : 1
-        // 2 > 1+1 > 1 / 2 > 2 : 3
-        // 3 > 1+1+1 > 1 /  1+2 > 2 /  2+1 > 2 / 3 > 5 : 10
-        // 4 > 1+1+1+1 > 1 / 1+1+2 > 2 / 1+2+1 > 2 / 2+1+1 > 2 / 2+2 > 4 / 3+1 > 5 / 1+3 > 5 : 21
+        if(n==1) return 1;
+        if(n==2) return 3;
 
+        long[] dp = new long[n+1];
 
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 3;
 
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]*2 + dp[i-3]*5 + add(dp, i-4))%1000000007;
+        }
 
-        return 0;
+        return (int) dp[n];
+    }
+
+    private static long add(long[] dp, int i) {
+        long sum = 0;
+        int[] array = new int[] {2, 2, 4};
+        int index = 0;
+
+        while(i>-1){
+            sum += dp[i--]*array[index++%3];
+        }
+
+        return sum;
     }
 }
