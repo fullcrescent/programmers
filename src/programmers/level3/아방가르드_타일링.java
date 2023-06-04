7801bc91d514ca462cf9da8aabe76a6b7f56e1cb
fullcrescent <fullcrescent@naver.com>
2023-06-04 21:52:33 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/아방가르드_타일링.java
+++ src/programmers/level3/아방가르드_타일링.java
@@ -7,6 +7,10 @@ public class 아방가르드_타일링 {
         int n = 9;
         int answer = solution(n);
         System.out.println(answer);
+
+        int n1 = 9;
+        int answer1 = solution1(n1);
+        System.out.println(answer1);
     }
 
     public static int solution(int n) {
@@ -23,7 +27,7 @@ public class 아방가르드_타일링 {
             // 1 > 1 : 1
             // 2 > 1+1>1 / 2>2 : 3
             // 3 > 1+1+1>1 / 1+2>2 / 2+1>2 / 3>5 : 10
-            // 4 > 1+1+1+1>1 / 1+1+2>2 / 1+2+1>2 / 1+3>5 || 2+1+1>2 / 2+2>4 || 3+1>5 || 질문하기 테스트 케이스 참고>2 : 21 + 2
+            // 4 > 1+1+1+1>1 / 1+1+2>2 / 1+2+1>2 / 1+3>5 || 2+1+1>2 / 2+2>4 || 3+1>5 || 질문하기 테스트 케이스 참고:add>2 : 21 + 2
             dp[i] = (dp[i-1] + dp[i-2]*2 + dp[i-3]*5 + add(dp, i-4))%1000000007;
         }
 
@@ -36,5 +40,24 @@ public class 아방가르드_타일링 {
         return IntStream.range(0, end+1)
                 .mapToLong(i -> dp[i]*array[(end-i)%3])
                 .sum();
+    }
+
+    /*다른 사람의 풀이 참고*/
+    public static int solution1(int n) {
+        int mod = 1000000007;
+        long[] dp = new long[100001];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 10;
+        dp[4] = 23;
+        dp[5] = dp[4] + dp[3]*2 + dp[2]*5 + dp[1]*2 + dp[0]*2;
+
+        for(int i=6; i<=n; i++){
+          //dp[i] = (dp[i-1] + dp[i-2]*2 + dp[i-3]*5 + add(dp, i-4))        % mod;
+            dp[i] = (dp[i-1] + dp[i-2]*2 + dp[i-3]*6 + dp[i-4]-dp[i-6]+mod) % mod;
+        }
+
+        return (int) dp[n];
     }
 }
