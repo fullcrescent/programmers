36597bd3839ff70a6f16f695f05e90e6a6f3b91d
fullcrescent <fullcrescent@naver.com>
2023-06-07 21:24:11 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/표_병합.java
+++ src/programmers/level3/표_병합.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class 표_병합 {
     public static void main(String[] args) {
-        String[] commands = {"UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"};
+        String[] commands = {"UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"};
         String[] answer = solution(commands);
         System.out.println(Arrays.toString(answer));
     }
@@ -19,7 +19,7 @@ public class 표_병합 {
 
             String action = temp[0];
 
-            if(!isNumber(temp[1])){
+            if(action.equals("UPDATE")&&temp.length==3){
                 infoList.stream().filter(i -> i.value.equals(temp[1])).forEach(i -> i.value=temp[2]);
                 continue;
             }
@@ -47,10 +47,12 @@ public class 표_병합 {
                     int mergeY = Integer.parseInt(temp[4]);
 
                     if(array[mergeX][mergeY]!=null){
+                        infoList.remove(array[mergeX][mergeY]);
                         array[mergeX][mergeY].list.forEach(i -> array[i.x][i.y]=array[x][y]);
                     }else{
                         array[mergeX][mergeY] = array[x][y];
                     }
+
                     array[x][y].addPoint(new Point(mergeX, mergeY));
 
                     break;
@@ -62,6 +64,7 @@ public class 표_병합 {
                     array[x][y].list.forEach(i -> array[i.x][i.y]=null);
 
                     array[x][y] = info;
+                    infoList.add(info);
 
                     break;
                 case "PRINT":
@@ -75,9 +78,6 @@ public class 표_병합 {
     }
 
     private static final int max = 51;
-    private static boolean isNumber(String s) {
-        return s!=null && s.matches("[0-9]+");
-    }
 
     static class Info{
         String value;
