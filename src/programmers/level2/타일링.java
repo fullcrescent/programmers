8af654af5dfaa2cf004eb71db222fc68199c3e46
fullcrescent <fullcrescent@naver.com>
2023-06-09 08:04:23 +0900

    알고리즘 소스

--- src/programmers/level2/타일링.java
+++ src/programmers/level2/타일링.java
@@ -3,21 +3,22 @@ package programmers.level2;
 public class 타일링 {
 
 	public static void main(String[] args) {
-		int n = 8;
+		int n = 1000;
 		int answer = solution(n);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int n) {
-		int[] dp = new int[n+1];
+		long[] dp = new long[n+1];
 		dp[2] = 3;
 		dp[4] = 11;
 
 		// 기존 경우의 수*3 + 기존 경우의 수에서 가로로 되어있는 건 제외
 		for(int i=6; i<=n; i=i+2){
-			dp[i] = dp[i-2]*3 + (dp[i-2]-dp[i-4])%1000000007;
+			dp[i] = dp[i-2]*3 + (dp[i-2]-dp[i-4]);
+			dp[i] = (dp[i]+1000000007)%1000000007;
 		}
 
-		return dp[n];
+		return (int) dp[n];
 	}
 }
