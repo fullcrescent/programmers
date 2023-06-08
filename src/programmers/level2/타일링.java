f42f90d50a2aae8ba513b7db509bbed888a5cb4e
fullcrescent <fullcrescent@naver.com>
2023-06-09 07:48:10 +0900

    알고리즘 소스

--- src/programmers/level2/타일링.java
+++ src/programmers/level2/타일링.java
@@ -3,32 +3,21 @@ package programmers.level2;
 public class 타일링 {
 
 	public static void main(String[] args) {
-		int n = 6;
+		int n = 8;
 		int answer = solution(n);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int n) {
-		int divide = 1000000007;
-		
-		int vertical = 1;
-		int horizontal = 1;
-		int fac = 1;
-		
-		int index = 2;
-		
-		while(index++<n) {
-			int temp = vertical;
-			vertical = ((vertical + horizontal) % divide);
-			horizontal = temp;
-			fac = (fac * (fac+1)) %divide ;
+		int[] dp = new int[n+1];
+		dp[2] = 3;
+		dp[4] = 11;
+
+		// 기존 경우의 수*3 + 기존 경우의 수에서 가로로 되어있는 건 제외
+		for(int i=6; i<=n; i=i+2){
+			dp[i] = dp[i-2]*3 + (dp[i-2]-dp[i-4])%1000000007;
 		}
-		
-		int n2 = (vertical + horizontal) % divide;
-		
-//		int add = Math.pow(3, fac-2);
-		int answer = (n2*2) % divide;
-		
-		return answer-1;
+
+		return dp[n];
 	}
 }
