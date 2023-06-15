b6abcc4df950c9ad1630d58f896c08a8bc6ba5a7
fullcrescent <fullcrescent@naver.com>
2023-06-15 23:22:03 +0900

    알고리즘 소스 작성중

--- src/programmers/level2/하노이의_탑.java
+++ src/programmers/level2/하노이의_탑.java
@@ -8,34 +8,33 @@ import java.util.stream.IntStream;
 
 public class 하노이의_탑 {
     public static void main(String[] args){
-        int n = 2;
+        int n = 4;
         int[][] answer = solution(n);
         Arrays.stream(answer).forEach(i -> Arrays.toString(i));
     }
 
     public static int[][] solution(int n) {
+        List<Stack<Integer>> list = new ArrayList<>();
+        /*stream.map으로 값 반환 stack 풀로 찬것을 기준으로 순서 매핑*/
+
         List<int[]> answer = new ArrayList<>();
         Stack<Integer> stack1 = new Stack<>();
         Stack<Integer> stack2 = new Stack<>();
         Stack<Integer> stack3 = new Stack<>();
 
-        IntStream.range(1, n+1).forEach(i -> stack1.add(i));
-
-        if(stack1.size()%2==0){
-
-        }
-
         while(!stack1.isEmpty()){
+
+
+            Stack<Integer> temp = stack2.peek() < stack3.peek() ? stack2 : stack3;
+
             int size = stack1.size();
             int value = stack1.pop();
 
-            if(stack2.isEmpty()){
-                answer.add(new int[] {1, 2});
+            if(size%2==value%2){
+                answer.add(new int[] {value, 2});
                 stack2.add(value);
-            }
-
-            if(stack3.isEmpty()){
-                answer.add(new int[] {1, 3});
+            }else{
+                answer.add(new int[] {value, 3});
                 stack3.add(value);
             }
 
