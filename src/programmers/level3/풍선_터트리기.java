49949138241d48e22bcad9d5ecfdd4e368c000c7
fullcrescent <fullcrescent@naver.com>
2023-06-17 23:10:48 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/풍선_터트리기.java
+++ src/programmers/level3/풍선_터트리기.java
@@ -3,32 +3,28 @@ package programmers.level3;
 public class 풍선_터트리기 {
 
 	public static void main(String[] args) {
-		int[] a = {-16,27,65,66,67,68,69,70,71,72};
+		int[] a = {-16,27,65,-2,58,-92,-71,-68,-61,-33};
 		int answer = solution(a);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int[] a) {
-		int answer = a.length;
-		
-		int left = a[0];
-		int right = 1000000000;
-		
-		for(int i=2; i<a.length; i++) right = Math.min(right, a[i]);
-		
-		for(int i=1; i<a.length-1; i++) {
-			int value = a[i];
-			
-			left = Math.min(left, a[i-1]);
-			
-			if(left<value)	{
-				right = 1000000000;
-				for(int j=i+1; j<a.length; j++) right = Math.min(right, a[j]);
+		int minLeft = 1000000000;
+		int minRight = 1000000000;
 
-				if(value>right)	answer--;
+		int leftIndex = 0;
+		int rightIndex = a.length-1;
+
+		while(leftIndex<rightIndex){
+			if(a[leftIndex]<minLeft){
+				minLeft = a[leftIndex++];
+			}else if(a[rightIndex]<minRight){
+				minRight = a[rightIndex--];
+			}else{
+				break;
 			}
 		}
-		
-		return answer;
+
+		return a.length-(rightIndex-leftIndex+1);
 	}
 }
