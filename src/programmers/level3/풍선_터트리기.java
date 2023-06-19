29b3d9e3258af441bf7992664e39ffda9515e165
fullcrescent <fullcrescent@naver.com>
2023-06-19 23:22:08 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/풍선_터트리기.java
+++ src/programmers/level3/풍선_터트리기.java
@@ -1,34 +1,44 @@
 package programmers.level3;
 
-import java.util.Arrays;
-
 public class 풍선_터트리기 {
 
 	public static void main(String[] args) {
-		int[] a = {9, -1, -5};
+		int[] a = {9, -1, 5};
 		int answer = solution(a);
 		System.out.println(answer);
 	}
 	
 	public static int solution(int[] a) {
+		int count = 2;
+
 		int leftIndex = 0;
 		int rightIndex = a.length-1;
 
 		int minLeft = a[leftIndex++];
 		int minRight = a[rightIndex--];
 
-		while(leftIndex<rightIndex){
+		while(leftIndex<=rightIndex){
 			if(a[leftIndex]<minLeft){
 				minLeft = a[leftIndex++];
 			}else if(a[rightIndex]<minRight){
 				minRight = a[rightIndex--];
-			}else{
-				break;
+			}else if(minLeft<minRight){
+				while(a[rightIndex]>minRight){
+					rightIndex--;
+				}
+				minRight=a[rightIndex];
+			}else if(minRight<minLeft){
+				while(a[leftIndex]>minLeft){
+					leftIndex++;
+				}
+				minLeft=a[leftIndex];
 			}
+
+			if(minLeft==minRight) count--;
+
+			count++;
 		}
 
-		int filter = Math.max(a[leftIndex-1], a[rightIndex+1]);
-
-		return a.length-(rightIndex-leftIndex+1) + (int) Arrays.stream(a, leftIndex, rightIndex + 1).filter(i -> i<filter).count();
+		return count;
 	}
 }
