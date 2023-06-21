c3c804b4c8bb6531ef30c6102d0ad5fa354c0fe9
fullcrescent <fullcrescent@naver.com>
2023-06-21 22:45:54 +0900

    알고리즘 소스 작성중

---
+++ src/programmers/level3/거스름돈.java
@@ -0,0 +1,21 @@
+package programmers.level3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 거스름돈 {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] money = {1,2,5};
+        int answer = solution(n, money);
+        System.out.println(answer);
+    }
+
+    public static int solution(int n, int[] money) {
+        Set<Integer> set = new HashSet<>();
+
+
+
+        return 0;
+    }
+}
