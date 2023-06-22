03a4d23dfc273ae760d1649aedf4487d80f96a78
fullcrescent <fullcrescent@naver.com>
2023-06-22 19:44:59 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/표_편집.java
+++ src/programmers/level3/표_편집.java
@@ -15,104 +15,26 @@ public class 표_편집 {
 	public static String solution(int n, int k, String[] cmd) {
 		Stack<Integer> stack = new Stack<>();
 		boolean[] remove = new boolean[n];
-		boolean[] visit = new boolean[n];
-		
-		for(int i=0; i<cmd.length-1; i++) {
-			Character temp = cmd[i].charAt(0);
-			if(temp=='U' && cmd[i+1].charAt(0)=='D'
-					|| temp=='D' && cmd[i+1].charAt(0)=='U') {
-				visit[i] = true;
-				visit[i+1] = true;
-			}
-		}
-		
-		int start = k;
-		
-		for(int i=0; i<cmd.length; i++ ) {
-			if(visit[i]) continue;
-			
-			String temp = cmd[i];
-			int value = 1;
-			
-			if(temp.length()>1) {
-				value = Integer.valueOf(temp.split(" ")[1]);
-			}
-			
-			switch(temp.charAt(0)) {
+
+		for(String command : cmd) {
+			Character temp = command.charAt(0);
+
+			switch(temp) {
 				case 'U' :
-					start = binarySearch(remove, 0, start-1, value);
-					
+
 					break;
-				case 'D' : 
-					start = binarySearch(remove, start+1, n, value);
-					
+				case 'D' :
+
 					break;
 				case 'C' :
-					stack.add(start);
-					remove[start] = true;
-					
-					start = binarySearch(remove, start+1, n, value);
-					
-					break;
-				case 'Z' : 
-					remove[stack.pop()] = false;
-					
-					break;
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0; i<n; i++) {
-			if(remove[i])	sb.append("X");
-			else 			sb.append("O");
-		}
-		
-		return sb.toString();
-	}
 
-	private static int binarySearch(boolean[] array, int left, int right, int value) {
-		int mid = 0, index = 0, weight = 0, answer = 0;
-		
-		if(left==0) {
-			index = right;
-			weight = -1;
-		}
-		else{
-			index = left;
-			weight = 1;
-		}
-		
-		while(left<=right){
-			int count = 0;
-			
-			mid = (left+right)/2;
-			
-			for(int i=index; i!=mid+weight; i += weight) {
-				if(i==array.length) return binarySearch(array, 0, left-2, value);
-				
-				if(!array[i]) count++;
-				
-				if(count==value) break;
-			}
-			
-			if(weight==1) {
-				if(count==value) {
-					answer = mid;
-					right = mid-1;
-				}else {
-					left = mid+1;
-				}
-			}else {
-				if(count==value) {
-					answer = mid;
-					left = mid+1;
-				}else {
-					right = mid-1;
-				}
+					break;
+				case 'Z' :
+
+					break;
 			}
 		}
-		
-		return answer;
+
+		return null;
 	}
 }
