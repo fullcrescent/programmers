6569dd9a432272054edac94ad3e51581a3a92c3f
fullcrescent <fullcrescent@naver.com>
2023-06-30 12:21:53 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/파괴되지_않는_건물.java
+++ src/programmers/level3/파괴되지_않는_건물.java
@@ -10,29 +10,44 @@ public class 파괴되지_않는_건물 {
         System.out.println(answer);
     }
 
-    public static int solution(int[][] board, int[][] skills) {
+    public static int solution(int[][] board, int[][] skill) {
         int row = board.length;
-        int column = board[0].length;
+        int column = board[0].length+1;
 
-        int[] temp = new int[row*column];
+        int[][] sum = new int[row+1][column+1];
 
-        for(int i=0; i<row; i++){
-            System.arraycopy(board[i], 0, temp, i * column, column);
+        for(int[] temp : skill){
+            int x1=temp[1], y1=temp[2], x2=temp[3], y2=temp[4], value=temp[5];
+
+            if(temp[0]==1) value*=-1;
+
+            sum[x1][y1] = value;
+            sum[x1][y2+1] = -value;
+            sum[x2+1][y1] = -value;
+            sum[x2+1][y2+1] = value;
         }
 
-        Arrays.stream(skills)
-                .forEach(skill -> {
-                    int add = skill[0]==1 ? -skill[5] : skill[5];
+        for(int i=0; i<row; i++){
+            for(int j=1; j<column; j++){
+                sum[i][j] += sum[i][j-1];
+            }
+        }
 
-                    for(int a=skill[1]; a<=skill[3]; a++){
-                        for(int b=skill[2]; b<=skill[4]; b++){
-                            temp[a*column + b] += add;
-                        }
-                    }
-                });
+        for(int i=0; i<column; i++){
+            for(int j=1; j<row; j++){
+                sum[j][i] += sum[j-1][i];
+            }
+        }
 
-        return (int) Arrays.stream(temp)
-                .filter(i -> i>0)
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[i].length; j++){
+                board[i][j] += sum[i][j];
+            }
+        }
+
+        return (int) Arrays.stream(board)
+                .flatMapToInt(Arrays::stream)
+                .filter(i -> i>=0)
                 .count();
     }
 }
