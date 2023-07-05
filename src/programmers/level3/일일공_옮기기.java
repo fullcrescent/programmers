66d2bf99c9c6b29bf448a410e885ca24ad1c26f2
fullcrescent <fullcrescent@naver.com>
2023-07-06 01:03:58 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/일일공_옮기기.java
+++ src/programmers/level3/일일공_옮기기.java
@@ -16,26 +16,15 @@ public class 일일공_옮기기 {
 
         for(String temp : s){
             StringBuilder sb = new StringBuilder(temp);
+            StringBuilder add = new StringBuilder();
 
-            while(true){
-                String prev = sb.toString();
-
+            while(sb.indexOf("110")!=-1){
                 int index_110 = sb.indexOf("110");
-                int lastIndex_110 = sb.lastIndexOf("110");
-                int index_111 = sb.indexOf("111");
-
-                if(index_110!=-1){
-                    if(index_111==-1){
-                        sb.delete(index_110, index_110+3);
-                        sb.insert(sb.lastIndexOf("0")+1, "110");
-                    }else if(lastIndex_110>index_111){
-                        sb.delete(lastIndex_110, lastIndex_110+3);
-                        sb.insert(index_111, "110");
-                    }
-                }
-
-                if(prev.equals(sb.toString()))  break;
+                sb.delete(index_110, index_110+3);
+                add.append("110");
             }
+
+            sb.insert(sb.lastIndexOf("0")+1, add);
 
             list.add(sb.toString());
         }
