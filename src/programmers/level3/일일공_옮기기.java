f173271459789de77d533a64a0ea6b55aaa2984c
fullcrescent <fullcrescent@naver.com>
2023-07-09 20:16:17 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/일일공_옮기기.java
+++ src/programmers/level3/일일공_옮기기.java
@@ -10,9 +10,50 @@ public class 일일공_옮기기 {
         String[] s = {"1100111011101001"};
         String[] answer = solution(s);
         System.out.println(Arrays.toString(answer));
+
+        String[] s1 = {"1100111011101001"};
+        String[] answer1 = solution1(s1);
+        System.out.println(Arrays.toString(answer1));
     }
 
     public static String[] solution(String[] s) {
+        List<String> list = new ArrayList<>();
+
+        for(String temp : s){
+            StringBuilder add = new StringBuilder();
+            Stack<Character> stack = new Stack<>();
+
+            for(Character c : temp.toCharArray()){
+                if(!stack.isEmpty() && stack.peek()=='1' && c=='0'){
+                    stack.pop();
+
+                    if(!stack.isEmpty() && stack.peek()=='1'){
+                        stack.pop();
+                        add.append("110");
+                    }else{
+                        stack.add('1');
+                        stack.add(c);
+                    }
+                }else{
+                    stack.add(c);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while(!stack.isEmpty()){
+                sb.insert(0, stack.pop());
+            }
+
+            sb.insert(sb.lastIndexOf("0")+1, add);
+
+            list.add(sb.toString());
+        }
+
+        return list.toArray(String[]::new);
+    }
+
+    /*다른 사람의 풀이 참고 - 변경X*/
+    public static String[] solution1(String[] s) {
         List<String> list = new ArrayList<>();
 
         for(String temp : s){
