f87338bc210f1d77ca4736fe881653e07cc70467
fullcrescent <fullcrescent@naver.com>
2023-07-10 22:56:11 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level2/하노이의_탑.java
+++ src/programmers/level2/하노이의_탑.java
@@ -9,6 +9,10 @@ public class 하노이의_탑 {
         int n = 3;
         int[][] answer = solution(n);
         Arrays.stream(answer).forEach(i -> System.out.println(Arrays.toString(i)));
+
+        int n1 = 3;
+        int[][] answer1 = solution1(n1);
+        Arrays.stream(answer1).forEach(i -> System.out.println(Arrays.toString(i)));
     }
 
     public static int[][] solution(int n) {
@@ -25,5 +29,21 @@ public class 하노이의_탑 {
             answer.add(new int[] {start, end});
             function(n-1, mid, start, end, answer);
         }
+    }
+
+    /*다른 사람의 풀이 참고*/
+    public static int[][] solution1(int n) {
+        return function1(new ArrayList<>(), n, 1, 2, 3);
+    }
+
+    private static int[][] function1(List<int[]> answer, int n, int start, int path, int end) {
+        if(n==1){
+            answer.add(new int[] {start, end});
+            return answer.toArray(int[][]::new);
+        }
+
+        function1(answer, n-1, start, end, path);
+        answer.add(new int[] {start, end});
+        return function1(answer, n-1, path, start, end);
     }
 }
