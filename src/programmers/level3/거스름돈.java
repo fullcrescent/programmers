b33e98d9e63ea1d7a30f280c5db1490373bd4eab
fullcrescent <fullcrescent@naver.com>
2023-07-14 22:36:47 +0900

    알고리즘 소스 및 다른 사람의 풀이 참고

--- src/programmers/level3/거스름돈.java
+++ src/programmers/level3/거스름돈.java
@@ -8,6 +8,11 @@ public class 거스름돈 {
         int[] money = {1,2,5};
         int answer = solution(n, money);
         System.out.println(answer);
+
+        int n1 = 6;
+        int[] money1 = {1,2,5};
+        int answer1 = solution1(n1, money1);
+        System.out.println(answer1);
     }
 
     public static int solution(int n, int[] money) {
@@ -22,6 +27,23 @@ public class 거스름돈 {
                         dp[j] += dp[j-i]%mod;
                     }
                 });
+
+        return dp[n];
+    }
+
+    /*다른 사람의 풀이 참고*/
+    public static int solution1(int n, int[] money) {
+        int[] dp = new int[n+1];
+
+        for(int temp : money){
+            dp[temp] += 1;
+
+            for(int i=1; i<=n; i++){
+                if(temp<=i){
+                    dp[i] += dp[i-temp];
+                }
+            }
+        }
 
         return dp[n];
     }
