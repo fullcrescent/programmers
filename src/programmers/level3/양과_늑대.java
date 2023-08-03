c1fb7e061ecfd4211e004fef1cc8911fdccfa16b
fullcrescent <fullcrescent@naver.com>
2023-08-03 19:10:48 +0900

    알고리즘 소스 작성중

---
+++ src/programmers/level3/양과_늑대.java
@@ -0,0 +1,16 @@
+package programmers.level3;
+
+public class 양과_늑대 {
+    public static void main(String[] args) {
+        int[] info = {0,0,1,1,1,0,1,0,1,0,1,1};
+        int[][] edges = {{0,1},{1,2},{1,4},{0,8},{8,7},{9,10},{9,11},{4,3},{6,5},{4,6},{8,9}};
+        int answer = solution(info, edges);
+        System.out.println(answer);
+    }
+
+    public static int solution(int[] info, int[][] edges) {
+
+
+        return 0;
+    }
+}
