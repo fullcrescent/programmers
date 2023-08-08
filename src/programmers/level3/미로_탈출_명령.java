89d75224e996d758719c4ea8b2df715500c3f032
fullcrescent <fullcrescent@naver.com>
2023-08-08 22:57:40 +0900

    알고리즘 소스 작성중

---
+++ src/programmers/level3/미로_탈출_명령.java
@@ -0,0 +1,22 @@
+package programmers.level3;
+
+public class 미로_탈출_명령 {
+    public static void main(String[] args) {
+        int n = 3;
+        int m = 4;
+        int x = 2;
+        int y = 3;
+        int r = 3;
+        int c = 1;
+        int k = 5;
+        String answer = solution(n, m, x, y, r, c, k);
+        System.out.println(answer);
+    }
+
+    public static String solution(int n, int m, int x, int y, int r, int c, int k) {
+        char[] array = {'d', 'l', 'r', 'u'};
+
+
+        return "impossible";
+    }
+}
