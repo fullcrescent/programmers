7158bab9700a7a81f734bc413186c59b731284fe
fullcrescent <fullcrescent@naver.com>
2023-08-09 17:25:34 +0900

    알고리즘 소스 작성중

--- src/programmers/level3/양과_늑대.java
+++ src/programmers/level3/양과_늑대.java
@@ -1,5 +1,10 @@
 package programmers.level3;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class 양과_늑대 {
     public static void main(String[] args) {
         int[] info = {0,0,1,1,1,0,1,0,1,0,1,1};
@@ -9,8 +14,80 @@ public class 양과_늑대 {
     }
 
     public static int solution(int[] info, int[][] edges) {
+        if(info[0]==1) return 0;
+
+        Map<Integer, Node> map = new HashMap<>();
+
+        for(int i=0; i<info.length; i++){
+            map.put(i, new Node(info[i]==0? Type.SHEEP : Type.WOLF));
+        }
+
+        for(int[] edge : edges){
+            Node parent = map.get(edge[0]);
+            Node child = map.get(edge[1]);
+
+            parent.addNode(child);
+            child.parent = parent;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(map.get(0));
+        int sheepCount = 0;
+
+        while(!queue.isEmpty()){
+            Node temp = queue.poll();
+            sheepCount++;
+            temp.visit = true;
+
+            Node leftNode = temp.left;
+            Node rightNode = temp.right;
+
+            if(leftNode!=null && leftNode.type==Type.SHEEP){
+                queue.add(leftNode);
+            }
+            if(rightNode!=null && rightNode.type==Type.SHEEP){
+                queue.add(rightNode);
+            }
+        }
+
+        for(int key : map.keySet()){
+            Node temp = map.get(key);
+
+            if(temp.visit)  continue;
+
+            if(temp.parent.type==Type.WOLF){
+                queue.add(temp);
+            }
+        }
 
 
-        return 0;
+
+        return sheepCount;
+    }
+
+    static class Node{
+        Node parent;
+        Node left;
+        Node right;
+        Type type;
+        boolean visit;
+
+        public Node(Type type) {
+            this.type = type;
+        }
+
+        public void addNode(Node node) {
+            if(left==null){
+                left = node;
+            }else{
+                right = node;
+            }
+        }
+
+
+    }
+
+    enum Type{
+        SHEEP, WOLF;
     }
 }
